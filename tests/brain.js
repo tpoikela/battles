35016e5eb7f78c4f896f155def956230ea307939
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-08-29 14:39:19 +0200

    Added test section for playerbrain.

--- tests/brain.js
+++ tests/brain.js
@@ -1,10 +1,19 @@
-
+e
 var chai = require("chai");
 var expect = chai.expect;
 
 var RG = require("../battles");
 
 var Brain = RG.Brain;
+
+describe('How PlayerBrain handles commands', function() {
+    it('description', function() {
+        var player = new RogueActor("Player");
+        player.setIsPlayer(true);
+        var brain = new Brain.Player(player);
+
+    });
+});
 
 describe('Basic functions of Rogue Brain', function() {
     it('description', function() {
