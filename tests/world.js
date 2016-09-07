52b8be0be7972c6756d3b1bd37e00cb5c980ba5a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-09-07 23:31:05 +0200

    Added unit test file for RG.World

---
+++ tests/world.js
@@ -0,0 +1,36 @@
+
+var chai = require("chai");
+var expect = chai.expect;
+
+var RG = require("../battles.js");
+
+var World = require("../src/world.js");
+
+
+describe('World.Branch', function() {
+    it('Contains a number of connected levels', function() {
+    });
+});
+
+
+describe('World.Dungeon', function() {
+    it('Contains a number of connected branches', function() {
+    });
+});
+
+describe('World.AreaTile', function() {
+    it('Contains a level and connects from sides to other tiles', function() {
+    });
+});
+
+
+describe('World.Area', function() {
+    it('Contains a number of connected tiles', function() {
+    });
+});
+
+
+describe('World.World', function() {
+    it('Contains a number of dungeon and areas', function() {
+    });
+});
