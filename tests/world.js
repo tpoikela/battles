bc534a920d386349939409d50f9fb9e0d4cb3c9b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-09-08 00:01:12 +0200

    Added stairs connection test for Branch.

--- tests/world.js
+++ tests/world.js
@@ -9,6 +9,26 @@ var World = require("../src/world.js");
 
 describe('World.Branch', function() {
     it('Contains a number of connected levels', function() {
+        var nlevels = 4;
+        var levels = [];
+        var branch = new World.Branch();
+        for (var i = 0; i < nlevels; i++) {
+            levels.push(RG.FACT.createLevel("arena", 20, 20));
+            branch.addLevel(levels[i]);
+            expect(branch.hasLevel(levels[i])).to.equal(true);
+        }
+
+        branch.connectLevels();
+
+        var stairs = levels[0].getStairs(levels[1]);
+        expect(stairs === null).to.equal(false);
+        stairs = levels[0].getStairs(levels[2]);
+        expect(stairs === null).to.equal(true);
+        var entrance = branch.getEntrance();
+        expect(entrance === null).to.equal(false);
+
+
+
     });
 });
 
