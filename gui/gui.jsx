5b797f39ef3350a312e6b539688b4371d426004a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-09-16 23:29:12 +0200

    Added few missing commands to the help modal.

--- gui/gui.jsx
+++ gui/gui.jsx
@@ -259,22 +259,28 @@ var GameHelpScreen = React.createClass({
                         <div className="modal-body row">
                             <div className="col-md-6">
                                 <p>To move around, use:</p>
-                                <table id="mov-buttons-table" className="table">
+                                <table id="mov-buttons-table" className="table table-large">
                                     <thead></thead>
                                     <tbody>
-                                        <tr><td>Move NW: q</td><td> Move N: w</td><td>Move NE: e</td></tr>
-                                        <tr><td>Move W: a</td><td>Rest: s</td><td>Move E: d</td></tr>
-                                        <tr><td>Move SW: z</td><td>Move S: x</td><td>Move SE: c</td></tr>
+                                        <tr><td>{'\u2B09'} q</td><td>{'\u2B06'} w</td><td>{'\u2B08'} e</td></tr>
+                                        <tr><td>{'\u2B05'} a</td><td>Rest: s</td><td>{'\u27A1'} d</td></tr>
+                                        <tr><td>{'\u2B0B'} z</td><td>{'\u2B07'} x</td><td>{'\u2B0A'} c</td></tr>
                                     </tbody>
                                 </table>
                             </div>
                             <div className="col-md-6 help-info-buttons">
+                                <p><span className="text-primary">b</span> - Use stairs.</p>
+                                <p><span className="text-primary">f</span> - Change fight mode.</p>
+                                <p><span className="text-primary">h</span> - See next item in the cell.</p>
                                 <p><span className="text-primary">m</span> - Toggle the map/player view.</p>
+                                <p><span className="text-primary">n</span> - Next target (when in targeting mode).</p>
                                 <p><span className="text-primary">i</span> - View inventory.</p>
+                                <p><span className="text-primary">o</span> - Open/close door.</p>
                                 <p><span className="text-primary">r</span> - Toggle run mode (1.5 x speed).</p>
-                                <p><span className="text-primary">t</span> - Enter target mode. Click on a cell to fire.</p>
-                                <p><span className="text-primary">.</span> - Pick up an item.</p>
-                                <p><span className="text-primary">,</span> - Use stairs.</p>
+                                <p><span className="text-primary">t</span> - Enter targeting mode. Press again to fire.</p>
+                                <p><span className="text-primary">u</span> - Use an item.</p>
+                                <p><span className="text-primary">,</span> - Pick up an item.</p>
+                                <p><span className="text-primary">.</span> - Rest (takes less energy than moving).</p>
                             </div>
                         </div>
 
