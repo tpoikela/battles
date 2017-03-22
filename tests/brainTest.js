045dec3e658010ef33ba8ead1b93a2c149afa1d5
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-22 18:02:02 +0100

    Added tests for commands requiring confirmation.

--- tests/brainTest.js
+++ tests/brainTest.js
@@ -10,17 +10,29 @@ var Brain = RG.Brain;
 
 describe('Brain.Player', function() {
 
-    var level = RG.FACT.createLevel('arena', 10, 10);
-    var player = new RG.Actor.Rogue('Player');
+    var level = null;
+    var player = null;
+    var demon = null;
+    var human = null;
 
-    var demon = new RG.Actor.Rogue('Demon');
-    demon.setType('demon');
-    demon.setBrain(new RG.Brain.Demon(demon));
-    demon.addEnemy(player);
+    beforeEach( () => {
 
-    player.setIsPlayer(true);
-    level.addActor(player, 1, 1);
-    level.addActor(demon, 1, 2);
+        level = RG.FACT.createLevel('arena', 10, 10);
+        player = new RG.Actor.Rogue('Player');
+        demon = new RG.Actor.Rogue('Demon');
+        human = new RG.Actor.Rogue('Human friend');
+
+        demon.setType('demon');
+        demon.setBrain(new RG.Brain.Demon(demon));
+        demon.addEnemy(player);
+
+        human.setType('human');
+        human.setBrain(new RG.Brain.Human(human));
+        player.setIsPlayer(true);
+        level.addActor(player, 1, 1);
+        level.addActor(demon, 1, 2);
+
+    });
 
     it('Accepts key commands', function() {
         var brain = new Brain.Player(player);
@@ -85,10 +97,75 @@ describe('Brain.Player', function() {
         attackCallback();
         expect(player.getSpeed()).to.equal(80);
     });
+
+    it('Needs confirm before attacking friends', function() {
+        level.addActor(human, 2, 2);
+        var brain = new Brain.Player(player);
+
+        brain.decideNextAction({code: RG.K_MOVE_SE});
+        expect(brain.energy).to.equal(0);
+        brain.decideNextAction({code: RG.K_REST});
+        expect(brain.energy).to.equal(0);
+
+        brain.decideNextAction({code: RG.K_MOVE_SE});
+        brain.decideNextAction({code: RG.K_YES});
+        expect(brain.energy).to.equal(RG.energy.ATTACK);
+
+    });
+
+    it('can toggle between fighting modes', function() {
+        var brain = new Brain.Player(player);
+        var fightMode = brain.getFightMode();
+        expect(fightMode).to.equal(RG.FMODE_NORMAL);
+        brain.decideNextAction({code: RG.K_FIGHT});
+        fightMode = brain.getFightMode();
+        expect(fightMode).to.equal(RG.FMODE_FAST);
+
+        brain.decideNextAction({code: RG.K_FIGHT});
+        fightMode = brain.getFightMode();
+        expect(fightMode).to.equal(RG.FMODE_SLOW);
+        brain.decideNextAction({code: RG.K_FIGHT});
+        fightMode = brain.getFightMode();
+        expect(fightMode).to.equal(RG.FMODE_NORMAL);
+
+    });
+
+    it('handles picking up of items', function() {
+        var brain = new Brain.Player(player);
+        var food = new RG.Item.Food('food');
+        var weapon = new RG.Item.Weapon('weapon');
+        // level.addItem(
+
+    });
 });
 
-describe('Basic functions of Rogue Brain', function() {
-    it('Has 1st priority for enemies', function() {
-        // var brain = new Brain.Rogue();
+describe('RG.Brain.Rogue', function() {
+
+    var level = null;
+    var player = null;
+    var demon = null;
+
+    beforeEach( () => {
+
+        level = RG.FACT.createLevel('arena', 10, 10);
+        player = new RG.Actor.Rogue('Player');
+        demon = new RG.Actor.Rogue('Demon');
+
+        demon.setType('demon');
+        demon.setBrain(new RG.Brain.Demon(demon));
+        demon.addEnemy(player);
+
+        player.setIsPlayer(true);
+        level.addActor(player, 1, 1);
+        level.addActor(demon, 1, 2);
+
     });
+
+    it('Has 1st priority for enemies', function() {
+        // var brain = demon.getBrain();
+        var cells = RG.Brain.getCellsAround(demon);
+        expect(cells).to.have.length(9);
+
+    });
+
 });
