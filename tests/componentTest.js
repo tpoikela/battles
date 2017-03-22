636eb471f794faa8e56e8b525483fb97b218084b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-22 23:05:13 +0100

    Added tests for base component. Added .travis.yml

---
+++ tests/componentTest.js
@@ -0,0 +1,40 @@
+
+var expect = require('chai').expect;
+var RG = require('../client/src/battles.js');
+
+describe('Component.Base', function() {
+
+    it('has exactly one related entity', function() {
+        var entity = new RG.Entity();
+        var entity2 = new RG.Entity();
+        var comp = new RG.Component.Base('Base');
+
+        expect(comp.getType()).to.equal('Base');
+
+        comp.setEntity(entity);
+        expect(comp.getEntity().getID()).to.equal(entity.getID());
+
+        // Try to override the entity
+        comp.setEntity(entity2);
+        expect(comp.getEntity().getID()).to.equal(entity.getID());
+
+        comp.setEntity(null);
+        expect(comp.getEntity()).to.be.null;
+
+    });
+
+    it('can be copied, cloned, compared', function() {
+        var comp = new RG.Component.Base('Base');
+
+        var compClone = comp.clone();
+        expect(comp.equals(compClone)).to.be.true;
+        expect(compClone.equals(comp)).to.be.true;
+
+        var compCopy = new RG.Component.Base('XXX');
+        compCopy.copy(comp);
+        expect(comp.equals(compCopy)).to.be.true;
+
+        expect(comp.toString()).to.match(/Base/);
+
+    });
+});
