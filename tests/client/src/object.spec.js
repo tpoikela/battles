3ba5daeab088ec332c712eeca75c9381a3f5c64b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-07 23:37:15 +0300

    Corrected require path. Changed var -> const.

--- tests/client/src/object.spec.js
+++ tests/client/src/object.spec.js
@@ -1,14 +1,13 @@
 
-var expect = require('chai').expect;
+const expect = require('chai').expect;
+const Obj = require('../../../client/src/object.js');
 
-var Obj = require('../client/src/object.js');
-
-var TypedObj = Obj.Typed;
+const TypedObj = Obj.Typed;
 
 describe('Defense Object', function() {
     it('Holds information related to attack/defense', function() {
-        var defObj = new Obj.Defense();
-        var defObj2 = new Obj.Defense();
+        const defObj = new Obj.Defense();
+        const defObj2 = new Obj.Defense();
         expect(defObj.equals(defObj2)).to.equal(true);
         defObj.setAttack(3);
         expect(defObj.equals(defObj2)).to.equal(false);
@@ -17,8 +16,8 @@ describe('Defense Object', function() {
 
 describe('Damage Object', function() {
     it('Contains info about damage', function() {
-        var dmg1 = new Obj.Damage();
-        var dmg2 = new Obj.Damage();
+        const dmg1 = new Obj.Damage();
+        const dmg2 = new Obj.Damage();
         expect(dmg2.equals(dmg1)).to.equal(true);
         dmg2.setDefense(9);
         expect(dmg2.equals(dmg1)).to.equal(false);
@@ -28,7 +27,7 @@ describe('Damage Object', function() {
 
 describe('TypedObject', function() {
     it('Stores type info about the objects', function() {
-        var obj = new TypedObj('actors', 'xxx');
+        const obj = new TypedObj('actors', 'xxx');
         expect(obj.getPropType()).to.equal('actors');
         expect(obj.getType()).to.equal('xxx');
     });
@@ -36,16 +35,16 @@ describe('TypedObject', function() {
 
 describe('Locatable Object', function() {
     it('Stores location information', function() {
-        var loc1 = new Obj.Locatable();
+        const loc1 = new Obj.Locatable();
         expect(loc1.getLevel()).to.equal(null);
 
-        var levelObj = {};
+        const levelObj = {};
 
         loc1.setLevel(levelObj);
         loc1.setXY(1, 2);
         expect(loc1.isLocated()).to.equal(true);
 
-        var loc2 = new Obj.Locatable();
+        const loc2 = new Obj.Locatable();
         loc2.setXY(1, 2);
         loc2.setLevel(levelObj);
         expect(loc1.isSamePos(loc2)).to.equal(true);
@@ -55,13 +54,13 @@ describe('Locatable Object', function() {
 
 describe('Ownable Object', function() {
     it('Asks Locatable for location information', function() {
-        var owner = new Obj.Locatable();
+        const owner = new Obj.Locatable();
         owner.setXY(1, 2);
-        var owned = new Obj.Ownable(owner);
+        const owned = new Obj.Ownable(owner);
         expect(owner.getX()).to.equal(1);
         expect(owner.getY()).to.equal(2);
 
-        var level = {id: 'ABCD'};
+        const level = {id: 'ABCD'};
         owner.setLevel(level);
         expect(owner.getLevel()).to.equal(level);
 
