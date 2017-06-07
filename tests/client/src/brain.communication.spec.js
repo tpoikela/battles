bc3b77d30d140b3d8d4dc2478665cbf6db773bcf
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-07 23:49:25 +0300

    Changed require paths. Changed var -> const|let.

--- tests/client/src/brain.communication.spec.js
+++ tests/client/src/brain.communication.spec.js
@@ -1,29 +1,29 @@
 
-var expect = require('chai').expect;
-var RG = require('../client/src/battles');
+const expect = require('chai').expect;
+const RG = require('../../../client/src/battles');
 
-var Memory = RG.Brain.Memory;
-var Brain = RG.Brain.Rogue;
+const Memory = RG.Brain.Memory;
+const Brain = RG.Brain.Rogue;
 
-RG.Game = require('../client/src/game.js');
+RG.Game = require('../../../client/src/game.js');
 
 /* Updates given systems in given order.*/
-var updateSystems = function(systems) {
-    for (var i = 0; i < systems.length; i++) {
+const updateSystems = function(systems) {
+    for (let i = 0; i < systems.length; i++) {
         systems[i].update();
     }
 };
 
 describe('How AI brain memory performs basic functions', function() {
-    var hunter = RG.FACT.createActor('hunter');
-    var brain = new Brain(hunter);
+    const hunter = RG.FACT.createActor('hunter');
+    const brain = new Brain(hunter);
     hunter.setBrain(brain);
 
-    var animal = RG.FACT.createActor('animal');
-    var beast = RG.FACT.createActor('beast');
+    const animal = RG.FACT.createActor('animal');
+    const beast = RG.FACT.createActor('beast');
 
     it('Keeps track of enemies', function() {
-        var memory = new Memory(brain);
+        const memory = new Memory(brain);
 
         expect(memory.isEnemy(animal)).to.equal(false);
         memory.addEnemy(animal);
@@ -36,7 +36,7 @@ describe('How AI brain memory performs basic functions', function() {
     });
 
     it('Keeps track of communications', function() {
-        var memory = new Memory(brain);
+        const memory = new Memory(brain);
 
         expect(memory.hasCommunicatedWith(animal)).to.equal(false);
         memory.addCommunicationWith(animal);
@@ -48,31 +48,31 @@ describe('How AI brain memory performs basic functions', function() {
 describe('How actors communicate with each other', function() {
 
     it('Passes info between actors via comm components', function() {
-        var comSys = new RG.System.Communication('Communication',
+        const comSys = new RG.System.Communication('Communication',
             ['Communication']);
-        var systems = [comSys];
+        const systems = [comSys];
 
-        var hunter1 = RG.FACT.createActor('hunter1');
-        var hunter2 = RG.FACT.createActor('hunter2');
+        const hunter1 = RG.FACT.createActor('hunter1');
+        const hunter2 = RG.FACT.createActor('hunter2');
 
-        var brain1 = new Brain(hunter1);
+        const brain1 = new Brain(hunter1);
         hunter1.setBrain(brain1);
 
-        var brain2 = new Brain(hunter2);
+        const brain2 = new Brain(hunter2);
         hunter2.setBrain(brain2);
 
-        var animal = RG.FACT.createActor('animal');
+        const animal = RG.FACT.createActor('animal');
 
         hunter1.addEnemy(animal);
-        var mem1 = brain1.getMemory();
+        const mem1 = brain1.getMemory();
 
-        var comComp = new RG.Component.Communication();
+        const comComp = new RG.Component.Communication();
         comComp.addMsg({type: 'Enemies', enemies: mem1.getEnemies()});
         expect(comSys.entities.hasOwnProperty(hunter2.getID())).to.equal(false);
         hunter2.add('Communication', comComp);
         expect(comSys.entities.hasOwnProperty(hunter2.getID())).to.equal(true);
 
-        var mem2 = brain2.getMemory();
+        const mem2 = brain2.getMemory();
         expect(mem2.isEnemy(animal)).to.equal(false);
 
         updateSystems(systems);
