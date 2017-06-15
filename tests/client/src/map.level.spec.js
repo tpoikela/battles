e339ebe4716b2c43e27761d46ef535b04aa54d4c
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-15 08:57:35 +0200

    Added unit tests for removing actors, items, stairs.

--- tests/client/src/map.level.spec.js
+++ tests/client/src/map.level.spec.js
@@ -42,15 +42,48 @@ describe('Map.Level', () => {
     it('has actors', () => {
         const level1 = RGTest.createLevel('arena', 20, 20);
         const actor = new Actor('actor');
-        level1.addActor(actor);
+        level1.addActor(actor, 2, 2);
+
+        let actors = level1.getActors();
+        expect(actors).to.have.length(1);
+        expect(actors[0].getID()).to.equal(actor.getID());
+
+        level1.removeActor(actor);
+        actors = level1.getActors();
+        expect(actors).to.have.length(0);
     });
 
     it('has items', () => {
         const level1 = RGTest.createLevel('arena', 20, 20);
+        const item1 = new Item('item1');
+        const item2 = new Item('item2');
+        expect(level1.addItem(item1, 2, 2)).to.be.true;
+        expect(level1.addItem(item2, 3, 3)).to.be.true;
+
+        let items = level1.getItems();
+        expect(items).to.have.length(2);
+        expect(items[0]).to.equal(item1);
+
+        expect(level1.removeItem(item1, 2, 2)).to.be.true;
+        expect(level1.removeItem(item1, 2, 2)).to.be.false;
+
+        items = level1.getItems();
+        expect(items).to.have.length(1);
+        expect(items[0]).to.equal(item2);
     });
 
     it('has stairs', () => {
         const level1 = RGTest.createLevel('arena', 20, 20);
+        const level2 = RGTest.createLevel('arena', 20, 20);
+        const stairs = new Stairs(true, level1, level2);
+        level1.addStairs(stairs, 5, 5);
+
+        let sList = level1.getStairs();
+        expect(sList).to.have.length(1);
+
+        // level1._removePropFromLevelXY(RG.TYPE_ELEM, stairs, 5, 5);
+        // sList = level1.getStairs();
+        // expect(sList).to.have.length(0);
     });
 
 });
