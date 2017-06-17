2dbcb8e6a8426667ae40098e2a57e94f8c059d19
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-17 13:27:32 +0200

    Added a unit test to check addComp effect.

--- tests/client/src/rg.effects.spec.js
+++ tests/client/src/rg.effects.spec.js
@@ -44,4 +44,57 @@ describe('RG.Effects', () => {
         const hpAfter = actor.get('Health').getHP();
         expect(hpAfter).to.be.above(hpBefore);
     });
+
+    it('has stun effect', () => {
+        const stunEffect = getEffectByName(Effects, 'stun');
+        const useEffect = getEffectByName(Effects, 'use');
+
+        const potion = new RG.Item.Potion('Stunning potion');
+        potion.useArgs = { };
+        potion.useArgs.duration = '12d1';
+
+        potion.use = useEffect.func.bind(potion);
+        const stunFunc = stunEffect.func.bind(potion);
+        potion.useFuncs = [];
+        potion.useFuncs.push(stunFunc);
+
+        const actor = new RG.Actor.Rogue('Healed one');
+        actor.getInvEq().addItem(potion);
+
+        const cell = new RG.Map.Cell();
+        cell.setProp('actors', actor);
+
+        expect(actor.has('Stun')).to.equal(false);
+        expect(actor.has('Expiration')).to.equal(false);
+        potion.use({target: cell});
+        expect(actor.has('Stun')).to.equal(true);
+        expect(actor.has('Expiration')).to.equal(true);
+    });
+
+    it('has effect to add any component for specific duration', () => {
+        const addCompEffect = getEffectByName(Effects, 'addComp');
+        const useEffect = getEffectByName(Effects, 'use');
+
+        const sword = new RG.Item.Weapon('Add comp');
+        sword.useArgs = { };
+        sword.useArgs.duration = '12d1';
+        sword.useArgs.name = 'Ethereal';
+
+        sword.use = useEffect.func.bind(sword);
+        const stunFunc = addCompEffect.func.bind(sword);
+        sword.useFuncs = [];
+        sword.useFuncs.push(stunFunc);
+
+        const actor = new RG.Actor.Rogue('Ethereal one');
+        actor.getInvEq().addItem(sword);
+
+        const cell = new RG.Map.Cell();
+        cell.setProp('actors', actor);
+
+        expect(actor.has('Ethereal')).to.equal(false);
+        expect(actor.has('Expiration')).to.equal(false);
+        sword.use({target: cell});
+        expect(actor.has('Ethereal')).to.equal(true);
+        expect(actor.has('Expiration')).to.equal(true);
+    });
 });
