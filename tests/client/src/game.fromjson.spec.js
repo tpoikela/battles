3df0c399ba0af48f0f6bca9f51080323d2f93b8f
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-21 21:02:07 +0200

    Added unit test with actor and item.

--- tests/client/src/game.fromjson.spec.js
+++ tests/client/src/game.fromjson.spec.js
@@ -31,7 +31,37 @@ describe('RG.Game.FromJSON', () => {
         const level = RGTest.createLevel('arena', 20, 20);
         const json = level.toJSON();
         const newLevel = fromJSON.createLevel(json);
-
         expect(newLevel.getID()).to.equal(level.getID());
+    });
+
+    it('Converts level.map JSON back to RG.Map', () => {
+        const level = RGTest.createLevel('arena', 20, 20);
+        const json = level.toJSON();
+        const newLevel = fromJSON.createLevel(json);
+        const newMap = newLevel.getMap();
+        for (let x = 0; x < 20; x++) {
+            expect(newMap.getCell(x, 0).isPassable(),
+                `Cell ${x},0 should have wall thus not passable`,
+            ).to.equal(false);
+        }
+    });
+
+    it('converts level and its objects into JSON and back to object', () => {
+        const level = RGTest.createLevel('arena', 100, 100);
+        const actor = new RG.Actor.Rogue('Urkh!');
+        actor.setType('goblin');
+        const item = new RG.Item.Weapon('sword');
+        level.addActor(actor, 2, 2);
+        level.addItem(item, 3, 3);
+        const json = level.toJSON();
+        console.log(JSON.stringify(json));
+        const newLevel = fromJSON.createLevel(json);
+
+        const actors = newLevel.getActors();
+        const items = newLevel.getItems();
+        expect(actors).to.have.length(1);
+        expect(actors[0].getName()).to.equal('Urkh!');
+        expect(items).to.have.length(1);
+        expect(items[0].getName()).to.equal('sword');
     });
 });
