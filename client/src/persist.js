dfb222452be1cd869bb7a7219e7d5ded6868cb20
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-27 22:38:21 +0300

    Reverted to previous version.

--- client/src/persist.js
+++ client/src/persist.js
@@ -3,9 +3,9 @@
 
 const IDB_VERSION = 1;
 
-function connectToIDB(indexedDB, name, version) {
+function connectToIDB(name, version) {
     return new Promise((resolve, reject) => {
-        const request = indexedDB.open(name, version);
+        const request = window.indexedDB.open(name, version);
         request.onupgradeneeded = () => {
             request.result.createObjectStore(name,
                 { keyPath: 'id', autoIncrement: true });
@@ -27,6 +27,7 @@ function operateOnIDB(name, IDB, operation, data) {
             case 'GET':
                 request = store.getAll();
                 request.onsuccess = () => {
+                    console.log('GET request.result length: ' + request.result.length);
                     return resolve(request.result);
                 };
                 request.onerror = () => reject(request.error);
@@ -48,12 +49,12 @@ function operateOnIDB(name, IDB, operation, data) {
     });
 }
 
-async function operateWithIDB(indexedDB, store, operation, data) {
+async function operateWithIDB(store, operation, data) {
     let IDB;
     try {
         console.log('Trying to operate with IDB');
-        IDB = await connectToIDB(indexedDB, store, IDB_VERSION);
-        return await operateOnIDB(indexedDB, store, IDB, operation, data);
+        IDB = await connectToIDB(store, IDB_VERSION);
+        return await operateOnIDB(store, IDB, operation, data);
     }
     catch (exception) {
         console.error(exception);
@@ -66,10 +67,8 @@ async function operateWithIDB(indexedDB, store, operation, data) {
     }
 }
 
-module.exports = function Persist(indexedDB, storeName) {
-    const _indexedDB = indexedDB;
-    this.fromStorage = () => operateWithIDB(_indexedDB, storeName, 'GET', null);
-    this.toStorage = data => operateWithIDB(_indexedDB, storeName, 'PUT', data);
-    this.deleteFromStorage = data =>
-        operateWithIDB(_indexedDB, storeName, 'DELETE', data);
+module.exports = function Persist(storeName) {
+    this.fromStorage = () => operateWithIDB(storeName, 'GET', null);
+    this.toStorage = data => operateWithIDB(storeName, 'PUT', data);
+    this.deleteFromStorage = data => operateWithIDB(storeName, 'DELETE', data);
 };
