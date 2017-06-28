2803d0a15aa8d4a8bb7fec13c73b7b559168be4a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-28 12:42:26 +0300

    Added a test for full game creation.

---
+++ tests/client/functional/game.create.test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const RG = require('../../../client/src/battles');
+
+const worldConf = require('../../../client/data/conf.world');
+
+describe('Function: Creating game world from a file', function() {
+    this.timeout(5000);
+    it('can create world from external config object', () => {
+        const fact = new RG.Factory.World();
+        const world = fact.createWorld(worldConf);
+        expect(world.getAreas()).to.have.length(worldConf.nAreas);
+        expect(world.getName()).to.equal(worldConf.name);
+        expect(world.getLevels()).to.have.length.above(0);
+    });
+});
