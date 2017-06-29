c01b0871375d2e0c6964d9a0a9aac973df7cd7af
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-29 08:32:25 +0200

    mapShown has a bug, removed it for the moment.

--- client/jsx/game-board.jsx
+++ client/jsx/game-board.jsx
@@ -79,11 +79,11 @@ const GameBoard = React.createClass({
         const map = this.props.map;
 
         let shownCells = map;
-        if (!mapShown) {
+        // if (!mapShown) {
             shownCells = new GUI.Viewport(this.props.viewportX,
                 this.props.viewportY, map);
             shownCells.getCellsInViewPort(playX, playY, map);
-        }
+        // }
 
         const cellRows = [];
         const charRows = [];
