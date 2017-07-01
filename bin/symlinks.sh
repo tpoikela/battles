104584959c7d2b292c096ec0a2d91059df2e5293
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-01 12:05:56 +0200

    Added bin/symlinks.sh.

---
+++ bin/symlinks.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+ln -s node_modules/eslint/bin/eslint.js ./eslint
+ln -s node_modules/browserify/bin/cmd.js ./browserify
+ln -s node_modules/sass-lint/bin/sass-lint.js ./sass-lint
+ln -s node_modules/babel-cli/bin/babel.js ./babel
