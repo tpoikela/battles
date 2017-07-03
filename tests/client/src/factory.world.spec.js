43807356a1fc02066a2c8fe65275e77c8958fd53
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-03 20:36:47 +0200

    Moved long taking test to functional from unit tests.

--- tests/client/src/factory.world.spec.js
+++ tests/client/src/factory.world.spec.js
@@ -6,7 +6,6 @@ const RGTest = require('../../roguetest');
 const expectConnected = RGTest.expectConnected;
 
 describe('Factory.World', function() {
-    this.timeout(3000);
     let fact = null;
 
     beforeEach(() => {
@@ -96,36 +95,6 @@ describe('Factory.World', function() {
         expect(entrB2.getTargetStairs()).to.be.null;
     });
 
-    it('Can create World using config object', function() {
-        const worldConf = {
-            name: 'w1',
-            nAreas: 2,
-            area: [
-                { name: 'a1', maxX: 2, maxY: 3, nDungeons: 1,
-                    dungeon: [
-                        {x: 0, y: 0, name: 'd1.1', nBranches: 1,
-                            branch: [ { name: 'b1', nLevels: 2,
-                                entranceLevel: 0}]
-                        }
-                    ]
-                },
-                { name: 'a2', maxX: 1, maxY: 4, nMountains: 1,
-                    mountain: [{x: 0, y: 1, name: 'm2.1', nFaces: 1,
-                        face: [{name: 'Steep', nLevels: 1, x: 10, y: 10}]
-                    }]
-                }
-            ]
-        };
-        const world = fact.createWorld(worldConf);
-        expect(world.getName()).to.equal('w1');
-        expect(world.getAreas()).to.have.length(2);
-        expect(world.getAreas()[0].getDungeons()).to.have.length(1);
-        expect(world.getDungeons(), 'Found 1 dungeon').to.have.length(1);
-        expect(world.getMountains(), 'Found 1 mountain').to.have.length(1);
-
-        expect(world.getDungeons()[0].getName(),
-            'Dungeon name OK.').to.equal('d1.1');
-    });
 
     it('can create cities within areas', () => {
         const worldConf = {
