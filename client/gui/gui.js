5aa40f9dfb51e09beeb2486c4f18e8f1e7bd7fea
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-08 23:49:40 +0300

    Fixed linting errors. Accesses map._map now instead of calling getCell.

--- client/gui/gui.js
+++ client/gui/gui.js
@@ -4,60 +4,72 @@ var $DEBUG = 0;
 
 function debug(msg) {
     if ($DEBUG) {
-        console.log("DEBUG:" + msg);
+        console.log('DEBUG:' + msg);
     }
 }
 
-var GUI = {};
+const GUI = {};
 
-/** Object which manages the shown part of the level.*/
-GUI.Viewport = function(viewportX, viewportY, map) {
+/* Object which manages the shown part of the level.*/
+GUI.Viewport = function(viewportX, viewportY) {
 
     // Size of the viewport, feel free to adjust
     this.viewportX = viewportX;
     this.viewportY = viewportY;
 
-    /** Returns an object containing all cells in viewport, and viewport
+    /* Returns an object containing all cells in viewport, and viewport
      * coordinates.
      */
     this.getCellsInViewPort = function(x, y, map) {
-        var startX = x - this.viewportX;
-        var endX = x + this.viewportX;
-        var startY = y - this.viewportY;
-        var endY = y + this.viewportY;
-        var maxX = map.cols - 1;
-        var maxY = map.rows - 1;
+        let startX = x - this.viewportX;
+        let endX = x + this.viewportX;
+        let startY = y - this.viewportY;
+        let endY = y + this.viewportY;
+        const maxX = map.cols - 1;
+        const maxY = map.rows - 1;
 
         // If player is too close to level edge, viewport must be expanded from
         // the other side.
-        var leftStartX = this.viewportX - x;
+        const leftStartX = this.viewportX - x;
         if (leftStartX > 0) {
             endX += leftStartX;
         }
         else {
-            var leftEndX = x + this.viewportX - maxX;
-            if (leftEndX > 0) startX -= leftEndX;
+            const leftEndX = x + this.viewportX - maxX;
+            if (leftEndX > 0) {
+                startX -= leftEndX;
+            }
         }
 
-        var leftStartY = this.viewportY - y;
+        const leftStartY = this.viewportY - y;
         if (leftStartY > 0) {
             endY += leftStartY;
         }
         else {
-            var leftEndY = y + this.viewportY - maxY;
-            if (leftEndY > 0) startY -= leftEndY;
+            const leftEndY = y + this.viewportY - maxY;
+            if (leftEndY > 0) {
+                startY -= leftEndY;
+            }
         }
 
         // Some sanity checks for level edges
-        if (startX < 0) startX = 0;
-        if (startY < 0) startY = 0;
-        if (endX > map.cols-1) endX = map.cols - 1;
-        if (endY > map.rows-1) endY = map.rows - 1;
+        if (startX < 0) {
+            startX = 0;
+        }
+        if (startY < 0) {
+            startY = 0;
+        }
+        if (endX > map.cols - 1) {
+            endX = map.cols - 1;
+        }
+        if (endY > map.rows - 1) {
+            endY = map.rows - 1;
+        }
 
-        for (var yy = startY; yy <= endY; yy++) {
+        for (let yy = startY; yy <= endY; yy++) {
             this[yy] = [];
-            for (var xx = startX; xx <= endX; xx++) {
-                this[yy].push(map.getCell(xx, yy));
+            for (let xx = startX; xx <= endX; xx++) {
+                this[yy].push(map._map[xx][yy]);
             }
         }
 
@@ -66,7 +78,7 @@ GUI.Viewport = function(viewportX, viewportY, map) {
         this.startY = startY;
         this.endY = endY;
         this.rows = map.rows;
-    },
+    };
 
     this.getCellRow = function(y) {return this[y];};
 
