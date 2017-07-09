22f83be8c30afea488348de575baab6ca13e0a7a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-09 22:37:25 +0300

    Added a test to check that the viewport works correctly.

--- tests/client/gui/screen.spec.js
+++ tests/client/gui/screen.spec.js
@@ -7,7 +7,7 @@ const wallChar = '#';
 const wallClass = 'cell-element-wall';
 
 describe('GUI.Screen', () => {
-    it('', () => {
+    it('Returns ASCII chars and CSS classes for the level', () => {
         const level = RG.FACT.createLevel('arena', 10, 10);
         const map = level.getMap();
         const screen = new Screen(10, 10);
@@ -28,6 +28,25 @@ describe('GUI.Screen', () => {
         classes[0].forEach(cell => {
             expect(cell).to.equal(wallClass);
         });
+    });
+
+    it('uses a viewport to show only part of the level', () => {
+        const level = RG.FACT.createLevel('arena', 40, 40);
+        const map = level.getMap();
+        const screen = new Screen(20, 20);
+        screen.setViewportXY(5, 5);
+        const actor = new RG.Actor.Rogue('rogue');
+        actor.setIsPlayer(true);
+        actor.setFOVRange(10);
+        level.addActor(actor, 1, 1);
+        const visibleCells = level.exploreCells(actor);
+        screen.render(1, 1, map, visibleCells);
+
+        const chars = screen.getCharRows();
+        const classes = screen.getClassRows();
+
+        expect(chars).to.have.length(11);
+        expect(classes).to.have.length(11);
 
     });
 });
