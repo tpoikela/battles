e899feb6e42d1d3468e6673202de70f9ee517a50
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-12 01:15:24 +0300

    Added unit tests for Selected and Sequence nodes.

--- tests/client/src/aisequence.spec.js
+++ tests/client/src/aisequence.spec.js
@@ -3,9 +3,8 @@ const expect = require('chai').expect;
 const BTree = require('../../../client/src/aisequence');
 
 describe('BTree', () => {
-    it('Has Selector nodes', () => {
+    it('should return function sequence', () => {
         const actor = {};
-
         const setActorName = (actor) => {
             return () => {
                 actor.isNameSet = true;
@@ -16,14 +15,34 @@ describe('BTree', () => {
             [setActorName]
         );
 
-        const setFunc = BTree.execBehavTree(behavTree, actor);
-        expect(setFunc).to.be.function;
+        const funcs = BTree.startBehavTree(behavTree, actor);
+        expect(funcs).to.have.length(1);
+        expect(funcs[0]).to.be.function;
         expect(actor.isNameSet).to.be.empty;
 
-        /*
+        const setFunc = funcs[0];
         setFunc();
         expect(actor.isNameSet).to.be.true;
-        */
+    });
+
+    it('should have selections', () => {
+        const actor = {
+            hp: 15
+        };
+
+        const condFunc = (actor) => (actor.hp >= 10);
+        const ifTrueFunc = (actor) => () => {actor.hp *= 2;};
+        const ifFalseFunc = (actor) => {actor.hp /= 2;};
+
+        const behavTree = new BTree.SelectorNode(
+            condFunc, ifTrueFunc, ifFalseFunc
+        );
+
+        expect(actor.hp).to.equal(15);
+        const funcs = BTree.startBehavTree(behavTree, actor);
+        expect(funcs).to.have.length(1);
+        funcs[0]();
+        expect(actor.hp).to.equal(2 * 15);
     });
 
 });
