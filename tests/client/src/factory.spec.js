b9543119ae1ae6e3dfe3cdc9e029f1f43388c734
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-14 19:33:09 +0200

    Added a test to check that preset levels are inserted into the world correctly.

--- tests/client/src/factory.spec.js
+++ tests/client/src/factory.spec.js
@@ -3,6 +3,7 @@ const expect = require('chai').expect;
 const RG = require('../../../client/src/battles');
 
 const ItemRand = RG.Factory.ItemRandomizer;
+const temple = require('../../../client/data/temple.json');
 
 describe('RG.Factory.ItemRandomizer', function() {
     it('Randomizes item properties for proc generation', function() {
@@ -47,11 +48,12 @@ describe('RG.Factory.Base', function() {
     });
 });
 
-
 describe('RG.Factory.Game', function() {
-    it('can create new games', function() {
-        const gameFactory = new RG.Factory.Game();
-        const conf = {
+
+    let conf = null;
+
+    beforeEach(() => {
+        conf = {
             cols: 40,
             rows: 30,
             levels: 2,
@@ -63,9 +65,52 @@ describe('RG.Factory.Game', function() {
             loadedLevel: null,
             playerName: 'Player Hero'
         };
+    });
+
+
+    it('can create new games', function() {
+        const gameFactory = new RG.Factory.Game();
         const game = gameFactory.createNewGame(conf);
 
         expect(game).to.exist;
         expect(game.getPlayer().getName()).to.equal('Player Hero');
     });
+
+    it('can create new games with preset levels', () => {
+        const gameFactory = new RG.Factory.Game();
+        const worldConf = {
+            name: 'PresetLevelWorld',
+            presetLevels: {
+                'Beast dungeon.Animals': [{nLevel: 0, level: temple}]
+            },
+            nAreas: 1,
+            area: [
+                {
+                    name: 'Ravendark',
+                    maxX: 1,
+                    maxY: 1,
+                    cols: 70, rows: 30,
+                    nDungeons: 1,
+                    dungeon: [
+                        { x: 0, y: 0, name: 'Beast dungeon', nBranches: 1,
+                            branch: [
+                                {name: 'Animals', nLevels: 2, entranceLevel: 0}
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+        conf.world = worldConf;
+        conf.debugMode = 'World';
+        const game = gameFactory.createNewGame(conf);
+        expect(game).to.exist;
+        expect(game.getLevels()).to.have.length(3);
+
+        const presetLevel = game.getLevels().find(level => (
+            level.getID() === temple.id
+        ));
+        expect(presetLevel).to.exist;
+    });
+
 });
