ea6ad0d16cc9d125041aaf0288127e160764f718
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-21 23:17:10 +0300

    Added a test to check that world.creator produces config that is compatible with Factory.World.

---
+++ tests/client/functional/full-creator.test.js
@@ -0,0 +1,20 @@
+
+const expect = require('chai').expect;
+const RG = require('../../../client/src/battles');
+
+const Creator = require('../../../client/src/world.creator');
+
+describe('Creator + Factory', () => {
+    it('description', () => {
+        const creator = new Creator();
+        const worldConf = creator.createWorldConf({name: 'The North'});
+
+        console.log(JSON.stringify(worldConf, null, '  '));
+
+        const worldFact = new RG.Factory.World();
+        const world = worldFact.createWorld(worldConf);
+
+        expect(world.getName()).to.equal('The North');
+
+    });
+});
