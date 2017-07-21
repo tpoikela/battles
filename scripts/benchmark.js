ec20c42629ea0efbdcaedae3ab3052d1089fd3f0
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-21 15:19:28 +0300

    Added vars x,y for the screen size.

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,15 +1,18 @@
 /* A short script to check the performance of Screen.render. */
 
-const RG = require('./client/src/battles');
-const Screen = require('./client/gui/screen');
+const RG = require('../client/src/battles');
+const Screen = require('../client/gui/screen');
 
 let numRenders = 0;
 
 const startTime = new Date().getTime();
 
-const level = RG.FACT.createLevel('arena', 80, 25);
+const x = 80;
+const y = 28;
+
+const level = RG.FACT.createLevel('arena', x, y);
 const map = level.getMap();
-const screen = new Screen(80, 25);
+const screen = new Screen(x, y);
 const actor = new RG.Actor.Rogue('rogue');
 actor.setIsPlayer(true);
 actor.setFOVRange(10);
@@ -17,7 +20,7 @@ level.addActor(actor, 1, 1);
 
 for (let i = 0; i < 5000; i++) {
     const visibleCells = level.exploreCells(actor);
-    screen.render(1, 1, map, visibleCells);
+    screen.render(i % x, i % y, map, visibleCells);
 
     const chars = screen.getCharRows();
     const classes = screen.getClassRows();
@@ -25,7 +28,7 @@ for (let i = 0; i < 5000; i++) {
 }
 
 const endTime = new Date().getTime();
-const dur = endTime - startTime;
-const fps = numRenders / dur * 1000;
+const durationMs = endTime - startTime;
+const fps = numRenders / durationMs * 1000;
 
 console.log('FPS: ' + fps);
