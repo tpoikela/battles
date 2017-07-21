f1f7397483a90e1387a38b8e8cc05807fed39530
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-21 22:32:40 +0300

    Added a test to check that city config is created properly.

--- tests/client/src/world.creator.spec.js
+++ tests/client/src/world.creator.spec.js
@@ -27,8 +27,20 @@ describe('World.Creator', () => {
         expect(conf2).to.have.property('area');
 
         const areas = conf2.area;
-
         expect(areas[0].dungeon).to.have.length.above(0);
+    });
 
+    it('adds cities to the created configuration', () => {
+        const creator = new Creator();
+        const conf = creator.createWorldConf({name: 'My World'});
+
+        const area0 = conf.area[0];
+
+        expect(area0.nCities).to.be.above(0);
+
+        console.log(JSON.stringify(area0));
+
+        const city0 = area0.city[0];
+        expect(city0.quarter).to.have.length.above(0);
     });
 });
