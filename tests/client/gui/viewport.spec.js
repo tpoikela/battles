25c167dbaf4196be358e97e311e8fc7b3ec8e7c6
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-26 10:35:17 +0300

    Added 2 tests cases for the viewport.

--- tests/client/gui/viewport.spec.js
+++ tests/client/gui/viewport.spec.js
@@ -12,8 +12,20 @@ describe('Viewport', () => {
         const viewport = new Viewport(2, 2);
         viewport.getCellsInViewPort(0, 0, map);
 
-        const keys = Object.keys(viewport.coord);
+        let keys = Object.keys(viewport.coord);
         expect(keys).to.have.length(5);
-        expect(keys).to.deep.equal([0, 1, 2, 3, 4]);
+        expect(keys).to.deep.equal(['0', '1', '2', '3', '4']);
+
+        viewport.setViewportXY(3, 3);
+        viewport.getCellsInViewPort(1, 1, map);
+        keys = Object.keys(viewport.coord);
+        expect(keys).to.have.length(7);
+        expect(keys).to.deep.equal(['0', '1', '2', '3', '4', '5', '6']);
+
+        viewport.setViewportXY(1, 1);
+        viewport.getCellsInViewPort(9, 9, map);
+        keys = Object.keys(viewport.coord);
+        expect(keys).to.have.length(3);
+        expect(keys).to.deep.equal(['7', '8', '9']);
     });
 });
