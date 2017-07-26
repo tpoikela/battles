c11459f6a767f9bbb716bc8c47b1640b1b7deb1c
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-26 10:26:11 +0300

    Added whyDidYouUpdate.

--- client/jsx/app.jsx
+++ client/jsx/app.jsx
@@ -3,8 +3,21 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const BattlesTop = require('./top.jsx');
 
+const whyDidYouUpdate = require('why-did-you-update').whyDidYouUpdate;
+
 ReactDOM.render(
     <BattlesTop />,
     document.getElementById('mount-point')
 );
 
+if (process.env.NODE_ENV !== 'production') {
+	/* eslint-disable */
+	let createClass = React.createClass;
+    Object.defineProperty(React, 'createClass', {
+      set: (nextCreateClass) => {
+        createClass = nextCreateClass;
+      }
+    });
+	const conf = {};
+    whyDidYouUpdate(React, conf);
+}
