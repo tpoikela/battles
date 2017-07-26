25be3048d676a84ecdf9843f02d979bf8fa95fb1
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-26 10:30:13 +0300

    Rewrote as functional component.

--- client/jsx/modal-header.jsx
+++ client/jsx/modal-header.jsx
@@ -3,27 +3,19 @@
 const React = require('react');
 
 /* Header component for modals.*/
-class ModalHeader extends React.Component {
-
-    render() {
-        var id = this.props.id;
-        var text = this.props.text;
-        return (
-            <div className='modal-header'>
-                <button
-                    aria-label='Close'
-                    className='close'
-                    data-dismiss='modal'
-                    type='button'
-                    >
-                    <span aria-hidden='true'>&times;</span>
-                </button>
-                <h4 className='modal-title' id={id}>{text}</h4>
-            </div>
-        );
-    }
-
-}
+const ModalHeader = props => (
+    <div className='modal-header'>
+        <button
+            aria-label='Close'
+            className='close'
+            data-dismiss='modal'
+            type='button'
+        >
+            <span aria-hidden='true'>&times;</span>
+        </button>
+        <h4 className='modal-title' id={props.id}>{props.text}</h4>
+    </div>
+);
 
 ModalHeader.propTypes = {
     id: React.PropTypes.string,
