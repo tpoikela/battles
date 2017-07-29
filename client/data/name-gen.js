0a6a85f4d1c432c9b789907f7287e8559139df55
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-29 22:39:39 +0200

    Added a file for name generation.

---
+++ client/data/name-gen.js
@@ -0,0 +1,57 @@
+/* eslint comma-dangle: 0 */
+
+// Thanks to www.fantasynamegenerators.com for some tips
+
+const Names = {};
+
+Names.place = {
+    dungeon: [
+        'Crypt', 'Catacombs', 'Tombs',
+        'Dungeon', 'Cave', 'Grotto', 'Cavern',
+        'Burrows',
+        'Delves',
+        'Haunt', 'Point',
+        'Vault', 'Lair', 'Tunnels', 'Cells', 'Pits',
+        'Labyrinth', 'Maze'
+    ],
+
+    mountain: [
+        'Summit', 'Volcano', 'Tops', 'Peaks',
+        'Bluff', 'Highlands', 'Pinnacle', 'Rise', 'Needle', 'Hills',
+        'Slopes'
+    ],
+
+    face: [
+        'Face', 'Buttress', 'Ridge', 'Shoulder'
+    ],
+
+    forest: [
+        'Grove', 'Wilds', 'Woodlands', 'Timberland', 'Forest', 'Covert',
+        'Woods', 'Thicket', 'Glade',
+    ],
+
+    city: [
+
+    ],
+
+    lake: [
+        'Basin', 'Cove', 'Reservoir', 'Depths', 'Gorge', 'Lagoon', 'Domain',
+        'Pond', 'Expanse', 'Lake', 'Shallows', 'Loch', 'Falls', 'Rapids',
+    ],
+
+    quarter: [
+        'Plaza', 'Garden', 'Row', 'Works', 'Side', 'Market', 'Acre', 'Bazaar',
+    ],
+
+};
+
+Names.actor = {
+
+};
+
+Names.item = {
+
+};
+
+module.exports = Names;
+
