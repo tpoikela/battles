5b5d98b65af43f40c35a4839eed8e31e8f17b113
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-02 13:55:29 +0200

    Refactored in-game items/actors into their own files.

---
+++ client/data/commands.js
@@ -0,0 +1,25 @@
+
+const RG = require('../src/rg');
+
+// Contains all commands player can execute
+RG.Commands = {
+
+    drop: {
+
+    },
+
+    equip: {
+
+    },
+
+    missile: {
+
+    },
+
+    use: {
+
+    }
+
+};
+
+module.exports = RG.Commands;
