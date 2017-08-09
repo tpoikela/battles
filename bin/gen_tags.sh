7fd986e7bd463ca8e50b1943d307eaee1e9cb639
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-09 10:35:40 +0300

    Adds now jsx files to the tags file.

--- bin/gen_tags.sh
+++ bin/gen_tags.sh
@@ -1,2 +1,7 @@
 #! /bin/sh
-find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./jsctags {} -f \; | sed '/^$/d' | sort > tags
+find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./jsctags {} -f \; | sed '/^$/d' > tags_temp
+#find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./jsctags {} -f \; | sed '/^$/d' | sort > tags_temp
+ctags -R ./client/jsx
+cat tags >> tags_temp
+cat tags_temp | sort >> tags
+rm tags_temp
