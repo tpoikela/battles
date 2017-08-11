d737df3ee0e4f4811641c62888a73ba73efcc4ed
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-11 21:28:51 +0200

    Added Cell.isAtXY().

--- client/src/map.js
+++ client/src/map.js
@@ -27,6 +27,9 @@ RG.Map.Cell.prototype.getX = function() {return this._x;};
 RG.Map.Cell.prototype.getY = function() {return this._y;};
 RG.Map.Cell.prototype.setX = function(x) {this._x = x;};
 RG.Map.Cell.prototype.setY = function(y) {this._y = y;};
+RG.Map.Cell.prototype.isAtXY = function(x, y) {
+    return x === this._x && y === this._y;
+};
 
 /* Sets/gets the base element for this cell. There can be only one element.*/
 RG.Map.Cell.prototype.setBaseElem = function(elem) { this._baseElem = elem; };
