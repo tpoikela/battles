2051ff72153a95b7564bb31c75b0366acf5c6fe6
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-12 12:00:18 +0200

    Renamed factor->slowDown. Added few comments how the animation object works.

--- client/src/animation.js
+++ client/src/animation.js
@@ -3,24 +3,34 @@ const RG = require('./rg');
 
 RG.Animation = {};
 
+/* Very simple "animation" object handed to the Screen object. slowDown can be
+ * used to repeat the frame for addFrame() multiple times.
+ *  Each frame should be an object such as
+ *  {
+ *    '1,1': {char: '-', className: 'css-class'},
+ *    '2,2': {char: '-', className: 'css-class'}
+ *  }
+ * Screen will always render specified chars to the given locations over normal
+ * map cells.
+ * */
 RG.Animation.Animation = function() {
 
     this.numFrames = 0;
     this.currFrame = 0;
 
     // "Slows" down the animation by this factor
-    this.factor = 2;
+    this.slowDown = 2;
 
     this.frames = [];
 
     this.addFrame = function(frame) {
-        for (let i = 0; i < this.factor; i++) {
+        for (let i = 0; i < this.slowDown; i++) {
             ++this.numFrames;
             this.frames.push(frame);
         }
     };
 
-    /* Advances animation to the next frame. */
+    /* Advances animation to the next frame, and returns the frame */
     this.nextFrame = function() {
         const frame = this.frames[this.currFrame++];
         return frame;
