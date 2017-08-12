cbc17ad5862056369afbfe00efad1db318eaf4b7
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-12 17:20:15 +0200

    Added verify.js.

---
+++ client/src/verify.js
@@ -0,0 +1,33 @@
+
+const RG = require('./rg');
+
+/* This file contains verification functions for game logic. */
+
+RG.Verify = {};
+
+RG.Verify.Conf = function(objName) {
+    const _name = objName;
+
+    /* Verifies that configuration contains all required keys.*/
+    this.verifyConf = function(funcName, conf, required) {
+        let ok = true;
+        let errorMsg = '';
+        required.forEach(req => {
+            if (!conf.hasOwnProperty(req)) {
+                ok = false;
+                errorMsg += ` Missing: ${req}`;
+            }
+            else if (RG.isNullOrUndef([conf[req]])) {
+                ok = false;
+                errorMsg += ` Undef/null value in: ${req}`;
+            }
+        });
+        if (!ok) {
+            RG.err(_name, 'verifyConf', `${funcName} ${errorMsg}`);
+        }
+        return ok;
+    };
+
+};
+
+module.exports = RG.Verify;
