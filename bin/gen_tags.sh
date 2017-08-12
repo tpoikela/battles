ac530ab74d8b3b811444d66bcc50515f8803c0a9
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-12 17:16:07 +0200

    removed concat at the end.

--- bin/gen_tags.sh
+++ bin/gen_tags.sh
@@ -3,5 +3,5 @@ find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./js
 #find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./jsctags {} -f \; | sed '/^$/d' | sort > tags_temp
 ctags -R ./client/jsx
 cat tags >> tags_temp
-cat tags_temp | sort >> tags
+cat tags_temp | sort > tags
 rm tags_temp
