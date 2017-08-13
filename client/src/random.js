a3b5779a04b8f3c975c7c395c8e05c53d64a9b23
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-13 09:25:00 +0200

    Moved shuffle into random.js.

--- client/src/random.js
+++ client/src/random.js
@@ -65,6 +65,29 @@ RG.Random.prototype.toJSON = function() {
     };
 };
 
+/*
+ * From http://stackoverflow.com/questions/2450954/
+ * how-to-randomize-shuffle-a-javascript-array
+ */
+RG.Random.prototype.shuffle = function(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = this.getUniformInt(0, currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+};
+
 // Global RNG
 RG.RAND = new RG.Random();
 
