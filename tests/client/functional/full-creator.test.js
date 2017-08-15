8acdeb8e3e862a9561d5b29c94f0351d97e2a4e4
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-15 22:55:38 +0200

    Corrected the require().

--- tests/client/functional/full-creator.test.js
+++ tests/client/functional/full-creator.test.js
@@ -2,18 +2,18 @@
 const expect = require('chai').expect;
 const RG = require('../../../client/src/battles');
 
-const Creator = require('../../../client/src/world.creator');
+const WorldConf = require('../../../client/src/world.creator');
 
 describe('Creator + Factory', function() {
     this.timeout(60000);
-    it('description', () => {
+    it('creates a full randomized world', () => {
 
         const conf = {
             name: 'My World',
             worldSize: 'Small',
             areaSize: 'Small'
         };
-        const creator = new Creator();
+        const creator = new WorldConf.Creator();
         const worldConf = creator.createWorldConf(conf);
 
         const worldFact = new RG.Factory.World();
