60b3724064b0676b3cd7b77bc6185fbe48fa3c8c
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-15 22:45:47 +0200

    Corrected failing tests due to changed module.exports.

--- tests/client/src/world.creator.spec.js
+++ tests/client/src/world.creator.spec.js
@@ -2,13 +2,13 @@
 const expect = require('chai').expect;
 // const RG = require('../../../client/src/battles');
 
-const Creator = require('../../../client/src/world.creator');
+const WorldConf = require('../../../client/src/world.creator');
 
 describe('World.Creator', () => {
     it('can create a configuration to build the game world', () => {
         // Of course creator which creates the configuration needs to have its
         // own configuration. Maybe there will be World.Creator.ConfCreator?
-        const creator = new Creator();
+        const creator = new WorldConf.Creator();
 
         const worldConf = creator.createWorldConf({name: 'Westeros'});
         expect(worldConf.name).to.equal('Westeros');
@@ -31,7 +31,7 @@ describe('World.Creator', () => {
     });
 
     it('adds cities to the created configuration', () => {
-        const creator = new Creator();
+        const creator = new WorldConf.Creator();
         const conf = creator.createWorldConf({name: 'My World'});
         const area0 = conf.area[0];
         expect(area0.nCities).to.be.above(0);
@@ -41,7 +41,7 @@ describe('World.Creator', () => {
     });
 
     it('adds mountains to the created configuration', () => {
-        const creator = new Creator();
+        const creator = new WorldConf.Creator();
         const conf = creator.createWorldConf({name: 'My World'});
         const area0 = conf.area[0];
         expect(area0.nMountains).to.be.above(0);
