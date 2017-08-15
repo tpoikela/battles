2ed427db4ed06b32d11e3b7c19b2aa531675ce9b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-15 23:55:51 +0200

    Added a test for the factory function.

--- tests/client/src/overworld.map.spec.js
+++ tests/client/src/overworld.map.spec.js
@@ -1,11 +1,24 @@
 
 const expect = require('chai').expect;
-const RG = require('../../../client/src/battles');
+const OW = require('../../../client/src/overworld.map');
 
-describe('RG.OverWorld', () => {
+describe('OW.Map', () => {
     it('can be created', () => {
-        const ow = new RG.OverWorld.Map();
+        const ow = new OW.Map();
         expect(ow).to.exist;
+    });
+
+    it('can be created with factory function', () => {
+        const conf = {
+            owTilesX: 40,
+            owTilesY: 20,
+            printResult: false
+        };
+        const overworld = OW.createOverWorld(conf);
+        const map = overworld.getMap();
+
+        expect(map).to.have.length(40);
+        expect(map[0]).to.have.length(20);
 
     });
 });
