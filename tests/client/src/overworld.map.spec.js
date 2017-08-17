a1d7139b45b3097f089d0ed0dd756b43ac1b7818
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-17 10:48:42 +0200

    Added unit test for checking features and biomes.

--- tests/client/src/overworld.map.spec.js
+++ tests/client/src/overworld.map.spec.js
@@ -11,14 +11,24 @@ describe('OW.Map', () => {
     it('can be created with factory function', () => {
         const conf = {
             owTilesX: 40,
-            owTilesY: 20,
-            printResult: false
+            owTilesY: 20
         };
         const overworld = OW.createOverWorld(conf);
         const map = overworld.getMap();
 
         expect(map).to.have.length(40);
         expect(map[0]).to.have.length(20);
+    });
 
+    it('has biomes and features added', () => {
+        const conf = {
+            owTilesX: 20,
+            owTilesY: 10
+        };
+        const ow = OW.createOverWorld(conf);
+        expect(ow.getBiome(0, 1)).to.not.be.empty;
+
+        const features = ow.getFeaturesByType(OW.WCAPITAL);
+        expect(features).to.have.length(1);
     });
 });
