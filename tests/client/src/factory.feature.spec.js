dad74b3680b3cb245689a82b9e5e27f69ae7a1ae
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-17 14:24:32 +0200

    Added unit tests for Factory.Feature.

---
+++ tests/client/src/factory.feature.spec.js
@@ -0,0 +1,32 @@
+
+const RG = require('../../../client/src/battles');
+const expect = require('chai').expect;
+
+describe('Factory.Feature', () => {
+    it('can create village levels', () => {
+        const fact = new RG.Factory.Feature();
+        const villageConf = {
+            x: 80, y: 28,
+            cityType: 'Village',
+            groupType: 'village',
+            quarterType: ''
+        };
+
+        const village = fact.createCityLevel(villageConf);
+        expect(village).to.exist;
+        expect(village.getActors()).to.have.length.above(5);
+    });
+
+    it('can create the capital level', () => {
+        const fact = new RG.Factory.Feature();
+        const capitalConf = {
+            x: 100, y: 100,
+            cityType: 'Capital',
+            groupType: 'capital',
+            quarterType: ''
+        };
+        const capital = fact.createCityLevel(capitalConf);
+        expect(capital).to.exist;
+        expect(capital.getActors()).to.have.length.above(5);
+    });
+});
