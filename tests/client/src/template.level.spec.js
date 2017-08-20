babb5f0321bde2cea76891f407352d96976975f3
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-20 15:16:11 +0300

    Changed the room count.

--- tests/client/src/template.level.spec.js
+++ tests/client/src/template.level.spec.js
@@ -5,11 +5,12 @@ const TemplLevel = require('../../../client/src/template.level');
 
 describe('Template.Level', () => {
     it('creates a 2-d map of the level', () => {
-        const level = new TemplLevel(10, 10);
+        const level = new TemplLevel(10, 7);
 
         RG.RAND.setSeed(new Date().getTime());
 
-        level.setGenParams([1, 2, 3, 4]);
+        level.setGenParams([1, 2, 1, 1]);
+        level.setRoomCount(30);
         level.create();
         expect(Array.isArray(level.map)).to.be.true;
 
