fcf576d63ad1bad74881729113b6df66735a4691
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-20 15:57:56 +0300

    Added new unit test.

--- tests/client/src/template.level.spec.js
+++ tests/client/src/template.level.spec.js
@@ -3,6 +3,8 @@ const RG = require('../../../client/src/battles');
 const expect = require('chai').expect;
 const TemplLevel = require('../../../client/src/template.level');
 
+const Castle = require('../../../client/data/tiles.castle');
+
 describe('Template.Level', () => {
     it('creates a 2-d map of the level', () => {
         const level = new TemplLevel(10, 7);
@@ -14,8 +16,24 @@ describe('Template.Level', () => {
         level.create();
         expect(Array.isArray(level.map)).to.be.true;
 
-        console.log(JSON.stringify(level.map));
+        // console.log(JSON.stringify(level.map));
         RG.printMap(level.map);
 
     });
+
+    /*
+    it('can create 2-d castles', () => {
+        const level = new TemplLevel(10, 7);
+        RG.RAND.setSeed(new Date().getTime());
+
+        level.setFiller(Castle.filler);
+        level.setTemplates(Castle.templates);
+        level.setConstraintFunc(Castle.constraintFunc);
+        level.setGenParams([1, 2, 1, 1]);
+        level.setRoomCount(30);
+        level.create();
+
+        RG.printMap(level.map);
+    });
+    */
 });
