f9b0acc3e53e42bebf8c012aa3dd2d85531afd41
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-28 23:09:10 +0200

    Added serial-mocha.

--- bin/symlinks.sh
+++ bin/symlinks.sh
@@ -4,3 +4,5 @@ ln -s node_modules/eslint/bin/eslint.js ./eslint
 ln -s node_modules/browserify/bin/cmd.js ./browserify
 ln -s node_modules/sass-lint/bin/sass-lint.js ./sass-lint
 ln -s node_modules/babel-cli/bin/babel.js ./babel
+ln -s node_modules/serial-mocha/bin/cli.js ./serial-mocha
+
