47c72eb3d9f386ddc03f21452dde3d1bc7e7c804
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-08-28 23:10:37 +0200

    Added unit tests for overworld.

---
+++ tests/client/src/overworld.spec.js
@@ -0,0 +1,21 @@
+
+const expect = require('chai').expect;
+const RG = require('../../../client/src/battles');
+
+describe('OverWorld.createOverWorld', () => {
+    it('can create an overworld with features', () => {
+        const owConf = {
+            owTilesX: 40,
+            owTilesY: 20,
+            worldX: 400,
+            worldY: 200,
+            nLevelsX: 4,
+            nLevelsY: 2,
+            areaX: 4,
+            areaY: 2
+        };
+        const [world, worldConf] = RG.OverWorld.createOverWorld(owConf);
+        expect(world).to.exist;
+        expect(worldConf).to.exist;
+    });
+});
