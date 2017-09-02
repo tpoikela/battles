6ed28408da2d164e0f54325cc056d54a04941d06
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-02 15:41:09 +0300

    Added more checks for the 1st test case.

--- tests/client/src/actor-class.spec.js
+++ tests/client/src/actor-class.spec.js
@@ -4,11 +4,23 @@ const RG = require('../../../client/src/battles');
 
 const ActorClass = require('../../../client/src/actor-class');
 
-describe('ActorClass', () => {
+const RGTest = require('../../roguetest');
+
+describe('ActorClass.Blademaster', () => {
     it('an actor can be initialized with a class', () => {
         const rogue = new RG.Actor.Rogue('rogue');
         const bm = new ActorClass.Blademaster(rogue);
         rogue.setActorClass(bm);
         expect(rogue.getActorClass()).to.exist;
+
+        RGTest.wrapIntoLevel([rogue]);
+
+        rogue.get('Experience').setExpLevel(4);
+        bm.advanceLevel();
+        expect(rogue.has('Defender')).to.be.true;
+
+        rogue.get('Experience').setExpLevel(24);
+        bm.advanceLevel();
+        expect(rogue.has('Ambidexterity')).to.be.true;
     });
 });
