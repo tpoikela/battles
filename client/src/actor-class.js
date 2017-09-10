07e32ffba57c6a65134a77745b418a05ca4e331a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-11 01:23:07 +0300

    Refactored into classes.

--- client/src/actor-class.js
+++ client/src/actor-class.js
@@ -3,93 +3,106 @@ const ActorClass = {};
 const RG = require('./rg');
 RG.Component = require('./component');
 
-ActorClass.Base = function(name) {
-    this._className = name;
+class ActorClassBase {
 
-    this.getClassName = function() {
+    constructor(name) {
+        this._className = name;
+    }
+
+    getClassName() {
         return this._className;
-    };
-};
+    }
+}
 
 //-------------------------------------------------------------------------
 /* Adventurer actor class and its experience level-specific features. */
 //-------------------------------------------------------------------------
-ActorClass.Adventurer = function(actor) {
-    this._actor = actor;
+class Adventurer extends ActorClassBase {
+
+    constructor(actor) {
+        super('Adventurer');
+        this._actor = actor;
+    }
 
     /* Called when a level is advanced by the actor. Checks for messages, and if
      * the next ability is triggered. */
-    this.advanceLevel = function() {
+    advanceLevel() {
         const newLevel = this._actor.get('Experience').getExpLevel();
         if (newLevel % 4 === 0) {
             // Get other random actor class
             // Use it to add new abilities to the actor
         }
-    };
+    }
 
     /* Called at the creation of the actor. Gives certain set of starting items.
      */
-    this.addStartingItems = () => {
+    addStartingItems() {
 
-    };
+    }
 
-    this.setStartingStats = () => {
+    setStartingStats() {
 
-    };
-};
+    }
+}
+
+ActorClass.Adventurer = Adventurer;
 
 //-------------------------------------------------------------------------
 /* BladeMaster actor class and its experience level-specific features. */
 //-------------------------------------------------------------------------
-ActorClass.Blademaster = function(actor) {
-    this._actor = actor;
-    const _name = actor.getName();
+class Blademaster extends ActorClassBase {
 
-    this._messages = {
-        4: `${_name} knows now how to defend more skillfully`,
-        8: `${_name} knows now how to hit enemies more accurately`,
-        12: `${_name} knows now how to handle equipment better`,
-        16: `${_name} can now strike in two directions`,
-        20: `${_name} can now keep the weapons sharp`,
-        24: `${_name} can now wield two blades at once`,
-        28: `${_name} can now strike back immediately when attacked`,
-        32: `${_name} has become a True Blademaster`
-    };
+    constructor(actor) {
+        super('BladeMaster');
+        this._actor = actor;
+        const _name = actor.getName();
 
-    this._advances = {
-        1: () => {
+        this._messages = {
+            4: `${_name} knows now how to defend more skillfully`,
+            8: `${_name} knows now how to hit enemies more accurately`,
+            12: `${_name} knows now how to handle equipment better`,
+            16: `${_name} can now strike in two directions`,
+            20: `${_name} can now keep the weapons sharp`,
+            24: `${_name} can now wield two blades at once`,
+            28: `${_name} can now strike back immediately when attacked`,
+            32: `${_name} has become a True Blademaster`
+        };
 
-        },
-        4: () => {
-            this._actor.add(new RG.Component.Defender());
-        },
-        8: () => {
-            this._actor.add(new RG.Component.Attacker());
-        },
-        12: () => {
-            this._actor.add(new RG.Component.MasterEquipper());
-        },
-        16: () => {
-            this._actor.add(new RG.Component.BiDirStrike());
-        },
-        20: () => {
-            this._actor.add(new RG.Component.Sharpener());
-        },
-        24: () => {
-            this._actor.add(new RG.Component.Ambidexterity());
-        },
-        28: () => {
-            this._actor.add(new RG.Component.CounterAttack());
-        },
-        32: () => {
-            actor.get('Combat').setRange(2);
-            this._actor.add(new RG.Component.LongReach());
-        }
-    };
+        this._advances = {
+            1: () => {
+
+            },
+            4: () => {
+                this._actor.add(new RG.Component.Defender());
+            },
+            8: () => {
+                this._actor.add(new RG.Component.Attacker());
+            },
+            12: () => {
+                this._actor.add(new RG.Component.MasterEquipper());
+            },
+            16: () => {
+                this._actor.add(new RG.Component.BiDirStrike());
+            },
+            20: () => {
+                this._actor.add(new RG.Component.Sharpener());
+            },
+            24: () => {
+                this._actor.add(new RG.Component.Ambidexterity());
+            },
+            28: () => {
+                this._actor.add(new RG.Component.CounterAttack());
+            },
+            32: () => {
+                actor.get('Combat').setRange(2);
+                this._actor.add(new RG.Component.LongReach());
+            }
+        };
+    }
 
     /* Called when a level is advanced by the actor. Checks for messages, and if
      * the next ability is triggered. */
-    this.advanceLevel = function() {
+    advanceLevel() {
         const newLevel = this._actor.get('Experience').getExpLevel();
         if (this._messages.hasOwnProperty(newLevel)) {
             const cell = this._actor.getCell();
@@ -98,194 +111,212 @@ ActorClass.Blademaster = function(actor) {
         if (this._advances.hasOwnProperty(newLevel)) {
             this._advances[newLevel]();
         }
-    };
+    }
 
     /* Called at the creation of the actor. Gives certain set of starting items.
      */
-    this.addStartingItems = () => {
+    addStartingItems() {
 
-    };
+    }
 
-    this.setStartingStats = () => {
+    setStartingStats() {
 
-    };
+    }
 
-};
+}
 
-//-------------------------------------------------------------------------
-/* Cryomancer actor class and its experience level-specific features. */
-//-------------------------------------------------------------------------
-ActorClass.Cryomancer = function(actor) {
-    this._actor = actor;
-    const _name = actor.getName();
-
-    this._messages = {
-        4: `${_name} gains new skill`,
-        8: `${_name} gains new skill`,
-        12: `${_name} gains new skill`,
-        16: `${_name} gains new skill`,
-        20: `${_name} gains new skill`,
-        24: `${_name} gains new skill`,
-        28: `${_name} gains new skill`,
-        32: `${_name} has become a True Cryomancer`
-    };
-
-    this._advances = {
-        1: () => {
-
-        },
-        4: () => {
-
-        },
-        8: () => {
-
-        },
-        12: () => {
-
-        },
-        16: () => {
-
-        },
-        20: () => {
-
-        },
-        24: () => {
-
-        },
-        28: () => {
-
-        },
-        32: () => {
-
-        }
-    };
-
-    this.addStartingItems = () => {
-
-    };
-
-    this.setStartingStats = () => {
-
-    };
-
-};
+ActorClass.Blademaster = Blademaster;
 
 //-------------------------------------------------------------------------
 /* Cryomancer actor class and its experience level-specific features. */
 //-------------------------------------------------------------------------
-ActorClass.Marksman = function(actor) {
-    this._actor = actor;
-    const _name = actor.getName();
+class Cryomancer extends ActorClassBase {
 
-    this._messages = {
-        4: `${_name} gains new skill`,
-        8: `${_name} gains new skill`,
-        12: `${_name} gains new skill`,
-        16: `${_name} gains new skill`,
-        20: `${_name} gains new skill`,
-        24: `${_name} gains new skill`,
-        28: `${_name} gains new skill`,
-        32: `${_name} has become a True Marksman`
-    };
+    constructor(actor) {
+        super('Cryomancer');
+        this._actor = actor;
+        const _name = actor.getName();
 
-    this._advances = {
-        1: () => {
+        this._messages = {
+            4: `${_name} gains new skill`,
+            8: `${_name} gains new skill`,
+            12: `${_name} gains new skill`,
+            16: `${_name} gains new skill`,
+            20: `${_name} gains new skill`,
+            24: `${_name} gains new skill`,
+            28: `${_name} gains new skill`,
+            32: `${_name} has become a True Cryomancer`
+        };
 
-        },
-        4: () => {
+        this._advances = {
+            1: () => {
 
-        },
-        8: () => {
+            },
+            4: () => {
 
-        },
-        12: () => {
+            },
+            8: () => {
 
-        },
-        16: () => {
+            },
+            12: () => {
 
-        },
-        20: () => {
+            },
+            16: () => {
 
-        },
-        24: () => {
+            },
+            20: () => {
 
-        },
-        28: () => {
+            },
+            24: () => {
 
-        },
-        32: () => {
+            },
+            28: () => {
 
-        }
-    };
+            },
+            32: () => {
 
-    this.addStartingItems = () => {
+            }
+        };
+    }
 
-    };
+    addStartingItems() {
 
-    this.setStartingStats = () => {
+    }
 
-    };
+    setStartingStats() {
 
-};
+    }
+
+}
+
+ActorClass.Cryomancer = Cryomancer;
+
+//-------------------------------------------------------------------------
+/* Cryomancer actor class and its experience level-specific features. */
+//-------------------------------------------------------------------------
+class Marksman extends ActorClassBase {
+
+    constructor(actor) {
+        super('Marksman');
+        this._actor = actor;
+        const _name = actor.getName();
+
+        this._messages = {
+            4: `${_name} gains new skill`,
+            8: `${_name} gains new skill`,
+            12: `${_name} gains new skill`,
+            16: `${_name} gains new skill`,
+            20: `${_name} gains new skill`,
+            24: `${_name} gains new skill`,
+            28: `${_name} gains new skill`,
+            32: `${_name} has become a True Marksman`
+        };
+
+        this._advances = {
+            1: () => {
+
+            },
+            4: () => {
+
+            },
+            8: () => {
+
+            },
+            12: () => {
+
+            },
+            16: () => {
+
+            },
+            20: () => {
+
+            },
+            24: () => {
+
+            },
+            28: () => {
+
+            },
+            32: () => {
+
+            }
+        };
+    }
+
+    addStartingItems() {
+
+    }
+
+    setStartingStats() {
+
+    }
+
+}
+ActorClass.Marksman = Marksman;
 
 //-------------------------------------------------------------------------
 /* Spellsinger actor class and its experience level-specific features. */
 //-------------------------------------------------------------------------
-ActorClass.Spellsinger = function(actor) {
-    this._actor = actor;
-    const _name = actor.getName();
+class Spellsinger extends ActorClassBase {
 
-    this._messages = {
-        4: `${_name} gains new skill`,
-        8: `${_name} gains new skill`,
-        12: `${_name} gains new skill`,
-        16: `${_name} gains new skill`,
-        20: `${_name} gains new skill`,
-        24: `${_name} gains new skill`,
-        28: `${_name} gains new skill`,
-        32: `${_name} has become a Mighty Spellsinger`
-    };
+    constructor(actor) {
+        super('Spellsinger');
+        this._actor = actor;
+        const _name = actor.getName();
 
-    this._advances = {
-        1: () => {
+        this._messages = {
+            4: `${_name} gains new skill`,
+            8: `${_name} gains new skill`,
+            12: `${_name} gains new skill`,
+            16: `${_name} gains new skill`,
+            20: `${_name} gains new skill`,
+            24: `${_name} gains new skill`,
+            28: `${_name} gains new skill`,
+            32: `${_name} has become a Mighty Spellsinger`
+        };
 
-        },
-        4: () => {
+        this._advances = {
+            1: () => {
 
-        },
-        8: () => {
+            },
+            4: () => {
 
-        },
-        12: () => {
+            },
+            8: () => {
 
-        },
-        16: () => {
+            },
+            12: () => {
 
-        },
-        20: () => {
+            },
+            16: () => {
 
-        },
-        24: () => {
+            },
+            20: () => {
 
-        },
-        28: () => {
+            },
+            24: () => {
 
-        },
-        32: () => {
+            },
+            28: () => {
 
-        }
-    };
+            },
+            32: () => {
 
-    this.addStartingItems = () => {
+            }
+        };
+    }
+
+    addStartingItems() {
         // Starting instrument
 
-    };
+    }
 
-    this.setStartingStats = () => {
+    setStartingStats() {
 
-    };
+    }
 
-};
+}
+ActorClass.Spellsinger = Spellsinger;
 
 RG.ACTOR_CLASSES = Object.keys(ActorClass);
 
