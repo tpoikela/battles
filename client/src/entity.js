52764e6ae681f313b48993cb55999298fd92cce9
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-10 23:19:58 +0300

    Added entity.js.

---
+++ client/src/entity.js
@@ -0,0 +1,56 @@
+
+const RG = require('./rg');
+
+export default class Entity {
+    constructor() {
+        this._id = Entity.idCount++;
+        this._comps = {};
+    }
+
+    getID() {return this._id;}
+    setID(id) {this._id = id;}
+
+    get(name) {
+        if (this._comps.hasOwnProperty(name)) {return this._comps[name];}
+        return null;
+    }
+
+    add(nameOrComp, comp) {
+        let compName = nameOrComp;
+        let compObj = comp;
+        if (typeof nameOrComp === 'object') {
+            compObj = nameOrComp;
+            compName = nameOrComp.getType();
+        }
+        this._comps[compName] = compObj;
+        compObj.entityAddCallback(this);
+        RG.POOL.emitEvent(compName, {entity: this, add: true});
+    }
+
+    has(name) {
+        return this._comps.hasOwnProperty(name);
+    }
+
+    remove(name) {
+        if (this._comps.hasOwnProperty(name)) {
+            const comp = this._comps[name];
+            comp.entityRemoveCallback(this);
+            delete this._comps[name];
+            RG.POOL.emitEvent(name, {entity: this, remove: true});
+        }
+    }
+
+    getComponents() {return this._comps;}
+
+}
+
+Entity.idCount = 0;
+
+Entity.createEntityID = () => {
+    const id = Entity.prototype.idCount;
+    Entity.prototype.idCount += 1;
+    return id;
+};
+
+Entity.getIDCount = () => Entity.idCount;
+
