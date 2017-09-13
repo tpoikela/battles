85f52e5bd6df6651e7990f4244354fe178e38350
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-14 01:08:02 +0300

    Added test for clicking the slot.

--- tests/client/jsx/game-item-slot.spec.jsx
+++ tests/client/jsx/game-item-slot.spec.jsx
@@ -8,9 +8,12 @@ import GameItemSlot from '../../../client/jsx/game-item-slot';
 
 chai.use(chaiEnzyme());
 
+let selected = null;
+
 // Props with non-default values passes to the component
 const props = {
-    item: {toString: () => 'item string'}
+    item: {toString: () => 'item string', name: 'My item'},
+    setSelectedItem: item => selected = item
 
 };
 
@@ -18,5 +21,13 @@ describe('Component <GameItemSlot>', () => {
     it('should render', () => {
         const wrapper = shallow(<GameItemSlot {...props} />);
         expect(wrapper, 'Component must render with shallow').to.have.length(1);
+    });
+
+    it('can be clicked render', () => {
+        const wrapper = shallow(<GameItemSlot {...props} />);
+        const div = wrapper.find('div');
+        div.simulate('click');
+        expect(selected.name).to.equal('My item');
+
     });
 });
