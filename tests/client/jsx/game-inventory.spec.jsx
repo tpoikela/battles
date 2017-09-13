6670c23eb3add843b79d96a9387348955e538a98
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-14 01:21:38 +0300

    Added a test to check doInvCmd callback.

--- tests/client/jsx/game-inventory.spec.jsx
+++ tests/client/jsx/game-inventory.spec.jsx
@@ -9,21 +9,33 @@ RG.Inv = require('../../../client/src/inv');
 RG.Item = require('../../../client/src/item');
 RG.Actor = require('../../../client/src/actor');
 RG.Factory = require('../../../client/src/factory');
+const RGTest = require('../../roguetest');
 
 import GameItems from '../../../client/jsx/game-items';
 import GameInventory from '../../../client/jsx/game-inventory';
 
 chai.use(chaiEnzyme());
 
-// Props with non-default values passes to the component
-const player = new RG.Actor.Rogue('player');
-const props = {
-    player,
-    eq: new RG.Inv.Equipment(player),
-    inv: new RG.Item.Container(player)
-};
 
 describe('Component <GameInventory>', () => {
+
+    let numClicks = 0;
+    const resCmds = [];
+
+    // Props with non-default values passes to the component
+    const player = new RG.Actor.Rogue('player');
+    RGTest.wrapIntoLevel([player]);
+    const props = {
+        doInvCmd: cmd => {
+            ++numClicks;
+            resCmds.push(cmd);
+        },
+        player,
+        eq: new RG.Inv.Equipment(player),
+        inv: new RG.Item.Container(player),
+        equipSelected: {slotName: 'hand'}
+    };
+
     it('should render', () => {
         const wrapper = shallow(<GameInventory {...props} />);
         expect(wrapper, 'Component must render with shallow').to.have.length(1);
@@ -32,5 +44,16 @@ describe('Component <GameInventory>', () => {
     it('should have <GameItems>', () => {
         const wrapper = shallow(<GameInventory {...props} />);
         expect(wrapper.find(GameItems)).to.have.length(1);
+    });
+
+    it('has buttons for manipulating items', () => {
+        const wrapper = shallow(<GameInventory {...props} />);
+        const buttons = wrapper.find('button');
+        buttons.forEach(btn => btn.simulate('click'));
+        expect(numClicks).to.equal(4);
+
+        resCmds.forEach(cmd => {
+            expect(cmd).to.have.property('callback');
+        });
     });
 });
