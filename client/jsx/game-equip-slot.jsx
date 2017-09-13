aa75f1c29464a7df3c4801de29109b179026e0b5
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-13 18:46:24 +0300

    Refactored into class.

--- client/jsx/game-equip-slot.jsx
+++ client/jsx/game-equip-slot.jsx
@@ -1,30 +1,42 @@
 
-const React = require('react');
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 /** Component for one equipment slot.*/
-var GameEquipSlot = React.createClass({
+class GameEquipSlot extends Component {
 
-    setEquipSelected: function(evt) {
-        if (this.props.item !== null) {
-            var selection = {
-                slotName: this.props.slotName,
-                slotNumber: this.props.slotNumber,
-                item: this.props.item
-            };
-            this.props.setEquipSelected(selection);
-        }
-    },
+  constructor(props) {
+    super(props);
+    this.setEquipSelected = this.setEquipSelected.bind(this);
+  }
 
-    render: function() {
-        var slotName = this.props.slotName;
-        var item = this.props.item;
-        var msg = 'Empty';
-        if (item !== null) {msg = item.toString();}
-        return (
-            <div onClick={this.setEquipSelected} className='inv-equip-slot'>{slotName} {msg}</div>
-        );
+  setEquipSelected(evt) {
+    if (this.props.item !== null) {
+      const selection = {
+        slotName: this.props.slotName,
+        slotNumber: this.props.slotNumber,
+        item: this.props.item
+      };
+      this.props.setEquipSelected(selection);
     }
+  }
 
-});
+  render() {
+    const slotName = this.props.slotName;
+    const item = this.props.item;
+    let msg = 'Empty';
+    if (item !== null) {msg = item.toString();}
+    return (
+      <div onClick={this.setEquipSelected} className='inv-equip-slot'>{slotName} {msg}</div>
+    );
+  }
+
+}
+
+GameEquipSlot.propTypes = {
+  slotName: PropTypes.string,
+  item: PropTypes.object,
+  slotNumber: PropTypes.number
+};
 
 module.exports = GameEquipSlot;
