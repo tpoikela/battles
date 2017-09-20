dddbed1a95eee05308627db127287c35ee748206
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-20 18:11:04 +0300

    Added unit test for getMissilePath.

--- tests/client/src/geometry.spec.js
+++ tests/client/src/geometry.spec.js
@@ -29,12 +29,34 @@ describe('RG.Geometry', () => {
         const cellType2 = superLevel.getMap().getBaseElemXY(x2, y2).getType();
         superLevel.getMap().debugPrintInASCII();
         expect(cellType2).to.equal('wall');
-
     });
 
     it('can center the tiled levels.', () => {
         const conf = {x: 1, y: 1, centerX: true};
         RG.Geometry.tileLevels(superLevel, levels, conf);
         superLevel.getMap().debugPrintInASCII();
+    });
+
+    it('can generate "straight" paths for missiles', () => {
+        const [x0, y0] = [0, 0];
+        const [x1, y1] = [4, 4];
+        let path = RG.Geometry.getMissilePath(x0, y0, x1, y1);
+        expect(path).to.have.length(5);
+
+        path = RG.Geometry.getMissilePath(x0, y0, x1, y1, false);
+        expect(path).to.have.length(3);
+
+        const [x, y] = [0, 0];
+        const [xEnd, yEnd] = [2, 6];
+        path = RG.Geometry.getMissilePath(x, y, xEnd, yEnd);
+
+        expect(path).to.have.length(7);
+        console.log(JSON.stringify(path));
+
+        const [xx, yy] = [1, 7];
+        const [xxEnd, yyEnd] = [8, 3];
+        path = RG.Geometry.getMissilePath(xx, yy, xxEnd, yyEnd);
+        expect(path).to.have.length(8 - 1 + 1);
+        console.log(JSON.stringify(path));
     });
 });
