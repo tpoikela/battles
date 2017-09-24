5d42c25bb844bd12cc52889c817cefec71cbf943
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-25 01:23:21 +0200

    Added getShortestPassablePathWithDoor().

--- client/src/path.js
+++ client/src/path.js
@@ -14,6 +14,26 @@ Path.getShortestPassablePath = function(map, x0, y0, x1, y1) {
     return coords;
 };
 
+Path.getShortestPassablePathWithDoors = function(map, x0, y0, x1, y1) {
+    console.log('BEGIN getShortestPassablePathWithDoors');
+    const coords = [];
+    const passableCbDoor = (x, y) => {
+        if (map.hasXY(x, y)) {
+            if (map.getCell(x, y).hasDoor()) {
+                console.log('Found door at ' + x + ',' + y);
+            }
+            return map.isPassable(x, y) || map.getCell(x, y).hasDoor();
+        }
+        return false;
+    };
+    const finder = new ROT.Path.AStar(x1, y1, passableCbDoor);
+    finder.compute(x0, y0, (x, y) => {
+        coords.push({x, y});
+    });
+    console.log('Path with doors length: ' + coords.length);
+    return coords;
+};
+
 /* Returns shortest path (array of x,y pairs) between two points. Does not
 * check if any of the cells are passable. */
 Path.getShortestPath = function(x0, y0, x1, y1) {
@@ -44,7 +64,7 @@ Path.getPathWeight = (map, coord) => {
                 case 'water': w += 4; break;
                 case 'highrock': w += 4; break;
                 case 'chasm': w += 5; break;
-                case 'wall': w += 10; break;
+                case 'wall': w += 20; break;
                 default: w += 0; break;
             }
         }
@@ -53,13 +73,24 @@ Path.getPathWeight = (map, coord) => {
 };
 
 /* Gets the minimum weight path between x0,y0 and x1,y1. This algorithm will
-* tunnel through any obstacles eventually. */
-Path.getMinWeightPath = function(map, x0, y0, x1, y1) {
-    const coordPassable = Path.getShortestPassablePath(map,
-        x0, y0, x1, y1);
+* tunnel through any obstacles eventually. Optionally takes the path function
+* as last argument or uses the default getShortestPassablePath. */
+Path.getMinWeightPath = function(map, x0, y0, x1, y1, pathFunc) {
+    let coordPassable = [];
+    if (pathFunc) {
+        console.log('pathFunc given. Using it now');
+        coordPassable = pathFunc(map, x0, y0, x1, y1);
+    }
+    else {
+        coordPassable = Path.getShortestPassablePath(map, x0, y0, x1, y1);
+    }
+
     const coordShortest = Path.getShortestPath(x0, y0, x1, y1);
     const passableWeight = Path.getPathWeight(map, coordPassable);
     const shortestWeight = Path.getPathWeight(map, coordShortest);
+
+    console.log(`passableWeight: ${passableWeight}`);
+    console.log(`shortestWeight: ${shortestWeight}`);
 
     let coord = null;
     if (coordPassable.length === 0) {
@@ -71,7 +102,6 @@ Path.getMinWeightPath = function(map, x0, y0, x1, y1) {
     }
     return coord;
 };
-
 
 Path.addPathToMap = function(map, coord) {
     const chosenCoord = [];
