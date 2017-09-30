ab577523e1c906d49117903b3a040260e86f0374
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-30 16:41:27 +0300

    Add round() for floating point coords. The subLevel castle was not inserted properly.

--- client/data/abandoned-fort.js
+++ client/data/abandoned-fort.js
@@ -24,26 +24,28 @@ export default class AbandonedFort {
     const mainLevel = RG.FACT.createLevel('empty', cols, rows);
 
     const wallCols = Math.floor(cols / 2);
-    const mountWall = RG.FACT.createLevel('wall', wallCols, rows, mainWallOpts);
+    const mountWall = RG.FACT.createLevel('wall', wallCols, rows,
+      mainWallOpts);
+    const wallX = cols - mountWall.getMap().cols;
+    const wallY = 0;
+    RG.Geometry.insertSubLevel(mainLevel, mountWall, wallX, wallY);
 
     const castleRows = Math.floor(0.6 * rows);
     const castleCols = Math.floor(0.5 * cols);
     const castleOpts = {
       tilesX: Math.round(castleCols / 7),
       tilesY: Math.round(castleRows / 7),
-      roomCount: 50,
+      roomCount: 100,
       startRoomFunc: Castle.startRoomFuncWest
     };
 
     const castle = RG.FACT.createLevel('castle', castleCols,
         castleRows, castleOpts);
     const castleX = cols - castle.getMap().cols;
-    const castleY = (rows - castle.getMap().rows) / 2;
+    const castleY = Math.round((rows - castle.getMap().rows) / 2);
+    console.log(`castle X,Y ${castleX},${castleY}`);
     RG.Geometry.insertSubLevel(mainLevel, castle, castleX, castleY);
 
-    const wallX = cols - mountWall.getMap().cols;
-    const wallY = 0;
-    // RG.Geometry.insertSubLevel(mainLevel, mountWall, wallX, wallY);
 
     this.level = mainLevel;
   }
