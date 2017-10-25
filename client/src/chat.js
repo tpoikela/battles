2f6cdcb35c23956d0ca3d52634cbea519ebf88df
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-10-25 13:36:09 +0200

    Added costs.

--- client/src/chat.js
+++ client/src/chat.js
@@ -5,19 +5,20 @@ const RG = require('./rg');
 
 const Chat = {};
 
-const stats = ['Acc', 'Agi', 'Wil', 'Mag', 'Per', 'Str'];
+const stats = RG.STATS;
 
 class ChatTrainer {
 
     constructor() {
         this.selectionObject = {
             showMenu: () => true,
-            getMenu: function() {
+            getMenu: () => {
                 RG.gameMsg('Please select a stat to train:');
                 const indices = RG.menuIndices.slice(0, 6);
                 const menuObj = {};
                 stats.forEach((stat, index) => {
                     menuObj[indices[index]] = stats[index];
+                    menuObj[indices[index]] += ` (${this.costs[index]} gold)`;
                 });
                 return menuObj;
 
@@ -26,13 +27,10 @@ class ChatTrainer {
                 const selection = RG.codeToIndex(code);
                 if (selection < stats.length) {
                     const statSel = stats[selection];
-                    const name = this.target.getName();
-                    const tName = this.trainer.getName();
-                    const msg = `${name} trains ${statSel} with ${tName}`;
-                    RG.gameMsg(msg);
-                    // TODO add callback for training
-                    this.trainCallback(statSel);
+                    const cost = this.costs[selection];
+                    return this.trainCallback(statSel, cost);
                 }
+                return null;
             }
         };
         this.trainCallback = this.trainCallback.bind(this);
@@ -47,17 +45,59 @@ class ChatTrainer {
         this.trainer = trainer;
     }
 
-    /* Sets the target to train. */
+    /* Sets the target to train. Computes also the training costs based on the
+     * stats of the target. */
     setTarget(target) {
         this.target = target;
+        this.costs = [];
+        stats.forEach((stat) => {
+            const getFunc = 'get' + stat;
+            this.costs.push(100 * target.get('Stats')[getFunc]());
+        });
     }
 
     getSelectionObject() {
         return this.selectionObject;
     }
 
-    trainCallback(statSel) {
-        console.log('Train callback was called with: ' + statSel);
+    trainCallback(statSel, cost) {
+        const cb = () => {
+            const gw = RG.valueToGoldWeight(cost);
+            const taName = this.target.getName();
+
+            if (!RG.hasEnoughGold(this.target, gw)) {
+                const msg = `${taName} does not have enough gold.`;
+                RG.gameMsg({cell: this.target.getCell(), msg});
+                return;
+            }
+            else {
+                const nCoins = RG.getGoldInCoins(gw);
+                const coins = new RG.Item.GoldCoin();
+                coins.count = nCoins;
+                this.trainer.getInvEq().addItem(coins);
+
+            }
+
+            const targetStats = this.target.get('Stats');
+            const trainerStats = this.trainer.get('Stats');
+            const getFunc = 'get' + statSel;
+            const setFunc = 'set' + statSel;
+            const targetVal = targetStats[getFunc]();
+            const trainerVal = trainerStats[getFunc]();
+
+            const trName = this.trainer.getName();
+            if (targetVal < trainerVal) {
+                const newTargetVal = targetVal + 1;
+                targetStats[setFunc](newTargetVal);
+                const msg = `${trName} trains ${statSel} of ${taName}`;
+                RG.gameMsg({cell: this.target.getCell(), msg});
+            }
+            else {
+                const msg = `${trName} doesn't have skill to train that stat`;
+                RG.gameMsg({cell: this.target.getCell(), msg});
+            }
+        };
+        return cb;
     }
 
 }
