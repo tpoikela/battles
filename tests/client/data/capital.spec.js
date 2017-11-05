b21dbd966a1134946a022b12dbe1c8bd885e5952
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-11-05 12:12:59 +0100

    Reformatting

--- tests/client/data/capital.spec.js
+++ tests/client/data/capital.spec.js
@@ -5,20 +5,17 @@ import Capital from '../../../client/data/capital';
 const RG = require('../../../client/src/battles');
 
 describe('Capital', () => {
-    /*
     it('can be created without errors', () => {
         const capitalLevel = new Capital(100, 300).getLevel();
         expect(capitalLevel).to.exist;
     });
-    */
 
     it('can be simulated without errors', () => {
         const capitalLevel = new Capital(100, 300).getLevel();
         const game = new RG.Game.Main();
         game.addActiveLevel(capitalLevel);
         for (let i = 0; i < 100; i++) {
-            game.simulateGame();
-            //expect(() => {game.simulateGame();}).not.to.throw();
+            expect(() => {game.simulateGame();}).not.to.throw();
         }
     });
 });
