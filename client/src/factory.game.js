cd935e734a23a2395f43ca21e54ad83c96750dc6
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-01-21 12:36:40 +0200

    Changed the overworld size.

--- client/src/factory.game.js
+++ client/src/factory.game.js
@@ -249,6 +249,7 @@ RG.Factory.Game = function() {
 
     this.createOverWorld = function(obj, game, player) {
         const mult = 1;
+        const xMult = 2;
         const owConf = {
             yFirst: false,
             topToBottom: false,
@@ -256,14 +257,14 @@ RG.Factory.Game = function() {
             stopOnWall: true,
             // nHWalls: 2,
             nVWalls: [0.8],
-            owTilesX: mult * 40,
-            owTilesY: mult * 20,
-            worldX: mult * 800,
-            worldY: mult * 400,
-            nLevelsX: mult * 8,
-            nLevelsY: mult * 4,
-            nTilesX: mult * 8,
-            nTilesY: mult * 4
+            owTilesX: xMult * mult * 40,
+            owTilesY: mult * 40,
+            worldX: xMult * mult * 800,
+            worldY: mult * 800,
+            nLevelsX: xMult * mult * 8,
+            nLevelsY: mult * 8,
+            nTilesX: xMult * mult * 8,
+            nTilesY: mult * 8
         };
 
         const overworld = OW.createOverWorld(owConf);
