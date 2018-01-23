2a15c6841462fec0d9da1f0754e5ab765ea75db5
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-01-24 00:04:35 +0200

    Moved battle creation code to Factory.Battle.

--- client/src/game.master.js
+++ client/src/game.master.js
@@ -1,6 +1,8 @@
 
 const RG = require('./rg');
 
+RG.Factory = require('./factory');
+
 /* GameMaster objects reacts to various events caused by player and other
  * actors, and shapes the game world based on them. For example,
  * GameMaster can:
@@ -12,6 +14,7 @@ const GameMaster = function(pool, game) {
     this.eventPool = pool;
     this.player = null;
     this.game = game;
+    this.fact = new RG.Factory.Battle(game);
 
     this.setPlayer = player => {
         console.log('GameMaster setPlayer');
@@ -27,49 +30,30 @@ const GameMaster = function(pool, game) {
         if (evtName === RG.EVT_LEVEL_CHANGED) {
             const actor = args.actor;
             if (actor.isPlayer()) {
-                // Should spawn something nasty
+                const id = actor.getLevel().getID();
+                if (this.battles.hasOwnProperty(id)) {
+                    // Entered a battle
+
+                    // Get army selection object
+                }
             }
         }
         else if (evtName === RG.EVT_TILE_CHANGED) {
             console.log('EVT_TILE_CHANGED triggered');
             // Should spawn battle etc event
             if (!this.player) {
-                console.log('GameMaster player is null');
                 return;
             }
             const level = this.player.getLevel();
             const id = level.getID();
             if (!this.battles.hasOwnProperty(id)) {
-                this.battles[id] = 1;
-                // TODO refactor into factory method createBattle()
-                const battle = new RG.Game.Battle('Battle of level ' + id);
-                const forestConf = RG.getForestConf(80, 40);
-                const battleLevel = RG.FACT.createLevel('forest', 80, 40,
-                    forestConf);
-                battle.setLevel(battleLevel);
-                const stairsBattle = new RG.Element.Stairs(false);
-                battleLevel.addElement(stairsBattle, 1, 1);
-                const army1 = new RG.Game.Army('goblins');
-                const army2 = new RG.Game.Army('humans');
-                const parser = RG.ObjectShell.getParser();
-                for (let i = 0; i < 20; i++) {
-                    const goblin = parser.createActor('goblin');
-                    army1.addActor(goblin);
-                    const fighter = parser.createActor('fighter');
-                    army2.addActor(fighter);
-                }
-                battle.addArmy(army1, 10, 10);
-                battle.addArmy(army2, 20, 20);
-
-                const stairsArea = new RG.Element.Stairs(true);
-                level.addElement(stairsArea, 4, 4);
-                stairsArea.connect(stairsBattle);
-                this.game.addBattle(battle);
+                this.battles[id] = this.fact.createBattle(level);
             }
         }
         else if (evtName === RG.EVT_BATTLE_OVER) {
             console.log('GameMaster registered battle over');
             RG.gameMsg('Battle is over!');
+            // TODO delete the battle (but keep the level)
         }
     };
     RG.POOL.listenEvent(RG.EVT_LEVEL_CHANGED, this);
