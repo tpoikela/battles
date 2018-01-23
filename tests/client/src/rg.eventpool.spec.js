b34086722c8c121cc6c5c9ee4e5010db778a7996
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-01-23 19:44:49 +0200

    Added unit test for removeListener.

--- tests/client/src/rg.eventpool.spec.js
+++ tests/client/src/rg.eventpool.spec.js
@@ -29,22 +29,30 @@ const EventEmitter = function() {
     };
 };
 
-const empty = {};
-
-const listener = new Listener('ActualEvent');
-const pool = new EventPool();
-pool.listenEvent(listener.eventName, listener);
-
-RG.suppressErrorMessages = true;
-pool.listenEvent('TestEvent', empty);
-RG.suppressErrorMessages = false;
-
-const emitter = new EventEmitter();
-emitter.pool = pool;
-emitter.emit('TestEvent', {data: 'abcd'});
-
 describe('How events bubble in the system', () => {
+
+    let pool = null;
+    let listener = null;
+    let emitter = null;
+
+    beforeEach(() => {
+        pool = new EventPool();
+        listener = new Listener('ActualEvent');
+        pool.listenEvent(listener.eventName, listener);
+        emitter = new EventEmitter();
+        emitter.pool = pool;
+
+    });
+
     it('Sends notification to listener', () => {
+        const empty = {};
+
+        RG.suppressErrorMessages = true;
+        pool.listenEvent('TestEvent', empty);
+        RG.suppressErrorMessages = false;
+
+        emitter.emit('TestEvent', {data: 'abcd'});
+
         expect(listener.notified).to.equal(false);
         emitter.emit('ActualEvent', {data: 'abcd'});
         expect(listener.notified).to.equal(true);
@@ -52,6 +60,13 @@ describe('How events bubble in the system', () => {
         listener.clearNotify();
         expect(listener.notified).to.equal(false);
         emitter.emit('RandomEvent', {data: 'abcd'});
+    });
+
+    it('can have its listeners removed', () => {
+        pool.removeListener(listener);
+        expect(listener.notified).to.equal(false);
+        emitter.emit('ActualEvent', {data: 'abcd'});
+        expect(listener.notified).to.equal(false);
     });
 });
 
