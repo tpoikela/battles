d6b99ec1995c12afbcfdc48f792e4700e6e3c778
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-01-24 23:27:01 +0100

    Added System.Battle.

--- client/src/engine.js
+++ client/src/engine.js
@@ -35,6 +35,7 @@ const Engine = function(eventPool) {
     this.systemOrder = ['Disability', 'SpiritBind', 'Attack', 'Chat',
         'Shop', 'SpellCast',
         'SpellEffect', 'Missile', 'Movement', 'Animation', 'Damage',
+        'Battle',
         'Skills', 'ExpPoints', 'Communication'];
 
     this.systems = {};
@@ -53,6 +54,7 @@ const Engine = function(eventPool) {
     this.systems.Animation = new RG.System.Animation(
         ['Animation']);
     this.systems.Damage = new RG.System.Damage(['Damage', 'Health']);
+    this.systems.Battle = new RG.System.Battle(['BattleBadge']);
     this.systems.Skills = new RG.System.Skills(['SkillsExp']);
     this.systems.ExpPoints = new RG.System.ExpPoints(
         ['ExpPoints', 'Experience']);
