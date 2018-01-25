6a23b5e0d3f8b9c6b12cf5e3053a4224aca3851d
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-01-25 13:39:38 +0100

    Changed Object.keys -> Reflect.ownKeys due to Symbols(). Removed event pool manipulation functions (not used).

--- client/src/rg.js
+++ client/src/rg.js
@@ -565,32 +565,7 @@ const RG = { // {{{2
         return res;
     },
 
-    // Event pool -related stuff
-
-    eventPools: [],
-    POOL: null,
-
-    resetEventPools: function() {
-        this.eventPools = [];
-        this.POOL = null;
-    },
-
-    // Event pool controls
-    pushEventPool: function(pool) {
-        this.eventPools.push(pool);
-        this.POOL = pool;
-    },
-
-    popEventPool: function() {
-        this.eventPools.pop();
-        const nlen = this.eventPools.length;
-        if (nlen > 0) {
-            this.POOL = this.eventPools[nlen - 1];
-        }
-        else {
-            RG.err('RG', 'popEventPool', 'No event pools left.');
-        }
-    },
+    POOL: null, // Global event pool
 
     //--------------------------------------------------------------
     // CONSTANTS
@@ -1407,6 +1382,9 @@ RG.EventPool = function() { // {{{2
     const _listeners = {};
     let _nListeners = 0;
 
+    const _poolID = RG.EventPool.id;
+    let _listenerID = 0;
+
     this.getNumListeners = () => _nListeners;
 
     /* Emits an event with given name. args must be in object-notation ie.
@@ -1443,6 +1421,9 @@ RG.EventPool = function() { // {{{2
                     _listeners[evtName] = [];
                     _listeners[evtName].push(obj);
                 }
+                if (!obj.hasOwnProperty('listenerID')) {
+                    obj.listenerID = _listenerID++;
+                }
             }
             else {
                 let msg = 'evtName: ' + evtName;
@@ -1458,20 +1439,32 @@ RG.EventPool = function() { // {{{2
 
     /* Removes the object from a list of event listeners. */
     this.removeListener = obj => {
-        Object.keys(_listeners).forEach(evt => {
-            const index = _listeners[evt].indexOf(obj);
-            if (index >= 0) {
-                console.log('<> Removing listeners');
-                _listeners[evt].splice(index, 1);
+        if (obj.hasOwnProperty('listenerID')) {
+            let nRemoved = 0;
+            const id = obj.listenerID;
+            Reflect.ownKeys(_listeners).forEach(evt => {
+                const index = _listeners[evt].findIndex(obj =>
+                    obj.listenerID === id);
+                if (index >= 0) {
+                    _listeners[evt].splice(index, 1);
+                    ++nRemoved;
+                }
+            });
+            if (nRemoved === 0) {
+                RG.warn('EventPool', 'removeListener',
+                    `ListenerID ${obj.listenerID} not found`);
             }
-            else {
-                console.log('<> No index for listener. Cannot remove');
-            }
-        });
-
+            delete obj.listenerID;
+        }
+        else {
+            const json = JSON.stringify(obj);
+            RG.err('EventPool', 'removeListener',
+                `No prop listener ID from on object ${json}`);
+        }
     };
 };
 RG.POOL = new RG.EventPool(); // Dangerous, global objects
+RG.EventPool.id = 0;
 
 //---------------------------------------------------------------------------
 // MessageHandler
