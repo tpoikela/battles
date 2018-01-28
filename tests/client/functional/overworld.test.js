54355b1207856bd06951f81dcfb75ed9c0a21c1a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-01-28 17:43:50 +0100

    Added after/beforeEach.

--- tests/client/functional/overworld.test.js
+++ tests/client/functional/overworld.test.js
@@ -7,21 +7,29 @@ RG.Factory.Game = require('../../../client/src/factory.game');
 
 describe('How Game is created from Overworld', function() {
     this.timeout(90000);
-    it('is created using factory from game/player objects', () => {
-        RG.RAND.setSeed(6666);
-        const gameFact = new RG.Factory.Game();
 
+    let game = null;
+
+    beforeEach(() => {
         const conf = {
             playMode: 'OverWorld',
             playerLevel: 'Medium',
             sqrPerItem: 100,
-            sqrPerActor: 100
+            sqrPerActor: 100,
+            yMult: 0.5
         };
+        const gameFact = new RG.Factory.Game();
+        game = gameFact.createNewGame(conf);
+    });
 
-        // const player = new RG.Actor.Rogue('My Hero');
-        // player.setIsPlayer(true);
-        // const gameObj = new RG.Game.Main();
-        const game = gameFact.createNewGame(conf);
+    afterEach(() => {
+        game = null;
+    });
+
+
+    it('is created using factory from game/player objects', () => {
+        RG.RAND.setSeed(6666);
+
         expect(game).to.exist;
 
         const places = game.getPlaces();
