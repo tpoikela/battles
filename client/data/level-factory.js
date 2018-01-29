b25e0e7e324e3c189defb521e482f6d11f4c56a6
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-01-29 21:00:14 +0100

    Added support for any level provided by FACt.createLevel.

--- client/data/level-factory.js
+++ client/data/level-factory.js
@@ -3,19 +3,24 @@ import AbandonedFort from './abandoned-fort';
 import Capital from './capital';
 import DwarvenCity from './dwarven-city';
 
-const RG = require('../src/rg');
+// const RG = require('../src/rg');
 
 export default class LevelFactory {
+
+    constructor(fact) {
+      this.fact = fact;
+    }
 
     create(name, args) {
         switch (name) {
             case 'Capital': return new Capital(...args).getLevel();
             case 'DwarvenCity': return new DwarvenCity(...args).getLevel();
             case 'AbandonedFort': return new AbandonedFort(...args).getLevel();
-            default: RG.err('LevelFactory', 'create',
-                `No constructor found for ${name}`);
+            default: return this.fact.createLevel(name, ...args);
+            /* default: RG.err('LevelFactory', 'create',
+                `No constructor found for ${name}`);*/
         }
-        return null;
+        // return null;
     }
 
 }
