028775d1853ccef56349468e2c308a9abe3ac976
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-07 13:07:09 +0200

    Added Evaluator.Orders.

--- client/src/evaluators.js
+++ client/src/evaluators.js
@@ -16,11 +16,11 @@ class EvaluatorBase {
         this.actorBias = actorBias;
     }
 
-    calculateDesirability(actor) {
+    calculateDesirability() {
         throw new Error('Pure virtual function');
     }
 
-    setGoal(actor) {
+    setActorGoal() {
         throw new Error('Pure virtual function');
     }
 
@@ -46,7 +46,7 @@ class EvaluatorAttackActor extends EvaluatorBase {
         return 0.0;
     }
 
-    setGoal(actor) {
+    setActorGoal(actor) {
         const topGoal = actor.getBrain().getGoal();
         const goal = new Goal.AttackActor(actor, this.enemyActor);
         topGoal.addGoal(goal);
@@ -70,7 +70,7 @@ class EvaluatorExplore extends EvaluatorBase {
         return this.actorBias;
     }
 
-    setGoal(actor) {
+    setActorGoal(actor) {
         const topGoal = actor.getBrain().getGoal();
         const goal = new Goal.Explore(actor);
         topGoal.addGoal(goal);
@@ -101,7 +101,7 @@ class EvaluatorFlee extends EvaluatorBase {
         return 0;
     }
 
-    setGoal(actor) {
+    setActorGoal(actor) {
         const topGoal = actor.getBrain().getGoal();
         const goal = new Goal.Flee(actor, this.enemyActor);
         topGoal.addGoal(goal);
@@ -122,11 +122,11 @@ class EvaluatorPatrol extends EvaluatorBase {
         this.coords = coords;
     }
 
-    calculateDesirability(actor) {
+    calculateDesirability() {
         return this.actorBias;
     }
 
-    setGoal(actor) {
+    setActorGoal(actor) {
         const topGoal = actor.getBrain().getGoal();
         const coords = this.coords;
         const goal = new Goal.Patrol(actor, coords);
@@ -134,5 +134,61 @@ class EvaluatorPatrol extends EvaluatorBase {
     }
 }
 Evaluator.Patrol = EvaluatorPatrol;
+
+/*
+class EvaluatorCommand extends EvaluatorBase {
+
+    constructor(actorBias) {
+        super(actorBias);
+    }
+
+}
+Evaluator.Command = EvaluatorCommand;
+*/
+
+/* Evaluator to check if actor should flee from a fight. */
+class EvaluatorOrders extends EvaluatorBase {
+
+    constructor(actorBias) {
+        super(actorBias);
+        this.goal = null;
+    }
+
+    /* Sets the arguments used for goal injection for commanded actor. */
+    setArgs(args) {
+        this.goal = args.goal;
+        this.srcActor = args.srcActor;
+    }
+
+    calculateDesirability() {
+        // TODO evaluate srcActor status
+        // Evaluate difficulty of goal
+        const commanderMult = 1.0;
+        const goalCateg = this.goal.getCategory();
+        const mult = goalCateg === Goal.Types.Kill ? 0.5 : 1.0;
+        if (this.srcActor.has('Commander')) {
+            return mult * commanderMult;
+        }
+        return 0.0;
+    }
+
+    setActorGoal(actor) {
+        if (this.goal) {
+            const topGoal = actor.getBrain().getGoal();
+            topGoal.addGoal(this.goal);
+        }
+        else {
+            RG.err('EvaluatorOrder', 'setActorGoal',
+                'this.goal must not be null');
+        }
+    }
+
+    setSubEvaluator(evaluator) {
+        this.subEval = evaluator;
+    }
+
+
+}
+Evaluator.Orders = EvaluatorOrders;
 
 module.exports = Evaluator;
