3c406f9e60bde371395720bfebf86203d6650303
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-07 13:09:02 +0200

    Added test for checking Evaluator and Goal.ORders.

--- tests/client/src/goals.spec.js
+++ tests/client/src/goals.spec.js
@@ -3,14 +3,25 @@ import { expect } from 'chai';
 
 const RGTest = require('../../roguetest');
 const RG = require('../../../client/src/battles');
-// const Goal = require('../../../client/src/goals');
+
+const Goal = require('../../../client/src/goals');
+const Evaluator = require('../../../client/src/evaluators');
 
 describe('Goal', () => {
-    it('', () => {
-        const movSys = new RG.System.Movement(['Movement']);
-        const attSys = new RG.System.Attack(['Attack']);
-        const dmgSys = new RG.System.Damage(['Damage']);
-        const systems = [movSys, attSys, dmgSys];
+
+    let movSys = null;
+    let attSys = null;
+    let dmgSys = null;
+    let systems = null;
+
+    beforeEach(() => {
+        movSys = new RG.System.Movement(['Movement']);
+        attSys = new RG.System.Attack(['Attack']);
+        dmgSys = new RG.System.Damage(['Damage']);
+        systems = [movSys, attSys, dmgSys];
+    });
+
+    it('it indicates to actor what to do', () => {
 
         const actor = new RG.Actor.Rogue('thinker');
         const enemy = new RG.Actor.Rogue('enemy');
@@ -19,7 +30,7 @@ describe('Goal', () => {
         actor.addEnemy(enemy);
         enemy.addEnemy(actor);
         const startHP = enemy.get('Health').getHP();
-        const level = RGTest.wrapIntoLevel([actor, enemy]);
+        RGTest.wrapIntoLevel([actor, enemy]);
         RGTest.moveEntityTo(actor, 5, 5);
         RGTest.moveEntityTo(actor, 17, 17);
 
@@ -29,7 +40,6 @@ describe('Goal', () => {
         systems.forEach(sys => {sys.update();});
 
         const [nX, nY] = actor.getXY();
-        console.log(`Actor at nX,nY ${nX},${nY}`);
 
         const coordSame = x === nX && y === nY;
         expect(coordSame).not.to.equal(true);
@@ -37,17 +47,10 @@ describe('Goal', () => {
         RGTest.moveEntityTo(actor, 2, 2);
         RGTest.moveEntityTo(enemy, 5, 5);
 
-        for (let i = 0; i < 5; i++) {
-            console.log(`>>> TURN ${i} BEGIN <<<`);
-            action = actor.nextAction();
-            action.doAction();
-            systems.forEach(sys => {sys.update();});
-            const [x, y] = actor.getXY();
-            console.log(`-> Actor location: ${x},${y}`);
-        }
+        RGTest.updateGame(actor, systems, 5);
         // RGTest.printScreen(actor);
         const endHP = enemy.get('Health').getHP();
-        expect(endHP).to.be.below(startHP);
+        expect(endHP, 'Health must decrease').to.be.below(startHP);
 
         [x, y] = actor.getXY();
         expect([x, y]).to.deep.equal([4, 4]);
@@ -61,4 +64,32 @@ describe('Goal', () => {
         }
 
     });
+
+    it('can be injected into actor via "commander"', () => {
+        const commander = new RG.Actor.Rogue('commander');
+        const soldier = new RG.Actor.Rogue('soldier');
+        const actors = [commander, soldier];
+        commander.setBrain(new RG.Brain.GoalOriented(commander));
+        soldier.setBrain(new RG.Brain.GoalOriented(soldier));
+
+        RGTest.wrapIntoLevel(actors);
+        RGTest.moveEntityTo(commander, 2, 2);
+        RGTest.moveEntityTo(soldier, 3, 3);
+
+        const topGoal = soldier.getBrain().getGoal();
+        topGoal.removeEvaluators();
+        const ordersGoal = new Goal.Orders(soldier);
+        ordersGoal.addSubGoal(new Goal.FollowPath(soldier, [10, 10]));
+
+        const orderBias = 1.0;
+        const commandEval = new Evaluator.Orders(orderBias);
+        commandEval.setArgs({srcActor: commander, goal: ordersGoal});
+        topGoal.giveOrders(commandEval);
+
+        RGTest.updateGame(actors, systems, 11);
+
+        const sXY = soldier.getXY();
+        expect(sXY).to.deep.equal([10, 10]);
+    });
+
 });
