d6891819ac8744c07288568807435de3feb97731
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-09 13:23:05 +0100

    Added <TopMenuLogic>. Modified the eventKey names.

--- client/editor/editor-top-menu.jsx
+++ client/editor/editor-top-menu.jsx
@@ -2,64 +2,93 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+import TopMenuLogic from './top-menu-logic';
+
 import {
   Nav, NavDropdown, MenuItem
 } from 'react-bootstrap';
 
+const RG = require('../src/rg');
+
 export default class EditorTopMenu extends Component {
 
+  constructor(props) {
+      super(props);
+      this.handleSelect = this.handleSelect.bind(this);
+  }
+
   handleSelect(eventKey) {
+    this.props.menuCallback(eventKey);
     console.log('eventKey: ' + eventKey);
+    this.menuLogic.menuCallback(eventKey);
   }
 
   render() {
     return (
+    <div>
       <Nav activeKey='1' bsStyle='tabs' onSelect={this.handleSelect}>
-        <NavDropdown eventKey='1' id='dropdown-file' title='File' >
-          <MenuItem eventKey='1.1'>New...</MenuItem>
-          <MenuItem eventKey='1.2'>Save as...</MenuItem>
-          <MenuItem eventKey='1.3'>Load</MenuItem>
-          <MenuItem eventKey='1.4'>Import</MenuItem>
+
+        <NavDropdown eventKey='file' id='dropdown-file' title='File' >
+          <MenuItem eventKey='file-new'>New...</MenuItem>
+          <MenuItem eventKey='file-save-as'>Save as...</MenuItem>
+          <MenuItem eventKey='file-load'>Load</MenuItem>
+          <MenuItem eventKey='file-import'>Import</MenuItem>
         </NavDropdown>
-        <NavDropdown eventKey='3' id='dropdown-edit' title='Edit' >
-          <MenuItem eventKey='3.1'>Select</MenuItem>
-          <MenuItem eventKey='3.2'>Select shape</MenuItem>
-          <MenuItem eventKey='3.3'>Cut</MenuItem>
-          <MenuItem eventKey='3.4'>Copy</MenuItem>
-          <MenuItem eventKey='3.5'>Paste</MenuItem>
-          <MenuItem eventKey='3.6'>Undo</MenuItem>
+
+        <NavDropdown eventKey='edit' id='dropdown-edit' title='Edit' >
+          <MenuItem eventKey='edit-select'>Select</MenuItem>
+          <MenuItem eventKey='edit-select-shape'>Select shape</MenuItem>
+          <MenuItem eventKey='edit-cut'>Cut</MenuItem>
+          <MenuItem eventKey='edit-copy'>Copy</MenuItem>
+          <MenuItem eventKey='edit-paste'>Paste</MenuItem>
+          <MenuItem eventKey='edit-undo'>Undo</MenuItem>
         </NavDropdown>
-        <NavDropdown eventKey='2' id='dropdown-simulate' title='Simulate' >
-          <MenuItem eventKey='2.1'>New game</MenuItem>
-          <MenuItem eventKey='2.2'>Simulate level</MenuItem>
-          <MenuItem eventKey='2.3'>Play/Pause</MenuItem>
-          <MenuItem eventKey='2.4'>Stop</MenuItem>
-          <MenuItem eventKey='2.5'>Play fast</MenuItem>
+
+        <NavDropdown
+            eventKey='simulate' id='dropdown-simulate' title='Simulate'
+        >
+          <MenuItem eventKey='simulate-1'>New game</MenuItem>
+          <MenuItem eventKey='simulate-2'>Simulate level</MenuItem>
+          <MenuItem eventKey='simulate-3'>Play/Pause</MenuItem>
+          <MenuItem eventKey='simulate-4'>Stop</MenuItem>
+          <MenuItem eventKey='simulate-5'>Play fast</MenuItem>
+          <MenuItem eventKey='simulate-6'>Step</MenuItem>
         </NavDropdown>
-        <NavDropdown eventKey='4' id='dropdown-view' title='View' >
-          <MenuItem eventKey='4.1'>Refresh</MenuItem>
-          <MenuItem eventKey='4.2'>Zoom in</MenuItem>
-          <MenuItem eventKey='4.3'>Zoom out</MenuItem>
-          <MenuItem eventKey='4.4'>Zoom fit</MenuItem>
+
+        <NavDropdown eventKey='view' id='dropdown-view' title='View' >
+          <MenuItem eventKey='view-1'>Refresh</MenuItem>
+          <MenuItem eventKey='view-2'>Zoom in</MenuItem>
+          <MenuItem eventKey='view-3'>Zoom out</MenuItem>
+          <MenuItem eventKey='view-4'>Zoom fit</MenuItem>
         </NavDropdown>
-        <NavDropdown eventKey='5' id='dropdown-insert' title='Insert' >
-          <MenuItem eventKey='5.1'>Actor</MenuItem>
-          <MenuItem eventKey='5.2'>Item</MenuItem>
-          <MenuItem eventKey='5.3'>Element</MenuItem>
-          <MenuItem eventKey='5.4'>Sublevel</MenuItem>
-          <MenuItem eventKey='5.5'>Shape</MenuItem>
+
+        <NavDropdown eventKey='insert' id='dropdown-insert' title='Insert' >
+          <MenuItem eventKey='insert-1'>Actor</MenuItem>
+          <MenuItem eventKey='insert-2'>Item</MenuItem>
+          <MenuItem eventKey='insert-3'>Element</MenuItem>
+          <MenuItem eventKey='insert-4'>Sublevel</MenuItem>
+          <MenuItem eventKey='insert-5'>Shape</MenuItem>
         </NavDropdown>
-        <NavDropdown eventKey='6' id='dropdown-level' title='Level' >
-          <MenuItem eventKey='6.1'>New level</MenuItem>
-          <MenuItem eventKey='6.2'>New zone</MenuItem>
-          <MenuItem eventKey='6.3'>New world</MenuItem>
-          <MenuItem eventKey='6.4'>New battle</MenuItem>
+
+        <NavDropdown eventKey='level' id='dropdown-level' title='Level' >
+          <MenuItem eventKey='level-new-level'>New level</MenuItem>
+          <MenuItem eventKey='level-new-zone'>New zone</MenuItem>
+          <MenuItem eventKey='level-new-world'>New world</MenuItem>
+          <MenuItem eventKey='level-new-battle'>New battle</MenuItem>
         </NavDropdown>
       </Nav>
+
+      <TopMenuLogic
+          level={this.props.level}
+          onRef={ref => {this.menuLogic = ref;}}
+      />
+
+      </div>
     );
   }
 }
 
 EditorTopMenu.propTypes = {
-
+    level: PropTypes.objectOf(RG.Map.Level),
+    menuCallback: PropTypes.func.isRequired
 };
