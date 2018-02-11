4b6256f3845c57a3abf58dbfd84f8a9e84cb2744
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-11 13:12:00 +0100

    Changed the tags generation.

--- bin/gen_tags.sh
+++ bin/gen_tags.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
-find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./jsctags {} -f \; | sed '/^$/d' > tags_temp
+
+#find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./jsctags {} -f \; | sed '/^$/d' > tags_temp
 #find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./jsctags {} -f \; | sed '/^$/d' | sort > tags_temp
-ctags -R ./client/jsx
-cat tags >> tags_temp
-cat tags_temp | sort > tags
-rm tags_temp
+ctags -R ./client
+#cat tags >> tags_temp
+#cat tags_temp | sort > tags
+#rm tags_temp
