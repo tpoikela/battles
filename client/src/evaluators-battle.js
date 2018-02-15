244ea251a12166d47cbd1ad36d404d6df4a4144e
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-15 21:57:24 +0200

    Removed cooldown from WinBattle.

--- client/src/evaluators-battle.js
+++ client/src/evaluators-battle.js
@@ -15,29 +15,21 @@ class EvaluatorWinBattle extends Evaluator.Base {
 
     constructor(actorBias) {
         super(actorBias);
-        this.cooldown = 0;
-        this.maxCoolDown = 20;
     }
 
-    calculateDesirability() {
-        if (this.cooldown === 0) {
-            this.cooldown = this.maxCoolDown;
+    calculateDesirability(actor) {
+        if (actor.has('InBattle')) {
             // can see an army?
             // required to adjust previous command or not
-            console.log('Evaluator WinBattle OK');
             return this.actorBias;
         }
-        else {
-            --this.cooldown;
-        }
-        return 0;
+        return Evaluator.NOT_POSSIBLE;
     }
 
     setActorGoal(actor) {
         const topGoal = actor.getBrain().getGoal();
         const goal = new GoalsBattle.WinBattle(actor);
         topGoal.addGoal(goal);
-        console.log('Set WinBattle as top-level goal');
     }
 }
 EvaluatorsBattle.WinBattle = EvaluatorWinBattle;
