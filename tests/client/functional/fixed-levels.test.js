d664771efa71b0308d903764143d0a8eb396c53e
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-22 07:33:43 +0100

    Added tests for fixed levels.

---
+++ tests/client/functional/fixed-levels.test.js
@@ -0,0 +1,20 @@
+
+
+import AbandonedFort from '../../../client/data/abandoned-fort';
+
+const expect = require('chai').expect;
+const RG = require('../../../client/src/battles');
+
+describe('Abandoned Fort', () => {
+    it('contains items and actors', () => {
+        const fort = new AbandonedFort(500, 200);
+        const level = fort.getLevel();
+
+        expect(level.getActors().length).to.be.above(50);
+        expect(level.getItems().length).to.be.above(10);
+
+        const stairs = level.getStairs();
+        expect(stairs).to.have.length(2);
+
+    });
+});
