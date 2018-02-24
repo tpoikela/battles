2adab4643640d28e4e3bf9685e45e64b002f35fc
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-24 22:20:04 +0100

    Added World.Top and removed checks for created state.

--- tests/client/src/chunk-manager.spec.js
+++ tests/client/src/chunk-manager.spec.js
@@ -1,7 +1,7 @@
 
 import { expect } from 'chai';
 
-import ChunkManager, {LOAD, CREATE} from '../../../client/src/chunk-manager';
+import ChunkManager, {LOAD} from '../../../client/src/chunk-manager';
 
 const RG = require('../../../client/src/battles');
 RG.World = require('../../../client/src/world');
@@ -11,13 +11,15 @@ describe('ChunkManager', () => {
     it('stores the state of world area/chunks ', () => {
         const game = new RG.Game.Main();
         const area = new RG.World.Area('north', 4, 4, 100, 100);
-        game.addPlace(area);
+        const world = new RG.World.Top('World');
+        world.addArea(area);
+        game.addPlace(world);
+
         expect(game.getLevels().length).to.equal(16);
         const manager = new ChunkManager(game, area);
-        expect(manager.getCreateState(0, 0)).to.equal(CREATE.CREATED);
+
         manager.serializeArea();
         expect(game.getLevels().length).to.equal(0);
-        expect(manager.getCreateState(0, 0)).to.equal(CREATE.CREATED);
         expect(manager.getLoadState(0, 0)).to.equal(LOAD.JSON);
 
         manager.setPlayerTile(0, 0);
