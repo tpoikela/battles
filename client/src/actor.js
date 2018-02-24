9b239f5714609375c4222dfd1927f153952827b2
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-24 11:08:29 +0100

    Removed the duplicate strength from getDamage().

--- client/src/actor.js
+++ client/src/actor.js
@@ -361,8 +361,7 @@ class RGActorRogue extends Mixin.Locatable(Mixin.Typed(Entity)) {
 
     getDamage() {
         let damage = this.get('Combat').rollDamage();
-        let strength = this.getStrength();
-        strength += this.getInvEq().getEquipment().getStrength();
+        const strength = this.getStrength();
         damage += RG.strengthToDamage(strength);
         damage += this._addFromCompList('CombatMods', 'getDamage');
         return damage;
