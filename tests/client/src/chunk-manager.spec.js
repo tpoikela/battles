35be0dd97de73b7db7f834943557e04edf87e355
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-25 12:01:03 +0100

    Added two unit tests, one for tile loading/reloading when player moves. 2nd checks functionality after game restore.

--- tests/client/src/chunk-manager.spec.js
+++ tests/client/src/chunk-manager.spec.js
@@ -1,8 +1,7 @@
 
 import { expect } from 'chai';
 
-import ChunkManager, {LOAD, printTileConnections}
-    from '../../../client/src/chunk-manager';
+import ChunkManager, {LOAD} from '../../../client/src/chunk-manager';
 
 const RG = require('../../../client/src/battles');
 RG.World = require('../../../client/src/world');
@@ -11,18 +10,26 @@ const RGTest = require('../../roguetest');
 
 describe('ChunkManager', () => {
 
-    it('stores the state of world area/chunks ', () => {
-        const game = new RG.Game.Main();
-        const area = new RG.World.Area('north', 4, 4, 100, 100);
-        const world = new RG.World.Top('World');
-        const player = new RG.Actor.Rogue('player');
+    let game = null;
+    let area = null;
+    let world = null;
+    let player = null;
+
+    beforeEach(() => {
+        game = new RG.Game.Main();
+        area = new RG.World.Area('north', 4, 4, 100, 100);
+        world = new RG.World.Top('World');
+        player = new RG.Actor.Rogue('player');
         player.setIsPlayer(true);
         world.addArea(area);
         game.addPlace(world);
+    });
+
+    it('stores the state of world area/chunks ', () => {
         // game.addPlayer(player);
 
-        expect(game.getLevels().length).to.equal(16);
         const manager = new ChunkManager(game, area);
+        expect(game.getLevels().length).to.equal(16);
 
         const tiles = area.getTiles();
         let level0 = tiles[0][0].getLevel();
@@ -69,4 +76,58 @@ describe('ChunkManager', () => {
         manager.setPlayerTile(2, 1, 2, 0);
         expect(game.getLevels().length).to.equal(9);
     });
+
+    it('loads/restores chunks when player moves', () => {
+        game.addPlayer(player);
+        const manager = game.getChunkManager();
+        expect(manager).to.exist;
+
+        const playerLevel = player.getLevel();
+        RG.POOL.emitEvent(RG.EVT_TILE_CHANGED, {actor: player,
+            target: playerLevel});
+
+        const playerPos = game.getPlayerOwPos();
+        expect(playerPos).to.deep.equal([]);
+
+        game.movePlayer(3, 3);
+        expect(manager.getLoadState(3, 3)).to.equal(LOAD.LOADED);
+        expect(manager.getLoadState(3, 2)).to.equal(LOAD.LOADED);
+        expect(manager.getLoadState(2, 3)).to.equal(LOAD.LOADED);
+        expect(manager.getLoadState(2, 2)).to.equal(LOAD.LOADED);
+
+        const numLoaded = manager.getNumInState(LOAD.LOADED);
+        const numJSON = manager.getNumInState(LOAD.JSON);
+        expect(numLoaded).to.equal(4);
+        expect(numJSON).to.equal(4 * 4 - numLoaded);
+
+        const levels = area.getLevels();
+        levels.forEach(level => {
+            const msg = `Level ${level.getID()} has parent`;
+            expect(level.getParent(), msg).to.exist;
+        });
+    });
+
+    it('prevents loading of the full world after restore', () => {
+        game.addPlayer(player);
+        const manager = game.getChunkManager();
+        expect(manager).to.exist;
+
+        game.movePlayer(2, 2);
+        const numLoaded = manager.getNumInState(LOAD.LOADED);
+        expect(numLoaded).to.equal(9);
+
+        world.getConf().createAllZones = false;
+
+        const json = game.toJSON();
+
+        const fromJSON = new RG.Game.FromJSON();
+        const newGame = fromJSON.createGame(json);
+        expect(newGame).to.exist;
+
+        const newManager = newGame.getChunkManager();
+        const newNumLoaded = newManager.getNumInState(LOAD.LOADED);
+        expect(newNumLoaded).to.equal(9);
+
+    });
+
 });
