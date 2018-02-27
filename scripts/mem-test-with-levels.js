40d335f2cad4f73df28a6401ae1f7917072758dc
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-28 00:39:12 +0100

    Test case can be now given from command line.

--- scripts/mem-test-with-levels.js
+++ scripts/mem-test-with-levels.js
@@ -2,7 +2,6 @@
 
 require('babel-register');
 
-
 const RG = require('../client/src/battles');
 const RGTest = require('../tests/roguetest');
 
@@ -10,9 +9,20 @@ const fact = new RG.Factory.Base();
 
 const levels = [];
 
+let selectCase = 0;
+if (process.argv.length > 2) {
+    selectCase = parseInt(process.argv[2], 10);
+}
+
+
 for (let i = 0; i < 1000; i++) {
     const level = fact.createLevel('arena', 100, 100);
-    levels.push(JSON.stringify(level.toJSON()));
+    // Case 1: Pure Map.Level object
+    if (selectCase === 0) {levels.push(level);}
+    // Case 2: Serialized Map.Level object
+    if (selectCase === 1) {levels.push(level.toJSON());}
+    // Case 3: JSON stringified
+    if (selectCase === 2) {levels.push(JSON.stringify(level));}
     if (i % 50 === 0) {
         RGTest.printMemUsage(`${i} levels created`);
     }
