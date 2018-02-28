2923b8e51936599410b1427dc59198bbb244f519
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-28 18:24:39 +0100

    Added --delete option.

--- bin/load_json_savegame.js
+++ bin/load_json_savegame.js
@@ -9,6 +9,7 @@ const RG = require('../client/src/battles');
 
 const optDefs = [
     {name: 'all', type: Boolean, descr: 'Loads each file in directory'},
+    {name: 'delete', type: Boolean, descr: 'Deletes invalid save game'},
     {name: 'json', type: String, descr: 'JSON file to load'}
 ];
 
@@ -31,9 +32,15 @@ let game = null;
 try {
     const fromJSON = new RG.Game.FromJSON();
     game = fromJSON.createGame(json);
-    game.update({key: RG.VK_PERIOD});
 }
 catch (e) {
+    if (opts.delete) {
+        console.log('Deleting invalid save file ' + jsonFile);
+        fs.unlinkSync(jsonFile);
+    }
     throw e;
 }
 
+if (game) {
+    game.update({code: RG.VK_PERIOD});
+}
