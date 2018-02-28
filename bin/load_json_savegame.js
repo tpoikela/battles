29b599337ff5799279e922dcbdb2e7886419471f
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-02-28 20:15:22 +0100

    Added --pretty option to print out the JSON in formatted manner.

--- bin/load_json_savegame.js
+++ bin/load_json_savegame.js
@@ -10,7 +10,8 @@ const RG = require('../client/src/battles');
 const optDefs = [
     {name: 'all', type: Boolean, descr: 'Loads each file in directory'},
     {name: 'delete', type: Boolean, descr: 'Deletes invalid save game'},
-    {name: 'json', type: String, descr: 'JSON file to load'}
+    {name: 'json', type: String, descr: 'JSON file to load'},
+    {name: 'pretty', type: Boolean, descr: 'Output a pretty version'}
 ];
 
 const opts = cmdLineArgs(optDefs);
@@ -19,28 +20,58 @@ if (!opts.json) {
     throw new Error('--json missing.');
 }
 
-const jsonFile = opts.json;
-const buf = fs.readFileSync(jsonFile);
-const json = JSON.parse(buf);
-
-let allJSON = fs.readdirSync('save_dumps/');
-allJSON = allJSON.sort();
-console.log(allJSON);
-
-let game = null;
-
-try {
-    const fromJSON = new RG.Game.FromJSON();
-    game = fromJSON.createGame(json);
+if (opts.pretty) {
+    outputPrettyVersion(opts.json);
 }
-catch (e) {
-    if (opts.delete) {
-        console.log('Deleting invalid save file ' + jsonFile);
-        fs.unlinkSync(jsonFile);
+else {
+    tryToLoadFile(opts.json);
+}
+
+//---------------------------------------------------------------------------
+// HELPER FUNCTIONS
+//---------------------------------------------------------------------------
+
+function outputPrettyVersion(jsonFile) {
+    const buf = fs.readFileSync(jsonFile);
+    const json = JSON.parse(buf);
+
+    const baseName = (/^(.*)\.json/).exec(jsonFile)[1];
+    if (baseName) {
+        console.log(baseName);
+        const jsonStr = JSON.stringify(json, null, ' ');
+        const fName = baseName + '_pretty.json';
+        fs.writeFileSync(fName, jsonStr);
+        console.log('Prettified output written to ' + fName);
     }
-    throw e;
+    else {
+        console.log('Cannot extract base name (before .json)');
+
+    }
 }
 
-if (game) {
-    game.update({code: RG.VK_PERIOD});
+function tryToLoadFile(jsonFile) {
+    const buf = fs.readFileSync(jsonFile);
+    const json = JSON.parse(buf);
+
+    let allJSON = fs.readdirSync('save_dumps/');
+    allJSON = allJSON.sort();
+    console.log(allJSON);
+
+    let game = null;
+
+    try {
+        const fromJSON = new RG.Game.FromJSON();
+        game = fromJSON.createGame(json);
+    }
+    catch (e) {
+        if (opts.delete) {
+            console.log('Deleting invalid save file ' + jsonFile);
+            fs.unlinkSync(jsonFile);
+        }
+        throw e;
+    }
+
+    if (game) {
+        game.update({code: RG.VK_PERIOD});
+    }
 }
