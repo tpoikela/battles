172ff513840a70f092d76fa826f9df1de31194ec
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-02 21:42:29 +0200

    Moved ConfStack code into separate file.

--- client/src/factory.js
+++ client/src/factory.js
@@ -4,6 +4,7 @@ import Constraints from './constraints';
 
 const RG = require('./rg.js');
 const debug = require('debug')('bitn:factory');
+const ConfStack = require('./conf-stack');
 
 RG.Actor = require('./actor');
 RG.Component = require('./component.js');
@@ -1001,72 +1002,6 @@ RG.Factory.Zone = function() {
 };
 RG.extend2(RG.Factory.Zone, RG.Factory.Base);
 
-/* An Object for managing configuration/scope stacks when creating a world. */
-const ConfStack = function() {
-
-    this.globalConf = {};
-    this.scope = [];
-    this.confStack = [];
-
-    this.setGlobalConf = function(conf) {
-        this.globalConf = conf;
-    };
-
-    this.getGlobalConf = function() {
-        return this.globalConf;
-    };
-
-    this.getScope = function() {
-        return this.scope;
-    };
-
-    /* Pushes the hier name and configuration on the stack. Config can be
-    * queried with getConf(). */
-    this.pushScope = function(conf) {
-        this.scope.push(conf.name);
-        this.confStack.push(conf);
-    };
-
-    /* Removes given config and the name it contains from stacks. Reports an
-    * error if removed name does not match the name in conf. */
-    this.popScope = function(conf) {
-        const name = conf.name;
-        const poppedName = this.scope.pop();
-        if (poppedName !== name) {
-            RG.err('Factory.ConfStack', 'popScope',
-                `Popped: ${poppedName}, Expected: ${name}`);
-        }
-        else {
-            const currConf = this.confStack.pop();
-            this.debug('Popped scope: ' + currConf.name);
-        }
-    };
-
-    /* Returns a config value. */
-    this.getConf = function(keys) {
-        // First travel the config stack from the top
-        for (let i = this.confStack.length - 1; i >= 0; i--) {
-            if (this.confStack[i].hasOwnProperty(keys)) {
-                return this.confStack[i][keys];
-            }
-        }
-
-        // If nothing found, try the global configuration
-        if (this.globalConf.hasOwnProperty(keys)) {
-            return this.globalConf[keys];
-        }
-
-        return null;
-    };
-
-    this.debug = function(msg) {
-        if (debug.enabled) {
-            console.log(msg);
-        }
-    };
-
-};
-RG.Factory.ConfStack = ConfStack;
 
 /* Factory object for creating worlds and zones. Uses conf object which is
  * somewhat involved. For an example, see ../data/conf.world.js. This Factory
