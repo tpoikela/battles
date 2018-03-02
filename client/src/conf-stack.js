2d1da08ebdd71e0e9ad5f88fe35b127feaed1005
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-02 21:42:17 +0200

    Moved ConfStack code into separate file.

---
+++ client/src/conf-stack.js
@@ -0,0 +1,71 @@
+
+const RG = require('./rg.js');
+const debug = require('debug')('bitn:ConfStack');
+
+/* An Object for managing configuration/scope stacks when creating a world. */
+const ConfStack = function() {
+
+    this.globalConf = {};
+    this.scope = [];
+    this.confStack = [];
+
+    this.setGlobalConf = function(conf) {
+        this.globalConf = conf;
+    };
+
+    this.getGlobalConf = function() {
+        return this.globalConf;
+    };
+
+    this.getScope = function() {
+        return this.scope;
+    };
+
+    /* Pushes the hier name and configuration on the stack. Config can be
+    * queried with getConf(). */
+    this.pushScope = function(conf) {
+        this.scope.push(conf.name);
+        this.confStack.push(conf);
+    };
+
+    /* Removes given config and the name it contains from stacks. Reports an
+    * error if removed name does not match the name in conf. */
+    this.popScope = function(conf) {
+        const name = conf.name;
+        const poppedName = this.scope.pop();
+        if (poppedName !== name) {
+            RG.err('Factory.ConfStack', 'popScope',
+                `Popped: ${poppedName}, Expected: ${name}`);
+        }
+        else {
+            const currConf = this.confStack.pop();
+            this.debug('Popped scope: ' + currConf.name);
+        }
+    };
+
+    /* Returns a config value. */
+    this.getConf = function(keys) {
+        // First travel the config stack from the top
+        for (let i = this.confStack.length - 1; i >= 0; i--) {
+            if (this.confStack[i].hasOwnProperty(keys)) {
+                return this.confStack[i][keys];
+            }
+        }
+
+        // If nothing found, try the global configuration
+        if (this.globalConf.hasOwnProperty(keys)) {
+            return this.globalConf[keys];
+        }
+
+        return null;
+    };
+
+    this.debug = function(msg) {
+        if (debug.enabled) {
+            console.log(msg);
+        }
+    };
+
+};
+
+module.exports = ConfStack;
