6ed161467557b1b124347e8cdc0934c6d9e7ae0a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-05 21:20:29 +0200

    Added processing of BattleOver component.

--- client/src/system.js
+++ client/src/system.js
@@ -1928,45 +1928,59 @@ RG.System.Battle = function(compTypes) {
     RG.System.Base.call(this, RG.SYS.BATTLE, compTypes);
 
     this.updateEntity = function(ent) {
-        const comp = ent.get('BattleOver');
-        if (ent.has('BattleExp')) {
-            const data = ent.get('BattleExp').getData();
-            const bName = data.name;
-            const badge = this._getBadgeForBattle(bName, ent);
-            if (data.kill > 0) {
-                addSkillsExp(ent, 'Battle', data.kill);
-                if (badge) {
-                    badge.updateData({kill: data.kill});
-                }
-                else {
-                    const msg = `No badge found for battle ${msg}`;
-                    RG.err('System.Battle', 'updateEntity', msg);
+        if (ent.has('BattleOver')) {
+            const comp = ent.get('BattleOver');
+            if (ent.has('BattleExp')) {
+                const data = ent.get('BattleExp').getData();
+                const bName = data.name;
+                const badge = this._getBadgeForBattle(bName, ent);
+                if (data.kill > 0) {
+                    addSkillsExp(ent, 'Battle', data.kill);
+                    if (badge) {
+                        badge.updateData({kill: data.kill});
+                    }
+                    else {
+                        const msg = `No badge found for battle ${msg}`;
+                        RG.err('System.Battle', 'updateEntity', msg);
+                    }
+
                 }
 
+                // Add some reputation for winner
+                if (badge.isWon()) {
+                    let rep = null;
+                    if (!ent.has('Reputation')) {
+                        rep = new RG.Component.Reputation();
+                        ent.add(rep);
+                    }
+                    else {
+                        rep = ent.get('Reputation');
+                    }
+                    rep.addToFame(1);
+                }
+
+                ent.remove('BattleExp');
             }
-
-            // Add some reputation for winner
-            if (badge.isWon()) {
-                let rep = null;
-                if (!ent.has('Reputation')) {
-                    rep = new RG.Component.Reputation();
-                    ent.add(rep);
-                }
-                else {
-                    rep = ent.get('Reputation');
-                }
-                rep.addToFame(1);
-            }
-
-            ent.remove('BattleExp');
+            ent.remove(comp);
         }
-        ent.remove(comp);
+        else if (ent.has('BattleOrder')) {
+            const orderComp = ent.get('BattleOrder');
+            this._emitMsg(ent, orderComp);
+            ent.remove(orderComp);
+        }
     };
 
     this._getBadgeForBattle = (bName, ent) => {
         const badges = ent.getList('BattleBadge');
         const badge = badges.find(b => b.getData().name === bName);
         return badge;
+    };
+
+    this._emitMsg = (ent, comp) => {
+        const srcName = comp.getArgs().srcActor;
+        const cell = ent.getCell();
+        const msg = `${srcName} shouts into your direction.`;
+        RG.gameMsg({msg, cell});
     };
 };
 RG.extend2(RG.System.Battle, RG.System.Base);
