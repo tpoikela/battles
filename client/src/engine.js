173afbcb93bc7c5bd49eef30087756be8b796664
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-05 21:17:52 +0200

    Added BattleOrder component.

--- client/src/engine.js
+++ client/src/engine.js
@@ -61,7 +61,7 @@ const Engine = function(eventPool) {
     this.systems.Animation = new RG.System.Animation(
         ['Animation']);
     this.systems.Damage = new RG.System.Damage(['Damage', 'Health']);
-    this.systems.Battle = new RG.System.Battle(['BattleOver']);
+    this.systems.Battle = new RG.System.Battle(['BattleOver', 'BattleOrder']);
     this.systems.Skills = new RG.System.Skills(['SkillsExp']);
     this.systems.ExpPoints = new RG.System.ExpPoints(
         ['ExpPoints', 'Experience']);
