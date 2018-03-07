6298212fcd5dc985fff93e83757ee226b01a84d5
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-07 15:16:02 +0200

    Added handleClick(). Added hasKeys(). Handles now storing of key buffer.

--- client/gui/cell-click-handler.js
+++ client/gui/cell-click-handler.js
@@ -3,8 +3,42 @@ const RG = require('../src/rg');
 const Path = require('../src/path');
 
 const dirToKeyCode = RG.KeyMap.dirToKeyCode;
-/* This class handles various actions when player clicks a cell. */
+
+/* This class handles various actions when player clicks a cell.
+ * It creates a key buffer corresponding to the automated command, and then game
+ * loop can request these keys.
+ * */
 export default class CellClickHandler {
+
+    constructor(game) {
+        this._game = game;
+        this._keyBuffer = []; // Stores keys for the pending command
+    }
+
+    /* Returns the next keycode or null if buffer is empty. */
+    getNextCode() {
+        if (this._keyBuffer.length > 0) {
+            return this._keyBuffer.shift();
+        }
+        return null;
+    }
+
+    hasKeys() {
+        return this._keyBuffer.length > 0;
+    }
+
+    handleClick(x, y, cell) {
+        const player = this._game.getPlayer();
+        const map = player.getLevel().getMap();
+        if (map.hasXY(x, y)) {
+            if (cell.hasActors()) {
+                this.moveTo(player, x, y);
+            }
+            else {
+                this.moveTo(player, x, y);
+            }
+        }
+    }
 
     /* Tries to compute a path to given coordinate. Uses 2 different methods. */
     moveTo(player, toX, toY) {
@@ -12,6 +46,12 @@ export default class CellClickHandler {
         let [pX, pY] = [player.getX(), player.getY()];
         let pathPossible = true;
         const map = player.getLevel().getMap();
+
+        if (!map.isExplored(toX, toY)) {
+            this._keyBuffer = [];
+            RG.gameMsg({msg: 'Cannot move to unexplored cell.'});
+            return;
+        }
 
         console.log(`STARTING POS: ${pX},${pY}`);
 
@@ -76,7 +116,7 @@ export default class CellClickHandler {
         }
 
         console.log('Returning ' + keyBuf.length + ' moves');
-        return keyBuf;
+        this._keyBuffer = keyBuf;
     }
 
 }
