73a18b91796febe662b1c67d73dfa6fda92f7539
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-11 22:56:19 +0100

    Added build sass.

--- bin/build_production.sh
+++ bin/build_production.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 
+npm run build:sass
 npm run create-config-prod && NODE_ENV=production ./browserify ./client/jsx/app.jsx --extension .jsx -t babelify | uglifyjs --mangle --compress > ./build/bundle.js
