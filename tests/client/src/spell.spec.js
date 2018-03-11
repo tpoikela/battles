ef3b7ba9778ccf122ea24cd3e7fc1303b8faecf1
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-11 23:00:51 +0100

    Added test to check that AI works correctly with Spell.LightningArrow.

--- tests/client/src/spell.spec.js
+++ tests/client/src/spell.spec.js
@@ -1,7 +1,11 @@
 
-const expect = require('chai').expect;
+const chai = require('chai');
 const RG = require('../../../client/src/battles');
 const RGTest = require('../../roguetest');
+const chaiBattles = require('../../helpers/chai-battles.js');
+
+const expect = chai.expect;
+chai.use(chaiBattles);
 
 describe('Spell.IcyPrison', () => {
 
@@ -24,5 +28,28 @@ describe('Spell.IcyPrison', () => {
 
         effSystem.update();
         expect(paralyzed.has('Paralysis')).to.be.true;
+    });
+});
+
+describe('Spell.LightningArrow', () => {
+    it('can be cast by AI', () => {
+        const castSystem = new RG.System.SpellCast(['SpellCast']);
+        const effSystem = new RG.System.SpellEffect(['SpellMissile']);
+        const systems = [castSystem, effSystem];
+
+        const parser = RG.ObjectShell.getParser();
+        const thunderbird = parser.createActor('thunderbird');
+        const human = parser.createActor('human');
+        RGTest.wrapIntoLevel([thunderbird, human]);
+        RGTest.moveEntityTo(thunderbird, 1, 1);
+        RGTest.moveEntityTo(human, 5, 5);
+
+        // Adjust evaluators and casting probability
+        RGTest.ensureSpellCast(thunderbird);
+
+        thunderbird.nextAction();
+        expect(thunderbird).to.have.component('SpellCast');
+        RGTest.updateSystems(systems);
+
     });
 });
