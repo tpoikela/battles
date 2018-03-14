06172df60d7709dbe667dca6105eb2ba913d508d
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-14 19:52:58 +0100

    Added two unit tests for testing segmented path generation.

--- tests/client/src/path.spec.js
+++ tests/client/src/path.spec.js
@@ -16,4 +16,31 @@ describe('Path', () => {
         expect(path.length).to.equal(0);
 
     });
+
+    it('has function to compute segmented path lengths', () => {
+        const segments1 = RG.Path.getPathSeg(16, 4);
+        expect(segments1).deep.to.equal([4, 4, 4, 4]);
+
+        const segments2 = RG.Path.getPathSeg(15, 4);
+        expect(segments2).deep.to.equal([3, 3, 3, 6]);
+
+        const segments3 = RG.Path.getPathSeg(17, 4);
+        expect(segments3).deep.to.equal([4, 4, 4, 5]);
+
+    });
+
+    it('has function to compute segmented paths', () => {
+        const map = RG.FACT.createLevel('arena', 30, 30).getMap();
+        const [x0, y0, x1, y1] = [1, 1, 10, 10];
+        for (let i = 1; i < 10; i++) {
+            const nSeg = i;
+            const path = Path.getWeightPathSegmented(map, x0, y0, x1, y1, nSeg);
+            expect(path.length).to.be.above(7);
+
+            const lastCoord = path[path.length - 1];
+            expect(lastCoord).deep.to.equal({x: 10, y: 10});
+        }
+
+    });
+
 });
