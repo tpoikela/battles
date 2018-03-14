f1e321ebdd61cba2e279d19509072dfde2939f80
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-14 19:51:14 +0100

    Added a wrapper for GameBoard when used in editor. Map updates can be controlled with checkbox.

---
+++ client/editor/editor-game-board.jsx
@@ -0,0 +1,43 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import GameBoard from '../jsx/game-board';
+
+/* Wrapper for the GameBoard to disable map updates for big maps. */
+export default class EditorGameBoard extends React.Component {
+
+    shouldComponentUpdate(nextProps) {
+        if (nextProps.updateMap) {return true;}
+        return false;
+    }
+
+    render() {
+        const {screen} = this.props;
+        return (
+          <div>
+            <GameBoard
+              boardClassName={this.props.boardClassName}
+              charRows={screen.getCharRows()}
+              classRows={screen.getClassRows()}
+              endY={screen.endY}
+              onCellClick={this.props.onCellClick}
+              rowClass={this.props.rowClass}
+              sizeX={this.props.sizeX}
+              startX={0}
+              startY={0}
+              useRLE={this.props.useRLE}
+            />
+          </div>
+        );
+    }
+}
+
+EditorGameBoard.propTypes = {
+    updateMap: PropTypes.bool,
+    boardClassName: PropTypes.string,
+    screen: PropTypes.object,
+    onCellClick: PropTypes.func,
+    rowClass: PropTypes.string,
+    useRLE: PropTypes.bool,
+    sizeX: PropTypes.number
+};
