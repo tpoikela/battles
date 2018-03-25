9eb0966284258577af8ac6020ae942ef9c2fc0ec
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-03-25 11:36:09 +0200

    Cleaning up.

--- client/gui/cell-click-handler.js
+++ client/gui/cell-click-handler.js
@@ -53,8 +53,6 @@ export default class CellClickHandler {
             return;
         }
 
-        console.log(`STARTING POS: ${pX},${pY}`);
-
         // Try to move diagonals first
         while (pX !== toX && pY !== toY) {
             const dx = toX - pX;
@@ -62,7 +60,6 @@ export default class CellClickHandler {
             keyBuf.push(dirToKeyCode(dx, dy));
             pX += dx / Math.abs(dx);
             pY += dy / Math.abs(dy);
-            console.log('X,Y are  ' + pX + ',' + pY);
             if (!map.isPassable(pX, pY)) {
                 pathPossible = false;
                 break;
@@ -75,7 +72,6 @@ export default class CellClickHandler {
                 const dx = toX - pX;
                 keyBuf.push(dirToKeyCode(dx, 0));
                 pX += dx / Math.abs(dx);
-                console.log('toX Set ' + pX + ',' + pY);
                 if (!map.isPassable(pX, pY)) {
                     pathPossible = false;
                     break;
@@ -87,7 +83,6 @@ export default class CellClickHandler {
                 const dy = toY - pY;
                 keyBuf.push(dirToKeyCode(0, dy));
                 pY += dy / Math.abs(dy);
-                console.log('toY Set ' + pX + ',' + pY);
                 if (!map.isPassable(pX, pY)) {
                     pathPossible = false;
                     break;
@@ -97,25 +92,18 @@ export default class CellClickHandler {
 
         // Use path finder, if more difficult path to follow
         if (!pathPossible) {
-            console.log('XYZ Using Path.getShortest... now');
             [pX, pY] = [player.getX(), player.getY()];
             keyBuf = [];
             const path = Path.getShortestActorPath(map, pX, pY, toX, toY);
-            console.log('Full path is ' + JSON.stringify(path));
             path.forEach(xy => {
-                console.log(`\tPath px,py: ${pX},${pY}`);
                 const dx = xy.x - pX;
                 const dy = xy.y - pY;
-                console.log(`\tPath dx,dy: ${dx},${dy}`);
                 keyBuf.push(dirToKeyCode(dx, dy));
                 if (dx !== 0) {pX += dx / Math.abs(dx);}
                 if (dy !== 0) {pY += dy / Math.abs(dy);}
             });
-
-            console.log('Comped dpath is ' + JSON.stringify(path));
         }
 
-        console.log('Returning ' + keyBuf.length + ' moves');
         this._keyBuffer = keyBuf;
     }
 
