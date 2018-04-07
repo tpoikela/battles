7aebad54a66474f79f59861b5d8d26c81177e78d
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-04-07 23:21:51 +0200

    Added object allMenuItems which is used to define menu items in different contexts.

--- client/jsx/context-menu.jsx
+++ client/jsx/context-menu.jsx
@@ -1,8 +1,39 @@
-
+/* eslint comma-dangle: 0 */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import {ContextMenu, MenuItem} from 'react-contextmenu';
+
+/* Menu items are defined as follows:
+ *   The first key defines a getter/query function for a cell. If that function
+ *   returns true, all items inside the corresponding array are shown.
+ */
+const allMenuItems = {
+  isPassable: [
+    {text: 'Move', type: 'move'},
+  ],
+  hasActors: [
+    {text: 'Attack', type: 'attack'},
+    {text: 'Chat', type: 'chat'},
+    {text: 'Order', type: 'order'},
+  ],
+  hasDoor: [
+    {text: 'Open/close', type: 'door'},
+  ],
+  hasItems: [
+    {text: 'Pick up', type: 'pickup'},
+  ],
+  hasPassage: [
+    {text: 'Travel', type: 'usestairs'},
+  ],
+  hasShop: [
+    {text: 'Buy item', type: 'buyitem'},
+    {text: 'Sel item', type: 'sellitem'},
+  ],
+  hasStairs: [
+    {text: 'Stairs', type: 'usestairs'},
+  ]
+};
 
 export default class GameContextMenu extends React.Component {
 
@@ -18,30 +49,53 @@ export default class GameContextMenu extends React.Component {
   }
 
   render() {
+    const menuItemElem = this.renderMenuItems();
     return (
       <div>
         <ContextMenu className='context-menu' id='right-click-context-menu'>
-          <MenuItem data={{type: 'attack'}} onClick={this.handleClick}>
-            Attack
-          </MenuItem>
-          <MenuItem data={{type: 'shoot'}} onClick={this.handleClick}>
-            Shoot
-          </MenuItem>
-          <MenuItem data={{type: 'pickup'}} onClick={this.handleClick}>
-            Pick up
-          </MenuItem>
-          <MenuItem divider={true} />
-          <MenuItem data={{type: 'move'}} onClick={this.handleClick}>
-            Move
-          </MenuItem>
-          <MenuItem data={{type: 'order'}} onClick={this.handleClick}>
-            Order
-          </MenuItem>
+          {menuItemElem}
         </ContextMenu>
-
       </div>
     );
   }
+
+  /* Calls different query functions and renders possible commands based on
+   * the cell contents. */
+  renderMenuItems() {
+    const items = [];
+    Object.keys(allMenuItems).forEach(queryFunc => {
+      if (this.isCorrectContext(queryFunc)) {
+        const menuItems = allMenuItems[queryFunc];
+        menuItems.forEach(item => {
+          items.push(
+            <MenuItem
+              data={{type: item.type}}
+              key={item.text}
+              onClick={this.handleClick}
+            >
+              {item.text}
+            </MenuItem>
+          );
+        });
+        items.push(<MenuItem divider={true} />);
+      }
+    });
+    return items;
+  }
+
+  isCorrectContext(queryFunc) {
+    const cell = this.props.mouseOverCell;
+    if (cell) {
+      if (typeof cell[queryFunc] === 'function') {
+        return cell[queryFunc]();
+      }
+      else if (typeof this[queryFunc] === 'function') {
+        return this[queryFunc]();
+      }
+    }
+    return false;
+  }
+
 }
 
 GameContextMenu.propTypes = {
