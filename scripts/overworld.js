7b097e304fa7cb5ab468cc2553c8258917942fd0
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-04-07 18:13:46 +0200

    Adjusted the parameters.

--- scripts/overworld.js
+++ scripts/overworld.js
@@ -35,14 +35,14 @@ conf = {
     // nHWalls: 2,
     // nVWalls: 4,
     nVWalls: [0.8],
-    owTilesX: mult * 80,
-    owTilesY: mult * 80,
-    worldX: mult * 800,
-    worldY: mult * 800,
-    nLevelsX: mult * 8,
-    nLevelsY: mult * 8,
-    areaX: mult * 8,
-    areaY: mult * 8,
+    owTilesX: mult * 40,
+    owTilesY: mult * 40,
+    worldX: mult * 400,
+    worldY: mult * 400,
+    nLevelsX: mult * 4,
+    nLevelsY: mult * 4,
+    areaX: mult * 4,
+    areaY: mult * 4,
     printResult: true
 };
 
