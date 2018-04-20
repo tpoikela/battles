c389e1d828fc9a205d95ed1168a978df8a5ba281
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-04-21 00:25:55 +0200

    Added a test to check that switching tab works.

--- tests/client/jsx/game-overworld-map.spec.jsx
+++ tests/client/jsx/game-overworld-map.spec.jsx
@@ -1,10 +1,14 @@
 
 import React from 'react';
-import { shallow, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import chaiEnzyme from 'chai-enzyme';
 import chai, { expect } from 'chai';
+// import Modal from 'react-modal';
 
+import GameBoard from '../../../client/jsx/game-board';
 import GameOverworldMap from '../../../client/jsx/game-overworld-map';
+
+const OW = require('../../../client/src/overworld.map');
 
 chai.use(chaiEnzyme());
 
@@ -17,5 +21,29 @@ describe('Component <GameOverworldMap>', () => {
     it('should render', () => {
         const wrapper = shallow(<GameOverworldMap {...props} />);
         expect(wrapper, 'Component must render with shallow').to.have.length(1);
+    });
+
+
+    it('has multiple tabs to shown', () => {
+        let toggled = false;
+        const conf = {
+            owTilesX: 40,
+            owTilesY: 20
+        };
+        const props = {
+            ow: OW.createOverWorld(conf),
+            toggleScreen: () => {toggled = !toggled;},
+            playerOwPos: [5, 5],
+            showOWMap: true
+        };
+        const wrapper = shallow(<GameOverworldMap {...props} />);
+        let board = wrapper.find(GameBoard);
+        expect(board).to.have.length(0);
+
+        wrapper.setState({tabShown: 'Region'});
+        wrapper.update();
+
+        board = wrapper.find(GameBoard);
+        expect(board).to.have.length(1);
     });
 });
