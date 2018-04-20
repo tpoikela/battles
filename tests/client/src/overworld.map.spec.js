9a4422182bd38ff9266d5a1f467f421cfd327761
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-04-20 18:08:49 +0200

    Added a unit test for OWMap.getCellList().

--- tests/client/src/overworld.map.spec.js
+++ tests/client/src/overworld.map.spec.js
@@ -1,6 +1,8 @@
 
 const expect = require('chai').expect;
+const RG = require('../../../client/src/battles');
 const OW = require('../../../client/src/overworld.map');
+const Screen = require('../../../client/gui/screen');
 
 describe('OW.Map', () => {
     it('can be created', () => {
@@ -30,5 +32,31 @@ describe('OW.Map', () => {
 
         const features = ow.getFeaturesByType(OW.WCAPITAL);
         expect(features).to.have.length(1);
+    });
+
+    it('can be constructed as Map.CellList', function() {
+        const conf = {
+            owTilesX: 40,
+            owTilesY: 20
+        };
+        const ow = OW.createOverWorld(conf);
+        const map = ow.getCellList();
+        map.debugPrintInASCII();
+        map.getCells(c => {
+            c._explored = true;
+        });
+
+        const player = new RG.Element.Marker('@');
+        map.getCell(10, 5).removeProps(RG.TYPE_ELEM);
+        map.getCell(10, 5).setProp(RG.TYPE_ELEM, player);
+
+        const screen = new Screen(15, 7);
+        screen.renderAllVisible(5, 5, map);
+        screen.printRenderedChars();
+
+        map.getCell(20, 10).removeProps(RG.TYPE_ELEM);
+        map.getCell(20, 10).setProp(RG.TYPE_ELEM, player);
+        screen.renderAllVisible(20, 10, map);
+        screen.printRenderedChars();
     });
 });
