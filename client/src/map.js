0a2bbc2f5683a88dded77062280e13517c0c0486
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-04-20 08:33:55 +0200

    Moved functions in Map.CellList to prototype.

--- client/src/map.js
+++ client/src/map.js
@@ -383,7 +383,6 @@ RG.Map.CellList = function(cols, rows, baseElem = RG.ELEM.FLOOR) { // {{{2
     this.cols = cols;
     this.rows = rows;
 
-
     if (typeof this.cols !== 'number' || typeof this.rows !== 'number') {
         RG.err('Map.CellList', 'constructor',
             'Map.CellList(rows, cols) expects 2 integers.');
@@ -399,261 +398,261 @@ RG.Map.CellList = function(cols, rows, baseElem = RG.ELEM.FLOOR) { // {{{2
         }
     }
 
-    /* Returns true if x,y are in the this._map.*/
-    this.hasXY = function(x, y) {
-        return (x >= 0) && (x < this.cols) && (y >= 0) && (y < this.rows);
-    };
-
-    /* Sets a property for the underlying cell.*/
-    this.setProp = function(x, y, prop, obj) {
-        this._map[x][y].setProp(prop, obj);
-    };
-
-    this.removeProp = function(x, y, prop, obj) {
-        return this._map[x][y].removeProp(prop, obj);
-    };
-
-    this.setElemXY = function(x, y, obj) {
-        this.setProp(x, y, RG.TYPE_ELEM, obj);
-    };
-
-    this.setBaseElemXY = function(x, y, elem) {
-        this._map[x][y].setBaseElem(elem);
-    };
-
-    this.getBaseElemXY = function(x, y) {
-        return this._map[x][y].getBaseElem();
-    };
-
-    this.getCell = function(x, y) {
-        return this._map[x][y];
-    };
-
-    this.isExplored = function(x, y) {
-        return this._map[x][y].isExplored();
-    };
-
-    this.getBaseElemRow = function(y) {
-        const row = [];
-        for (let i = 0; i < this.cols; ++i) {
-            row.push(this._map[i][y].getBaseElem());
-        }
-        return row;
-    };
-
-    this.getCellRow = function(y) {
-        const row = [];
-        for (let i = 0; i < this.cols; ++i) {
-            row.push(this._map[i][y]);
-        }
-        return row;
-    };
-
-    /* Returns all free cells in the this._map. 'free' means that cell can be
-    * traversed and is passable. */
-    this.getFree = function() {
-        const freeCells = [];
-        for (let x = 0; x < this.cols; x++) {
-            for (let y = 0; y < this.rows; y++) {
-                if (this._map[x][y].isFree()) {
-                    freeCells.push(this._map[x][y]);
-                }
-            }
-        }
-        return freeCells;
-    };
-
-    this.getFreeNotOnEdge = function() {
-        const freeCells = this.getFree();
-        return freeCells.filter(c => (
-            c._x > 0 && c._x < (this.cols - 1) &&
-            c._y > 0 && c._y < (this.rows - 1)
-        ));
-    };
-
-    /* Returns the first free cell starting from right edge of the level.
-    * Range of y-coord can be given, if not, searches all y-coordinates starting
-    * from 0.
-    */
-    this.getFirstFreeFromRight = function(y0 = 0, y1 = this.rows - 1) {
-        for (let x = this.cols - 1; x >= 0; x--) {
-            for (let y = y0; y <= y1; y++) {
-                if (this._map[x][y].isFree()) {
-                    return this._map[x][y];
-                }
-            }
-        }
-        return null;
-    };
-
-    /* Returns all free cells in the given bounding box. */
-    this.getFreeInBbox = function(bbox) {
-        const freeCells = [];
-        for (let x = bbox.ulx; x <= bbox.lrx; x++) {
-            for (let y = bbox.uly; y < bbox.lry; y++) {
-                if (this._map[x][y].isFree()) {
-                    freeCells.push(this._map[x][y]);
-                }
-            }
-        }
-        return freeCells;
-    };
-
-    /* Returns all empty cells. Cell is empty, if it has only the base
-     * element, but no props. */
-    this.getEmptyCells = function() {
-        const emptyCells = [];
-        for (let x = 0; x < this.cols; x++) {
-            for (let y = 0; y < this.rows; y++) {
-                if (!this._map[x][y].hasProps()) {
-                    emptyCells.push(this._map[x][y]);
-                }
-            }
-        }
-        return emptyCells;
-    };
-
-    /* Returns true if the this._map has a cell in given x,y location.*/
-    this._hasXY = (x, y) => (
-        (x >= 0) && (x < this.cols) && (y >= 0) && (y < this.rows)
-    );
-
-    /* Returns true if light passes through this cell.*/
-    this.lightPasses = function(x, y) {
-        if (this._hasXY(x, y)) {
-            return this._map[x][y].lightPasses(); // delegate to cell
-        }
-        return false;
-    };
-
-    this.hasObstacle = function(x, y) {
-        if (this._hasXY(x, y)) {
-            return this._map[x][y].hasObstacle();
-        }
-        return false;
-    };
-
-    this.isPassable = function(x, y) {
-        if (this._hasXY(x, y)) {
-            return this._map[x][y].isPassable();
-        }
-        return false;
-    };
-
-    this.isPassableByAir = function(x, y) {
-        if (this._hasXY(x, y)) {
-            return this._map[x][y].isPassableByAir();
-        }
-        return false;
-    };
-
     this.fov = new ROT.FOV.RecursiveShadowcasting(
         this.lightPasses.bind(this));
+};
 
-    /* Returns visible cells for given actor.*/
-    this.getVisibleCells = function(actor) {
-        const cells = [];
-        const [xA, yA] = actor.getXY();
-        if (actor.isLocated()) {
-            if (actor.getLevel().getMap() === this) {
-                const range = actor.getFOVRange();
-                this.fov.compute(xA, yA, range, (x, y, r, visibility) => {
-                    if (visibility) {
-                        if (this._hasXY(x, y)) {
-                            cells.push(this._map[x][y]);
-                        }
-                    }
-                });
-            }
-        }
-        return cells;
-    };
+/* Returns true if x,y are in the this._map.*/
+RG.Map.CellList.prototype.hasXY = function(x, y) {
+    return (x >= 0) && (x < this.cols) && (y >= 0) && (y < this.rows);
+};
 
-    /* Returns all cells explored by the player.*/
-    this.getExploredCells = function() {
-        const cells = [];
-        for (let x = 0; x < this.cols; x++) {
-            for (let y = 0; y < this.rows; y++) {
-                if (this._map[x][y].isExplored()) {
-                    cells.push(this._map[x][y]);
-                }
-            }
-        }
-    };
+/* Sets a property for the underlying cell.*/
+RG.Map.CellList.prototype.setProp = function(x, y, prop, obj) {
+    this._map[x][y].setProp(prop, obj);
+};
 
-    /* Returns true if x,y is located at this._map border cells.*/
-    this.isBorderXY = function(x, y) {
-        if (x === 0) {return true;}
-        if (y === 0) {return true;}
-        if (x === this.cols - 1) {return true;}
-        if (y === this.rows - 1) {return true;}
-        return false;
-    };
+RG.Map.CellList.prototype.removeProp = function(x, y, prop, obj) {
+    return this._map[x][y].removeProp(prop, obj);
+};
 
-    /* Prints the this._map in ASCII. */
-    this.debugPrintInASCII = function() {
-        let mapInASCII = '';
+RG.Map.CellList.prototype.setElemXY = function(x, y, obj) {
+    this.setProp(x, y, RG.TYPE_ELEM, obj);
+};
+
+RG.Map.CellList.prototype.setBaseElemXY = function(x, y, elem) {
+    this._map[x][y].setBaseElem(elem);
+};
+
+RG.Map.CellList.prototype.getBaseElemXY = function(x, y) {
+    return this._map[x][y].getBaseElem();
+};
+
+RG.Map.CellList.prototype.getCell = function(x, y) {
+    return this._map[x][y];
+};
+
+RG.Map.CellList.prototype.isExplored = function(x, y) {
+    return this._map[x][y].isExplored();
+};
+
+RG.Map.CellList.prototype.getBaseElemRow = function(y) {
+    const row = [];
+    for (let i = 0; i < this.cols; ++i) {
+        row.push(this._map[i][y].getBaseElem());
+    }
+    return row;
+};
+
+RG.Map.CellList.prototype.getCellRow = function(y) {
+    const row = [];
+    for (let i = 0; i < this.cols; ++i) {
+        row.push(this._map[i][y]);
+    }
+    return row;
+};
+
+/* Returns all free cells in the this._map. 'free' means that cell can be
+* traversed and is passable. */
+RG.Map.CellList.prototype.getFree = function() {
+    const freeCells = [];
+    for (let x = 0; x < this.cols; x++) {
         for (let y = 0; y < this.rows; y++) {
-            let row = '';
-            for (let x = 0; x < this.cols; x++) {
-                const cell = this._map[x][y];
-                const baseType = cell.getBaseElem().getType();
-                if (cell.hasActors()) {
-                    if (cell.getFirstActor().isPlayer()) {
-                        row += '@';
-                    }
-                    else {
-                        row += 'A';
-                    }
-                }
-                else if (cell.hasItems()) {row += 'I';}
-                else if (cell.getStairs() !== null) {row += '>';}
-                else if (cell.hasConnection()) {row += 'c';}
-                else if (cell.hasElements()) {
-                    const elem = cell.getElements()[0];
-                    if (elem.getType() === 'marker') {
-                        row += elem.char;
-                    }
-                    else if (elem.getType() === 'door') {
-                        row += '+';
-                    }
-                    else {
-                        row += 'E';
-                    }
-                }
-                else if ((/floor/).test(baseType)) {row += '.';}
-                else if ((/water|chasm|lava/).test(baseType)) {row += '~';}
-                else if ((/wall/).test(baseType)) {row += '#';}
-                else if ((/tree/).test(baseType)) {row += 'T';}
-                else if ((/grass/).test(baseType)) {row += '"';}
-                else {row += '?';}
+            if (this._map[x][y].isFree()) {
+                freeCells.push(this._map[x][y]);
             }
-            mapInASCII += row + '\n';
         }
-        RG.diag(mapInASCII);
-    };
+    }
+    return freeCells;
+};
 
-    /* Queries a row of cells. _optimizeForRowAccess must be called before this
-     * function is used. */
-    this.getCellRowFast = function(y) {
-        if (!this._isRowOptimized) {this._optimizeForRowAccess();}
-        return this._rowMap[y];
-    };
+RG.Map.CellList.prototype.getFreeNotOnEdge = function() {
+    const freeCells = this.getFree();
+    return freeCells.filter(c => (
+        c._x > 0 && c._x < (this.cols - 1) &&
+        c._y > 0 && c._y < (this.rows - 1)
+    ));
+};
 
-    /* Slow find for debugging. Tries to find all objects matching the
-     * filterFunc. */
-    this.findObj = function(filterFunc) {
-        let result = [];
+/* Returns the first free cell starting from right edge of the level.
+* Range of y-coord can be given, if not, searches all y-coordinates starting
+* from 0.
+*/
+RG.Map.CellList.prototype.getFirstFreeFromRight = function(
+    y0 = 0, y1 = this.rows - 1
+) {
+    for (let x = this.cols - 1; x >= 0; x--) {
+        for (let y = y0; y <= y1; y++) {
+            if (this._map[x][y].isFree()) {
+                return this._map[x][y];
+            }
+        }
+    }
+    return null;
+};
+
+/* Returns all free cells in the given bounding box. */
+RG.Map.CellList.prototype.getFreeInBbox = function(bbox) {
+    const freeCells = [];
+    for (let x = bbox.ulx; x <= bbox.lrx; x++) {
+        for (let y = bbox.uly; y < bbox.lry; y++) {
+            if (this._map[x][y].isFree()) {
+                freeCells.push(this._map[x][y]);
+            }
+        }
+    }
+    return freeCells;
+};
+
+/* Returns all empty cells. Cell is empty, if it has only the base
+ * element, but no props. */
+RG.Map.CellList.prototype.getEmptyCells = function() {
+    const emptyCells = [];
+    for (let x = 0; x < this.cols; x++) {
+        for (let y = 0; y < this.rows; y++) {
+            if (!this._map[x][y].hasProps()) {
+                emptyCells.push(this._map[x][y]);
+            }
+        }
+    }
+    return emptyCells;
+};
+
+/* Returns true if the this._map has a cell in given x,y location.*/
+RG.Map.CellList.prototype._hasXY = function(x, y) {
+    return (x >= 0) && (x < this.cols) && (y >= 0) && (y < this.rows);
+};
+
+/* Returns true if light passes through this cell.*/
+RG.Map.CellList.prototype.lightPasses = function(x, y) {
+    if (this._hasXY(x, y)) {
+        return this._map[x][y].lightPasses(); // delegate to cell
+    }
+    return false;
+};
+
+RG.Map.CellList.prototype.hasObstacle = function(x, y) {
+    if (this._hasXY(x, y)) {
+        return this._map[x][y].hasObstacle();
+    }
+    return false;
+};
+
+RG.Map.CellList.prototype.isPassable = function(x, y) {
+    if (this._hasXY(x, y)) {
+        return this._map[x][y].isPassable();
+    }
+    return false;
+};
+
+RG.Map.CellList.prototype.isPassableByAir = function(x, y) {
+    if (this._hasXY(x, y)) {
+        return this._map[x][y].isPassableByAir();
+    }
+    return false;
+};
+
+/* Returns visible cells for given actor.*/
+RG.Map.CellList.prototype.getVisibleCells = function(actor) {
+    const cells = [];
+    const [xA, yA] = actor.getXY();
+    if (actor.isLocated()) {
+        if (actor.getLevel().getMap() === this) {
+            const range = actor.getFOVRange();
+            this.fov.compute(xA, yA, range, (x, y, r, visibility) => {
+                if (visibility) {
+                    if (this._hasXY(x, y)) {
+                        cells.push(this._map[x][y]);
+                    }
+                }
+            });
+        }
+    }
+    return cells;
+};
+
+/* Returns all cells explored by the player.*/
+RG.Map.CellList.prototype.getExploredCells = function() {
+    const cells = [];
+    for (let x = 0; x < this.cols; x++) {
+        for (let y = 0; y < this.rows; y++) {
+            if (this._map[x][y].isExplored()) {
+                cells.push(this._map[x][y]);
+            }
+        }
+    }
+};
+
+/* Returns true if x,y is located at this._map border cells.*/
+RG.Map.CellList.prototype.isBorderXY = function(x, y) {
+    if (x === 0) {return true;}
+    if (y === 0) {return true;}
+    if (x === this.cols - 1) {return true;}
+    if (y === this.rows - 1) {return true;}
+    return false;
+};
+
+/* Prints the this._map in ASCII. */
+RG.Map.CellList.prototype.debugPrintInASCII = function() {
+    let mapInASCII = '';
+    for (let y = 0; y < this.rows; y++) {
+        let row = '';
         for (let x = 0; x < this.cols; x++) {
-            for (let y = 0; y < this.rows; y++) {
-                result = result.concat(this._map[x][y].findObj(filterFunc));
+            const cell = this._map[x][y];
+            const baseType = cell.getBaseElem().getType();
+            if (cell.hasActors()) {
+                if (cell.getFirstActor().isPlayer()) {
+                    row += '@';
+                }
+                else {
+                    row += 'A';
+                }
             }
+            else if (cell.hasItems()) {row += 'I';}
+            else if (cell.getStairs() !== null) {row += '>';}
+            else if (cell.hasConnection()) {row += 'c';}
+            else if (cell.hasElements()) {
+                const elem = cell.getElements()[0];
+                if (elem.getType() === 'marker') {
+                    row += elem.char;
+                }
+                else if (elem.getType() === 'door') {
+                    row += '+';
+                }
+                else {
+                    row += 'E';
+                }
+            }
+            else if ((/floor/).test(baseType)) {row += '.';}
+            else if ((/water|chasm|lava/).test(baseType)) {row += '~';}
+            else if ((/wall/).test(baseType)) {row += '#';}
+            else if ((/tree/).test(baseType)) {row += 'T';}
+            else if ((/grass/).test(baseType)) {row += '"';}
+            else {row += '?';}
         }
-        return result;
-    };
+        mapInASCII += row + '\n';
+    }
+    RG.diag(mapInASCII);
+};
 
+/* Queries a row of cells. _optimizeForRowAccess must be called before this
+ * function is used. */
+RG.Map.CellList.prototype.getCellRowFast = function(y) {
+    if (!this._isRowOptimized) {this._optimizeForRowAccess();}
+    return this._rowMap[y];
+};
 
-}; // }}} this._map.CellList
+/* Slow find for debugging. Tries to find all objects matching the
+ * filterFunc. */
+RG.Map.CellList.prototype.findObj = function(filterFunc) {
+    let result = [];
+    for (let x = 0; x < this.cols; x++) {
+        for (let y = 0; y < this.rows; y++) {
+            result = result.concat(this._map[x][y].findObj(filterFunc));
+        }
+    }
+    return result;
+};
 
 /* Get cells that return true for the given filter function. For example:
  *   cell => cell.hasActors()
