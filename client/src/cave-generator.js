66fb7707cb371c3f4e00c61e5a56d0c73fd48aac
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-04-22 11:50:33 +0200

    Added _createMapOptions.

--- client/src/cave-generator.js
+++ client/src/cave-generator.js
@@ -1,5 +1,7 @@
 
-/* Contains code to generate various types of caverns in the game. */
+/* Contains code to generate various types of caverns in the game.
+ *
+ */
 
 const RG = require('./rg.js');
 
@@ -7,15 +9,38 @@ const CaveGenerator = function() {
 
 };
 
+const Miners = require('../../scripts/map.miner');
+
 CaveGenerator.prototype.create = function(cols, rows, conf) {
+    const level = this._createLevel(cols, rows, conf);
+    return level;
+};
+
+/* Creates the Map.Level object with walls/floor and cave-flavor. */
+CaveGenerator.prototype._createLevel = function(cols, rows, conf) {
+    const mapOpts = this._createMapOptions(conf);
     const mapgen = new RG.Map.Generator();
-    let mapObj = null;
     const level = new RG.Map.Level(cols, rows);
     mapgen.setGen('cave', cols, rows);
-    mapObj = mapgen.createCave(cols, rows, conf);
+    const mapObj = mapgen.createCave(cols, rows, mapOpts);
     level.setMap(mapObj.map);
     this.setLevelExtras(level, mapObj);
     return level;
+};
+
+CaveGenerator.prototype._createMapOptions = function(conf) {
+    const {dungeonType} = conf;
+    let opts = {};
+
+    switch (dungeonType) {
+        case 'Cave': opts = Miners.getRandOpts(1, 3); break;
+        case 'Grotto': opts = Miners.getRandOpts(2, 4); break;
+        case 'Lair': opts = Miners.getRandOpts(1, 1); break;
+        case 'Cavern': opts = Miners.getRandOpts(3, 9); break;
+        default: opts = Miners.getRandOpts();
+    }
+
+    return opts;
 };
 
 module.exports = CaveGenerator;
