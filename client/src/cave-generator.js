30713479f799d25f13745356e2f8b2436d60fc37
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-04-22 12:30:54 +0200

    Moved Miner code from scripts/map.miner.js to cave-generator.

--- client/src/cave-generator.js
+++ client/src/cave-generator.js
@@ -9,7 +9,7 @@ const CaveGenerator = function() {
 
 };
 
-const Miners = require('../../scripts/map.miner');
+const Miners = {};
 
 CaveGenerator.prototype.create = function(cols, rows, conf) {
     const level = this._createLevel(cols, rows, conf);
@@ -43,4 +43,103 @@ CaveGenerator.prototype._createMapOptions = function(conf) {
     return opts;
 };
 
-module.exports = CaveGenerator;
+/* Returns an object containing the base miners for different directions. */
+function getMiners(cols, rows, border = 1) {
+    const midX = Math.round(cols / 2);
+    const midY = Math.round(rows / 2);
+
+    // Need -2 to preserve wall border of level
+    const endX = cols - 1 - border;
+    const endY = rows - 1 - border;
+
+    const miners = {
+        N: {x: midX, y: 1, dirWeights: {E: 1, W: 1, S: 5, SE: 5, SW: 5}},
+        S: {x: midX, y: endY, dirWeights: {E: 1, W: 1, N: 5, NE: 5, NW: 5}},
+        E: {x: endX, y: midY, dirWeights: {N: 1, S: 1, NW: 5, W: 5, SW: 5}},
+        W: {x: 1, y: midY, dirWeights: {N: 1, S: 1, NE: 5, E: 5, SE: 5}},
+        NE: {x: endX, y: 1, dirWeights: {NW: 1, W: 10, SW: 5, S: 10}},
+        NW: {x: 1, y: 1, dirWeights: {NE: 1, E: 10, SE: 5, S: 10}},
+        SE: {x: endX, y: endY, dirWeights: {SW: 1, W: 10, NW: 5, N: 10}},
+        SW: {x: 1, y: endY, dirWeights: {SE: 1, E: 10, NE: 5, N: 10}},
+        C: { // Central miner, all equal weights
+            x: midX, y: midY,
+            dirWeights: {N: 1, S: 1, E: 2, W: 2, NE: 1, SE: 1, NW: 1, SW: 1}
+        }
+    };
+    return miners;
+}
+Miners.getMiners = getMiners;
+
+function getOptsWithMiners(miners) {
+    const firstMiner = miners[0];
+    const opts = {
+        startX: firstMiner.x, startY: firstMiner.y,
+        dirWeights: firstMiner.dirWeights
+    };
+    const addMiners = [];
+    for (let i = 1; i < miners.length; i++) {
+        addMiners.push(miners[i]);
+    }
+    opts.addMiners = addMiners;
+    return opts;
+}
+Miners.getOptsWithMiners = getOptsWithMiners;
+
+/* Returns map options with random number of miners. */
+function getRandOpts(cols, rows, min = 1, max = 9) {
+    const miners = getMiners(cols, rows);
+    const minerValues = Object.values(miners);
+
+    const nMiners = RG.RAND.getUniformInt(min, max);
+    const randMiners = [];
+    for (let i = 0; i < nMiners; i++) {
+        const randMiner = RG.RAND.arrayGetRand(minerValues);
+        randMiners.push(randMiner);
+    }
+    return getOptsWithMiners(randMiners);
+}
+Miners.getRandOpts = getRandOpts;
+
+/* Returns options with miners placed on corners. */
+function getMinersCorners(cols, rows, miners) {
+    if (!miners) {
+        miners = getMiners(cols, rows);
+    }
+    const minersCorners = {
+        cols, rows,
+        // maxMinersCreated: 100,
+        dirWeights: miners.NW.dirWeights,
+        addMiners: [
+            miners.SW,
+            miners.NE,
+            miners.SE
+        ],
+        startX: miners.NW.x, startY: miners.NW.y
+    };
+    return minersCorners;
+}
+Miners.getMinersCorners = getMinersCorners;
+
+/* Returns options containing miners in each cardinal direction NSEW. */
+function getMinersNSEW(cols, rows, miners) {
+    if (!miners) {
+        miners = getMiners(cols, rows);
+    }
+    const minersNSEW = {
+        cols: 100, rows: 100,
+        maxMinersCreated: 100,
+
+        startX: miners.N.x, startY: miners.N.y,
+        dirWeights: miners.N.dirWeights,
+
+        addMiners: [
+            miners.S,
+            miners.E,
+            miners.W
+        ]
+    };
+    return minersNSEW;
+}
+Miners.getMinersNSEW = getMinersNSEW;
+
+module.exports = {CaveGenerator, Miners};
--- scripts/map.miner.js
+++ scripts/map.miner.js
@@ -4,38 +4,22 @@ require('babel-core/register');
 const ROT = require('../lib/rot');
 const RG = require('../client/src/battles');
 const MapMiner = require('../lib/map.miner');
+RG.Map = require('../client/src/map');
+RG.Elem = require('../client/src/element');
+const {Miners} = require('../client/src/cave-generator');
 
 const cols = 200;
 const rows = 200;
 
-const midX = Math.round(cols / 2);
-const midY = Math.round(rows / 2);
-
-const miners = {
-    N: {x: midX, y: 1, dirWeights: {E: 1, W: 1, S: 5, SE: 5, SW: 5}},
-    S: {x: midX, y: rows - 2, dirWeights: {E: 1, W: 1, N: 5, NE: 5, NW: 5}},
-    E: {x: cols - 2, y: midY, dirWeights: {N: 1, S: 1, NW: 5, W: 5, SW: 5}},
-    W: {x: 1, y: midY, dirWeights: {N: 1, S: 1, NE: 5, E: 5, SE: 5}},
-    NE: {x: cols - 2, y: 1, dirWeights: {NW: 1, W: 10, SW: 5, S: 10}},
-    NW: {x: 1, y: 1, dirWeights: {NE: 1, E: 10, SE: 5, S: 10}},
-    SE: {x: cols - 2, y: rows - 2, dirWeights: {SW: 1, W: 10, NW: 5, N: 10}},
-    SW: {x: 1, y: rows - 2, dirWeights: {SE: 1, E: 10, NE: 5, N: 10}},
-    C: {
-        x: midX, y: midY,
-        dirWeights: {N: 1, S: 1, E: 1, W: 1, NE: 5, SE: 10, NW: 1, SW: 1}
-    }
-};
-
-// const dirs = Object.keys(miners);
-const minerValues = Object.values(miners);
-
+const miners = Miners.getMiners(cols, rows);
 
 // ROT.RNG.setSeed(1524319401126);
 // const dontDig = {ulx: 1, uly: 1, lrx: 20, lry: 20};
 
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 20; i++) {
 
-    const opts = getRandOpts();
+    // const opts = getRandOpts(1, 3);
+    const opts = Miners.getOptsWithMiners(Object.values(miners));
     const gen = new MapMiner(cols, rows, opts);
     const map = new RG.Map.CellList(cols, rows);
 
@@ -55,56 +39,3 @@ for (let i = 0; i < 50; i++) {
 
 }
 
-function getRandOpts() {
-    const nMiners = RG.RAND.getUniformInt(1, 9);
-    const firstMiner = RG.RAND.arrayGetRand(minerValues);
-    const opts = {
-        startX: firstMiner.x, startY: firstMiner.y,
-        dirWeights: firstMiner.dirWeights
-    };
-    const addMiners = [];
-    for (let i = 0; i < nMiners - 1; i++) {
-        const extraMiner = RG.RAND.arrayGetRand(minerValues);
-        addMiners.push(extraMiner);
-    }
-
-    opts.addMiners = addMiners;
-    return opts;
-}
-
-const Conf = {};
-
-function getMinersCorners() {
-    const minersCorners = {
-        cols: 100, rows: 100,
-        maxMinersCreated: 100,
-        dirWeights: miners.NW.dirWeights,
-        addMiners: [
-            miners.SW,
-            miners.NE,
-            miners.SE
-        ],
-        startX: miners.NW.x, startY: miners.NW.y
-    };
-    console.log(minersCorners);
-    return minersCorners;
-}
-Conf.getMinersCorners = getMinersCorners;
-
-function getMinersNSEW() {
-    const minersNSEW = {
-        cols: 100, rows: 100,
-        maxMinersCreated: 100,
-
-        startX: miners.N.x, startY: miners.N.y,
-        dirWeights: miners.N.dirWeights,
-
-        addMiners: [
-            miners.S,
-            miners.E,
-            miners.W
-        ]
-    };
-    return minersNSEW;
-}
-Conf.getMinersNSEW = getMinersNSEW;
