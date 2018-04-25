ec2c87ff4da140917fb1229d3732a0fb4737f51e
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-04-25 14:59:25 +0200

    Added createPlayer() function.

--- scripts/player-class-balancing.js
+++ scripts/player-class-balancing.js
@@ -6,22 +6,43 @@ const RG = require('../client/src/battles');
 const Actors = require('../client/data/actors.js');
 const ActorBattles = require('../tests/actor-battles');
 
-const className = 'Marksman';
-const playerLevel = 16;
-const actorClass = new RG.Component.ActorClass();
-actorClass.setClassName(className);
+const createPlayer = function() {
+    const fact = new RG.Factory.Game();
+    const race = 'goblin';
+    const className = 'Marksman';
+    const playerLevel = 32;
+    /* const actorClass = new RG.Component.ActorClass();
+    actorClass.setClassName(className);
 
-const playerActor = new RG.Actor.Rogue('hero');
-playerActor.add(actorClass);
-RG.levelUpActor(playerActor, playerLevel);
+    const playerActor = new RG.Actor.Rogue('hero');
+    */
+    const conf = {
+        playerName: 'hero',
+        playerClass: className,
+        playerRace: race,
+        playerLevel: 'Medium'
+    };
+    const playerActor = fact.createPlayerUnlessLoaded(conf);
+    playerActor.remove('Player');
+    // playerActor.add(actorClass);
+    RG.levelUpActor(playerActor, playerLevel);
+
+    const brain = new RG.Brain.GoalOriented(playerActor);
+    playerActor.setBrain(brain);
+    return playerActor;
+};
 
 const shells = Actors.filter(a => !((/spirit/i).test(a.name)));
 const matchLimit = 2000;
-const monitorActor = playerActor.getName();
+const monitorActor = 'hero';
 const ab = new ActorBattles({monitorActor, matchLimit, shells});
 
-ab.runWithActor(playerActor, 1);
+ab.runWithActor(createPlayer, 10);
 ab.printOutputs();
 
 const durationMs = ab.getDuration();
 console.log('Total duration ' + (durationMs / 1000) + ' s');
+
+console.log('Player stats:');
+const player = createPlayer();
+console.log(JSON.stringify(player, null, 1));
