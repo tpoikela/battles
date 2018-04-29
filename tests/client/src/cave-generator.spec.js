798222b5d353ab72c01193aa9e67d91edf2f47e2
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-04-29 22:13:44 +0300

    Added a check that enough floor cells are produced.

--- tests/client/src/cave-generator.spec.js
+++ tests/client/src/cave-generator.spec.js
@@ -1,13 +1,28 @@
 
 import { expect } from 'chai';
 
-const RG = require('../../../client/src/battles');
+//  const RG = require('../../../client/src/battles');
 const {CaveGenerator} = require('../../../client/src/cave-generator');
 
 describe('CaveGenerator', () => {
-    it('it can generate cave level', () => {
+    it('can generate simple cave levels', () => {
         const caveGen = new CaveGenerator();
-        const conf = {};
+        const conf = {dungeonType: 'Cave', isCollapsed: false};
+        const level = caveGen.create(100, 50, conf);
+        expect(level).to.exist;
+
+        const cells = level.getMap().getCells(c => (
+            (/floor/).test(c.getBaseElem().getType())
+        ));
+        expect(cells.length).to.be.at.least(50);
+    });
+
+
+    it('it can generate collapsed cave level', () => {
+        const caveGen = new CaveGenerator();
+        const conf = {
+            dungeonType: 'Lair', isCollapsed: true
+        };
         const level = caveGen.create(100, 50, conf);
         expect(level).to.exist;
         level.debugPrintInASCII();
