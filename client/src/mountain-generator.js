ea8fe0e092e8090edb237f3ff0fd94947b7cc544
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-06-08 06:11:17 +0300

    Adds a guardian into the crux region.

--- client/src/mountain-generator.js
+++ client/src/mountain-generator.js
@@ -9,7 +9,7 @@ const MapGenerator = require('./map.generator');
 const Level = require('./level');
 const Geometry = require('./geometry');
 const Path = require('./path');
-
+const DungeonPopulate = require('./dungeon-populate');
 
 const MountainGenerator = function() {
 
@@ -17,15 +17,15 @@ const MountainGenerator = function() {
 
 MountainGenerator.options = {};
 
-MountainGenerator.getFaceOptions = function() {
-    return MountainGenerator.options.face;
-};
 MountainGenerator.getSummitOptions = function() {
     return MountainGenerator.options.summit;
 };
 MountainGenerator.getFaceOptions = function() {
     const mapOpts = MapGenerator.getOptions('mountain');
     const opts = Object.assign({}, mapOpts, MountainGenerator.options.face);
+    // Usually overridden by top-level conf
+    opts.maxDanger = 5;
+    opts.maxValue = 100;
     return opts;
 };
 
@@ -36,7 +36,6 @@ MountainGenerator.options.face = {
 MountainGenerator.options.summit = {
     ratio: 0.3
 };
-
 
 MountainGenerator.prototype.createFace = function(cols, rows, conf) {
     const mapgen = new MapGenerator();
@@ -63,7 +62,7 @@ MountainGenerator.prototype.createSummit = function(cols, rows, conf) {
     return level;
 };
 
-MountainGenerator.prototype.createCrux = function(level) {
+MountainGenerator.prototype.createCrux = function(level, conf) {
     const map = level.getMap();
     const cols = map.cols;
     const wallRows = Math.round(map.rows / 6);
@@ -82,7 +81,7 @@ MountainGenerator.prototype.createCrux = function(level) {
     // Carve a path through the wall
     const xTop = RG.RAND.getUniformInt(0, cols - 1);
     const xBottom = RG.RAND.getUniformInt(0, cols - 1);
-    this.carvePath(wallMap, xTop, 0, xBottom, wallRows - 1);
+    const carvedXY = this.carvePath(wallMap, xTop, 0, xBottom, wallRows - 1);
 
     const wallStartY = Math.round(map.rows / 5);
     const mergeCb = (c1, c2) => {
@@ -101,20 +100,34 @@ MountainGenerator.prototype.createCrux = function(level) {
     };
     mapgen.createMountainPath(map, paths, pathConf);
 
+    // Add some guardians to the crux points, offset Y-coord first
+    carvedXY.forEach(item => {
+        item[1] += wallStartY;
+    });
+
+    const nGuardians = 3;
+    const dungPopul = new DungeonPopulate(conf);
+    for (let i = 0; i < nGuardians; i++) {
+        const guardPoint = RG.RAND.arrayGetRand(carvedXY);
+        dungPopul.addPointGuardian(level, guardPoint, conf.maxDanger);
+    }
 };
 
 /* Carves a path between x0,y0 and x1,y1 using a shortest distance. */
 MountainGenerator.prototype.carvePath = function(map, x0, y0, x1, y1) {
+    const result = [];
     const path = Path.getShortestPath(x0, y0, x1, y1);
     path.forEach(xy => {
         const brush = Geometry.getCrossAround(xy.x, xy.y, 2, true);
         brush.forEach(xy => {
             const [x, y] = xy;
             if (map.hasXY(x, y)) {
+                result.push(xy);
                 map.setBaseElemXY(x, y, RG.ELEM.STONE);
             }
         });
     });
+    return result;
 };
 
 module.exports = MountainGenerator;
