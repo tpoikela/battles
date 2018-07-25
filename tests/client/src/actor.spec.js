1ac2d9de3c49d85acd7b693d4732d4b8cfc67296
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-07-26 00:23:30 +0300

    Removed unit test for CombatMods.

--- tests/client/src/actor.spec.js
+++ tests/client/src/actor.spec.js
@@ -81,12 +81,6 @@ describe('Rogue.Actor', () => {
 
         const dmgFood = actor.getDamage();
         expect(dmgFood).to.be.at.least(1);
-
-        meat.add(new RG.Component.CombatMods());
-        meat.get('CombatMods').setAttack(1);
-
-        const att = actor.getEquipAttack();
-        expect(att).to.be.at.least(1);
     });
 
     it('can have CombatMods added', () => {
