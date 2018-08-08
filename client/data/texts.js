48c74c4b7007ea777248016b40397bfcccfae0e3
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-08-08 22:59:44 +0200

    Added a file for in-game texts.

---
+++ client/data/texts.js
@@ -0,0 +1,44 @@
+
+const Texts = {};
+
+Texts.intro = {};
+Texts.intro.chapter1 = `
+Welcome to the wintry realms!
+Winds are ever-blowing. Blowing off the
+glaciers.
+Are you ready to face the challenges of the
+icy north? Hunger, coldness, ravenous
+beasts, glacial chasms and forthcoming
+eternal winter are waiting for you in the
+darkness.
+`;
+
+Texts.intro.chapter2 = `
+You have come a long way from your homelands
+seeking
+the thrill of the adventure. Now, you must
+fight freezing battles in
+the north against hordes of winter demons
+and blizzard beasts. Will you bring back the
+peace
+to the grim and frostbitten kingdoms. Or
+will you
+bring the Winter of Ages upon its lands,
+reigning
+your kingdom cold for all eternity? Or will
+you
+perish
+nameless and forgotten on the icy wastes?
+`;
+
+
+Texts.battle = {};
+Texts.battle.eyeOfStorm =
+    'You see an eye of the storm approaching. Brace yourself now..';
+
+Texts.battle.beastsSlain =
+    'All beasts have been slain. The blizzard seems to calm down';
+
+Texts.battle.enemiesDead =
+    'All enemies are dead! You emerge victorious. Congratulations!';
+module.exports = Texts;
