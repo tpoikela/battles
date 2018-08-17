17fe364d81f281639bc7e481a6be92c1bc4ae2eb
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-08-17 18:55:53 +0300

    Removed .js extension from requires.

--- client/src/battles.js
+++ client/src/battles.js
@@ -1,64 +1,64 @@
 
 /* Contains all source code modules assigned to RG. */
 
-const RG = require('./rg.js');
+const RG = require('./rg');
 
 import ChunkManager from './chunk-manager';
 
-RG.Verify = require('./verify.js');
+RG.Verify = require('./verify');
 
 RG.EventPool = require('./eventpool');
 
 // No deps to RG.SubElems
-RG.Random = require('./random.js');
-RG.Mixin = require('./mixin.js');
-RG.Geometry = require('./geometry.js');
-RG.Time = require('./time.js');
-RG.Template = require('./template.js');
-RG.Animation = require('./animation.js');
+RG.Random = require('./random');
+RG.Mixin = require('./mixin');
+RG.Geometry = require('./geometry');
+RG.Time = require('./time');
+RG.Template = require('./template');
+RG.Animation = require('./animation');
 
 // Minor deps to other exports
-RG.Component = require('./component.js');
-RG.Item = require('./item.js');
-RG.Spell = require('./spell.js');
-RG.System = require('./system.js');
-RG.BTree = require('./aisequence.js');
+RG.Component = require('./component');
+RG.Item = require('./item');
+RG.Spell = require('./spell');
+RG.System = require('./system');
+RG.BTree = require('./aisequence');
 
 RG.Goals = require('./goals');
 RG.GoalsBattle = require('./goals-battle');
 RG.GoalsTop = require('./goals-top');
 
-RG.Brain = require('./brain.js');
-RG.Inv = require('./inv.js');
-RG.Actor = require('./actor.js');
-RG.Element = require('./element.js');
+RG.Brain = require('./brain');
+RG.Inv = require('./inv');
+RG.Actor = require('./actor');
+RG.Element = require('./element');
 
-RG.Cell = require('./map.cell.js');
-RG.Map = require('./map.js');
-RG.Map.Level = require('./level.js');
+RG.Cell = require('./map.cell');
+RG.Map = require('./map');
+RG.Map.Level = require('./level');
 RG.LevelGenerator = require('./level-generator');
 RG.MountainGenerator = require('./mountain-generator');
 RG.DungeonGenerator = require('./dungeon-generator');
 RG.CaveGenerator = require('./cave-generator');
 
-RG.World = require('./world.js');
+RG.World = require('./world');
 
-RG.Effects = require('../data/effects.js');
-RG.ObjectShell = require('./objectshellparser.js');
+RG.Effects = require('../data/effects');
+RG.ObjectShell = require('./objectshellparser');
 
-RG.Game = require('./game.js');
-RG.Factory = require('./factory.js');
-RG.Factory.World = require('./factory.world.js');
+RG.Game = require('./game');
+RG.Factory = require('./factory');
+RG.Factory.World = require('./factory.world');
 
-RG.Names = require('../data/name-gen.js');
-RG.LevelGen = require('../data/level-gen.js');
+RG.Names = require('../data/name-gen');
+RG.LevelGen = require('../data/level-gen');
 
 RG.OW = require('./overworld.map');
-RG.Overworld = require('./overworld.js');
+RG.Overworld = require('./overworld');
 
-RG.Game.FromJSON = require('./game.fromjson.js');
+RG.Game.FromJSON = require('./game.fromjson');
 
-RG.Factory.Game = require('./factory.game.js');
+RG.Factory.Game = require('./factory.game');
 
 RG.ChunkManager = ChunkManager;
 
