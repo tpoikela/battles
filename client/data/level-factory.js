47542830b1c5c4f04488a39db6d60cb0ae019656
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-08-22 12:58:13 +0200

    Added BlackTower.

--- client/data/level-factory.js
+++ client/data/level-factory.js
@@ -1,14 +1,21 @@
 
 import AbandonedFort from './abandoned-fort';
+import BlackTower from './black-tower';
 import Capital from './capital';
 import DwarvenCity from './dwarven-city';
 
-// const RG = require('../src/rg');
+const RG = require('../src/rg');
 
+/* Factory for creating levels by specifying their name. */
 export default class LevelFactory {
 
     constructor(fact) {
-      this.fact = fact;
+        this.fact = fact;
+        this.createFunc = {};
+    }
+
+    addFunction(name, createFunc) {
+        this.createFunc[name] = createFunc;
     }
 
     create(name, args) {
@@ -16,11 +23,21 @@ export default class LevelFactory {
             case 'Capital': return new Capital(...args).getLevel();
             case 'DwarvenCity': return new DwarvenCity(...args).getLevel();
             case 'AbandonedFort': return new AbandonedFort(...args).getLevel();
-            default: return this.fact.createLevel(name, ...args);
-            /* default: RG.err('LevelFactory', 'create',
-                `No constructor found for ${name}`);*/
+            case 'BlackTower': return new BlackTower(...args).getLevels();
+            default: {
+                if (this.createFunc.hasOwnProperty(name)) {
+                    return this.createFunc[name](...args);
+                }
+                if (this.fact) {
+                    return this.fact.createLevel(name, ...args);
+                }
+                else {
+                    RG.err('LevelFactory', 'create',
+                        'No factory/factory function given. Name: ' + name);
+                }
+                return null;
+            }
         }
-        // return null;
     }
 
 }
