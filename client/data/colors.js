e46edae34a1198daec790d58308d176ed04d7b9f
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-08-24 08:46:24 +0200

    Added colors.js.

---
+++ client/data/colors.js
@@ -0,0 +1,9 @@
+/* This file contains color variables which can be used in
+ * actors, elements and items.
+ */
+
+const Colors = {};
+
+Colors.dwarven = {fg: 'Red', bg: 'Black'};
+
+module.exports = Colors;
