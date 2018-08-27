2548e91ac2b984f5987399dead46af55d27ddc20
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-08-27 22:20:11 +0300

    Added actor-mods.js.

---
+++ client/data/actor-mods.js
@@ -0,0 +1,41 @@
+/* This file contains modificatios that are applied to specific actors. */
+
+const ActorMods = {};
+
+ActorMods.bearfolk = {
+
+};
+
+ActorMods.catfolk = {
+
+};
+
+ActorMods.dogfolk = {
+
+};
+
+ActorMods.dwarf = {
+
+};
+
+ActorMods.goblin = {
+
+};
+
+ActorMods.human = {
+
+};
+
+ActorMods.hyrkhian = {
+
+};
+
+ActorMods.wildling = {
+
+};
+
+ActorMods.wolfclan = {
+
+};
+
+module.exports = ActorMods;
