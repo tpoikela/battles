e58d5a798e0ae0f00deb12f9b9d2719e7759bff4
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-08-28 19:16:20 +0300

    Moved updateEntity() into prototype.

--- client/src/system/system.hunger.js
+++ client/src/system/system.hunger.js
@@ -7,24 +7,23 @@ System.Base = require('./system.base');
 /* Processes entities with hunger component.*/
 System.Hunger = function(compTypes) {
     System.Base.call(this, RG.SYS.HUNGER, compTypes);
-
-    this.updateEntity = ent => {
-        const hungerComp = ent.get('Hunger');
-        const actionComp = ent.get('Action');
-        hungerComp.decrEnergy(actionComp.getEnergy());
-        actionComp.resetEnergy();
-        if (hungerComp.isStarving()) {
-
-            if (ent.has('Health') && RG.isSuccess(RG.HUNGER_PROB)) {
-                const dmg = new RG.Component.Damage(RG.HUNGER_DMG,
-                    RG.DMG.HUNGER);
-                ent.add('Damage', dmg);
-                RG.gameWarn(ent.getName() + ' is starving!');
-            }
-        }
-    };
-
 };
 RG.extend2(System.Hunger, System.Base);
+
+System.Hunger.prototype.updateEntity = function(ent) {
+    const hungerComp = ent.get('Hunger');
+    const actionComp = ent.get('Action');
+    hungerComp.decrEnergy(actionComp.getEnergy());
+    actionComp.resetEnergy();
+    if (hungerComp.isStarving()) {
+
+        if (ent.has('Health') && RG.isSuccess(RG.HUNGER_PROB)) {
+            const dmg = new RG.Component.Damage(RG.HUNGER_DMG,
+                RG.DMG.HUNGER);
+            ent.add(dmg);
+            RG.gameWarn(ent.getName() + ' is starving!');
+        }
+    }
+};
 
 module.exports = System.Hunger;
