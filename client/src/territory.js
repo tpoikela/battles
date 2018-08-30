55c35d360cefe6f3b827445b205dd310e1adc677
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-08-30 20:30:00 +0300

    Added support for N different positions per contestant + fixed positions.

--- client/src/territory.js
+++ client/src/territory.js
@@ -19,6 +19,8 @@ const Territory = function(cols, rows) {
     this.data = {};
     this.numEmpty = 0;
 
+    this._rng = RNG;
+
     // By default, use only 4 directions
     this.dirs = RG.DIR_NSEW.concat(RG.DIR_DIAG);
 
@@ -28,6 +30,10 @@ const Territory = function(cols, rows) {
             this._markEmpty(i, j);
         }
     }
+};
+
+Territory.prototype.setRNG = function(rng) {
+    this._rng = rng;
 };
 
 Territory.prototype.getMap = function() {
@@ -54,7 +60,6 @@ Territory.prototype.setAsEmpty = function(map, cellInfo) {
     for (let x = 0; x < map.length; x++) {
         for (let y = 0; y < map[0].length; y++) {
             if (cellInfo[map[x][y]]) {
-                console.log('Marking empty', x, y, 'with', map[x][y]);
                 this._markEmpty(x, y);
             }
             else {
@@ -70,31 +75,22 @@ Territory.prototype.addContestant = function(data) {
 };
 
 Territory.prototype.generate = function() {
-    RNG.shuffle(this.contestants);
+    this._rng.shuffle(this.contestants);
     while (this.hasEmpty() && this.contestants.length > 0) {
         const next = this.contestants.shift();
         // TODO Check if there is weight on the size
 
         const {name} = next;
         const contData = this.data[name];
-        const {open, isFirst} = contData;
+        const {open, currPos, maxNumPos} = contData;
 
         // If no cells occupied, pick one randomly
-        if (isFirst) {
-            const xy = this.getEmptyXY();
-            if (contData.startX >= 0) {
-                xy[0] = contData.startX;
-            }
-            if (contData.startY >= 0) {
-                xy[1] = contData.startY;
-            }
+        if (currPos < maxNumPos) {
+            const xy = this.getStartPosition(name);
             this.addOccupied(name, xy);
-            contData.isFirst = false;
-            console.log(name, ' 1st cell is', xy);
             this.contestants.push(next);
         }
         else if (Object.keys(open).length > 0) {
-            console.log(name, 'looking for an open cell');
             const xy = this.getOpenXY(name);
             const emptyXY = this.getEmptyAdjacentXY(xy);
             if (emptyXY) {
@@ -111,6 +107,30 @@ Territory.prototype.generate = function() {
     }
 };
 
+/* Returns the starting position for given contestant name. */
+Territory.prototype.getStartPosition = function(name) {
+    const contData = this.data[name];
+    const {currPos} = contData;
+    const xy = this.getEmptyXY();
+    if (contData.startX.length > currPos) {
+        xy[0] = contData.startX[currPos];
+    }
+    else {
+        contData.startX.push(xy[0]);
+    }
+
+    if (contData.startY.length > currPos) {
+        xy[1] = contData.startY[currPos];
+    }
+    else {
+        contData.startY.push(xy[1]);
+    }
+
+    contData.currPos += 1;
+    return xy;
+};
+
+
 Territory.prototype.addOccupied = function(name, xy) {
     const key = xy[0] + ',' + xy[1];
     this.occupied[key] = xy;
@@ -119,7 +139,8 @@ Territory.prototype.addOccupied = function(name, xy) {
     this.data[name].occupied[key] = xy;
     this.map[xy[0]][xy[1]] = this.data[name].char;
 
-    // 1st cell can be forced to be non-empty
+    // 1st cell can be forced to be non-empty, but if not,
+    // then clear the empty cell
     if (this.empty[key]) {
         --this.numEmpty;
         delete this.empty[key];
@@ -127,18 +148,18 @@ Territory.prototype.addOccupied = function(name, xy) {
 };
 
 Territory.prototype.getEmptyXY = function() {
-    return RNG.arrayGetRand(Object.values(this.empty));
+    return this._rng.arrayGetRand(Object.values(this.empty));
 };
 
 Territory.prototype.getOpenXY = function(name) {
     const {open} = this.data[name];
-    return RNG.arrayGetRand(Object.values(open));
+    return this._rng.arrayGetRand(Object.values(open));
 };
 
 
 Territory.prototype.getEmptyAdjacentXY = function(xy) {
     const dirs = this.dirs.slice();
-    RNG.shuffle(dirs);
+    this._rng.shuffle(dirs);
     while (dirs.length > 0) {
         const nextDir = dirs.shift();
         const [nX, nY] = RG.newXYFromDir(nextDir, xy);
@@ -170,17 +191,32 @@ Territory.prototype._initContestant = function(data) {
     this.contestants.push(data);
     const {name, char} = data;
     this.data[name] = {
-        isFirst: true,
+        currPos: 0,
+        maxNumPos: 1,
         char,
         occupied: {},
         closed: {}, // Cannot try anymore
-        open: {} // Available for trying
+        open: {}, // Available for trying
+        startX: [],
+        startY: []
     };
+
     if (data.startX >= 0) {
+        this.data[name].startX = [data.startX];
+    }
+    else if (Array.isArray(data.startX)) {
         this.data[name].startX = data.startX;
     }
+
     if (data.startY >= 0) {
+        this.data[name].startY = [data.startY];
+    }
+    else if (Array.isArray(data.startY)) {
         this.data[name].startY = data.startY;
+    }
+
+    if (data.numPos) {
+        this.data[name].maxNumPos = data.numPos;
     }
     this.occupiedBy[name] = [];
 };
@@ -198,7 +234,30 @@ Territory.prototype.mapToString = function() {
         lines.push(line);
     }
     return lines.map(line => line.join(''));
+};
 
+/* Histogram of how many cells each contestant occupies. */
+Territory.prototype.getAreaProportions = function() {
+    const hist = {};
+    Object.keys(this.occupiedBy).forEach(name => {
+        hist[name] = this.occupiedBy[name].length;
+    });
+    return hist;
+};
+
+/* To serialize the territory. */
+Territory.prototype.toJSON = function() {
+    const json = {
+        data: this.data,
+        map: this.map,
+        contestants: this.contestants,
+        empty: this.empty,
+        occupied: this.occupied,
+        occupiedBy: this.occupiedBy,
+        numEmpty: this.numEmpty,
+        dirs: this.dirs
+    };
+    return json;
 };
 
 module.exports = Territory;
