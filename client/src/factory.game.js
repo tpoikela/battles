f6c97d6113966f6ce40be078f6a48afa0624286b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-08-31 10:58:45 +0300

    Added territory map generation.

--- client/src/factory.game.js
+++ client/src/factory.game.js
@@ -300,6 +300,8 @@ RG.Factory.Game = function() {
             nTilesY: yMult * mult * 4
         };
 
+        const startTime = new Date().getTime();
+
         this.progress('Creating Overworld Tile Map...');
         const overworld = OW.createOverWorld(owConf);
         this.progress('DONE');
@@ -355,6 +357,10 @@ RG.Factory.Game = function() {
         player.setFOVRange(RG.PLAYER_FOV_RANGE);
         game.addPlayer(player); // Player already placed to level
         this.progress('DONE');
+
+        const endTime = new Date().getTime();
+        const totalDur = endTime - startTime;
+        this.progress('World generation took ' + totalDur + ' ms.');
         // RG.Verify.verifyStairsConnections(game, 'Factory.Game');
         // this.progress('Stairs connections verified');
         return game;
@@ -416,18 +422,19 @@ RG.Factory.Game = function() {
         }
     };
 
-	this.addTerritories = function(ow, conf, playerX, playerY) {
+	this.addTerritories = function(ow, conf, owConf, playerX, playerY) {
         const {playerRace} = conf;
 		const capXY = ow.getFeaturesByType(OW.WCAPITAL)[0];
 		const dwarves = ow.getFeaturesByType(OW.WTOWER)[0];
 		const btower = ow.getFeaturesByType(OW.BTOWER)[0];
-		// const bcapital = ow.getFeaturesByType(OW.BCAPITAL)[0];
+		const bcapital = ow.getFeaturesByType(OW.BCAPITAL)[0];
 
 		const owMap = ow.getOWMap();
-		const terrMap = new Territory(ow.getSizeX(), ow.getSizeY());
+        const terrConf = {startSize: 2, maxNumPos: 2, maxFillRatio: 0.7};
+		const terrMap = new Territory(ow.getSizeX(), ow.getSizeY(), terrConf);
 
 		// console.log(ow.mapToString());
-		terrMap.setAsEmpty(owMap, {
+		terrMap.useMap(owMap, {
 			[OW.TERM]: true,
 			[OW.MOUNTAIN]: true,
 			[OW.BVILLAGE]: true,
@@ -438,29 +445,35 @@ RG.Factory.Game = function() {
 			[OW.BTOWER]: true
 		});
 
-		const bears = {name: 'bearfolk',
-			char: 'B', numPos: 2};
+		const bears = {name: 'bearfolk', char: 'B'};
 		const undeads = {name: 'undead', char: 'u', numPos: 3,
 			startX: [ow.getCenterX()], startY: [ow.getSizeY() - 5]};
 
-		terrMap.addContestant({name: 'avian', char: 'A'});
-		terrMap.addContestant(undeads);
-		terrMap.addContestant({name: 'wildling', char: 'I'});
-		terrMap.addContestant(bears);
-		terrMap.addContestant({name: 'wolfclan', char: 'w'});
-		terrMap.addContestant({name: 'catfolk', char: 'c'});
-		terrMap.addContestant({name: 'dogfolk', char: 'd'});
-		terrMap.addContestant({name: 'human', char: '@'});
-		terrMap.addContestant({name: 'goblin', char: 'g', numPos: 8});
-		terrMap.addContestant({name: 'dwarf', char: 'D',
+		terrMap.addRival({name: 'avian', char: 'A'});
+		terrMap.addRival(undeads);
+		terrMap.addRival({name: 'wildling', char: 'I'});
+		terrMap.addRival(bears);
+		terrMap.addRival({name: 'wolfclan', char: 'w'});
+		terrMap.addRival({name: 'catfolk', char: 'c'});
+		terrMap.addRival({name: 'dogfolk', char: 'd'});
+		terrMap.addRival({name: 'human', char: '@'});
+		terrMap.addRival({name: 'goblin', char: 'g', numPos: 8});
+		terrMap.addRival({name: 'dwarf', char: 'D',
 			startX: dwarves[0], startY: dwarves[1]});
-		terrMap.addContestant({name: 'hyrkhian', char: 'y',
+		terrMap.addRival({name: 'hyrkhian', char: 'y',
 			startX: capXY[0], startY: capXY[1]});
-		terrMap.addContestant({name: 'winterbeing', char: 'W',
-			startX: btower[0], startY: btower[1]});
 
-        const coordMap = ow.coorMap;
+        const winterConf = {name: 'winterbeing', char: 'W',
+            startX: [btower[0], bcapital[0]],
+            startY: [btower[1], bcapital[1]]
+        };
+		terrMap.addRival(winterConf);
+
+        const coordMap = new RG.OverWorld.CoordMap();
+        coordMap.xMap = 10;
+        coordMap.yMap = 10;
         const bbox = coordMap.getOWTileBboxFromAreaTileXY(playerX, playerY);
+        console.log('Player bbox will be', bbox);
 
         const pData = terrMap.getData(playerRace);
         pData.numPos += 1;
@@ -468,6 +481,7 @@ RG.Factory.Game = function() {
         pData.startY.push(RNG.getUniformInt(bbox.uly, bbox.lry));
 
 		terrMap.generate();
+        console.log(terrMap.mapToString());
         ow.terrMap = terrMap;
 	};
 
