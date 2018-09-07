6c99052d54b996b2e135b95c7f12d9514ac5c5d8
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-09-07 18:55:32 +0200

    Moved all functions in Inv.Inventory to prototype.

--- client/src/inv.js
+++ client/src/inv.js
@@ -362,183 +362,202 @@ RG.Inv.Inventory = function(actor) {
     this._actor = actor;
     this._inv = new RG.Item.Container(actor);
     this._eq = new RG.Inv.Equipment(actor);
+};
 
-    // Wrappers for container methods
-    this.addItem = item => {this._inv.addItem(item);};
-    this.hasItem = item => this._inv.hasItem(item);
-    this.removeItem = item => this._inv.removeItem(item);
+// Wrappers for container methods
+RG.Inv.Inventory.prototype.addItem = function(item) {
+    this._inv.addItem(item);
+};
+RG.Inv.Inventory.prototype.hasItem = function(item) {
+    return this._inv.hasItem(item);
+};
+RG.Inv.Inventory.prototype.removeItem = function(item) {
+    return this._inv.removeItem(item);
+};
 
-    this.removeNItems = (item, n) => this._inv.removeNItems(item, n);
+RG.Inv.Inventory.prototype.removeNItems = function(item, n) {
+    return this._inv.removeNItems(item, n);
+};
 
-    this.getRemovedItem = () => this._inv.getRemovedItem();
+RG.Inv.Inventory.prototype.getRemovedItem = function() {
+    return this._inv.getRemovedItem();
+};
 
-    this.useItem = (item, obj) => {
-        if (this._inv.hasItem(item)) {
-            if (item.useItem) {
-                item.useItem(obj);
-                return true;
-            }
+/* For using item inside the container. */
+RG.Inv.Inventory.prototype.useItem = function(item, obj) {
+    if (this._inv.hasItem(item)) {
+        if (item.useItem) {
+            item.useItem(obj);
+            return true;
         }
-        else {
-            RG.err('Inv.Inventory', 'useItem', 'Not in inventory, cannot use!');
-        }
-        return false;
-    };
+    }
+    else {
+        RG.err('Inv.Inventory', 'useItem', 'Not in inventory, cannot use!');
+    }
+    return false;
+};
 
     /* Returns true if given item can be carried.*/
-    this.canCarryItem = item => {
-        const eqWeight = this._eq.getWeight();
-        const invWeight = this._inv.getWeight();
-        const newWeight = eqWeight + invWeight + item.getWeight();
-        const maxWeight = this._actor.getMaxWeight();
-        if (newWeight > maxWeight) {return false;}
-        return true;
-    };
+RG.Inv.Inventory.prototype.canCarryItem = function(item) {
+    const eqWeight = this._eq.getWeight();
+    const invWeight = this._inv.getWeight();
+    const newWeight = eqWeight + invWeight + item.getWeight();
+    const maxWeight = this._actor.getMaxWeight();
+    if (newWeight > maxWeight) {return false;}
+    return true;
+};
 
-    /* Drops selected item to the actor's current location.*/
-    this.dropItem = item => {
-        if (this._inv.removeItem(item)) {
-            const level = this._actor.getLevel();
-            const droppedItem = this.getRemovedItem();
-            const [x, y] = this._actor.getXY();
-            if (level.addItem(droppedItem, x, y)) {
-                return true;
-            }
-            else {
-                this._inv.addItem(droppedItem);
-            }
-        }
-        return false;
-    };
-
-    this.dropNItems = (item, n) => {
-        if (this.removeNItems(item, n)) {
-            const level = this._actor.getLevel();
-            const droppedItem = this.getRemovedItem();
-            const [x, y] = this._actor.getXY();
-            if (level.addItem(droppedItem, x, y)) {
-                return true;
-            }
-            else {
-                this._inv.addItem(droppedItem);
-            }
-        }
-        return false;
-    };
-
-    /* Removes and item and returns it. */
-    this.removeAndGetItem = item => {
-        if (this._inv.removeItem(item)) {
-            return this.getRemovedItem();
-        }
-        return null;
-    };
-
-    this.getInventory = () => this._inv;
-    this.getEquipment = () => this._eq;
-
-    /* Removes item from inventory and equips it.*/
-    this.equipItem = item => {
-        if (this._inv.hasItem(item)) {
-            // If item has count > 2, can't use the same item ref
-            const eqItem = _getItemToEquip(item);
-            if (RG.isNullOrUndef[eqItem]) {
-                RG.err('Inv.Inventory', 'equipItem',
-                    'equippedItem is null. Should not happen');
-                return false; // For suppressed errors
-            }
-
-            if (this._eq.equipItem(eqItem)) {
-                return true;
-            }
-            else {
-                this._inv.addItem(eqItem); // Failed, add back to inv
-            }
+/* Drops selected item to the actor's current location.*/
+RG.Inv.Inventory.prototype.dropItem = function(item) {
+    if (this._inv.removeItem(item)) {
+        const level = this._actor.getLevel();
+        const droppedItem = this.getRemovedItem();
+        const [x, y] = this._actor.getXY();
+        if (level.addItem(droppedItem, x, y)) {
+            return true;
         }
         else {
+            this._inv.addItem(droppedItem);
+        }
+    }
+    return false;
+};
+
+RG.Inv.Inventory.prototype.dropNItems = function(item, n) {
+    if (this.removeNItems(item, n)) {
+        const level = this._actor.getLevel();
+        const droppedItem = this.getRemovedItem();
+        const [x, y] = this._actor.getXY();
+        if (level.addItem(droppedItem, x, y)) {
+            return true;
+        }
+        else {
+            this._inv.addItem(droppedItem);
+        }
+    }
+    return false;
+};
+
+/* Removes and item and returns it. */
+RG.Inv.Inventory.prototype.removeAndGetItem = function(item) {
+    if (this._inv.removeItem(item)) {
+        return this.getRemovedItem();
+    }
+    return null;
+};
+
+RG.Inv.Inventory.prototype.getInventory = function() {
+    return this._inv;
+};
+
+RG.Inv.Inventory.prototype.getEquipment = function() {
+    return this._eq;
+};
+
+/* Removes item from inventory and equips it.*/
+RG.Inv.Inventory.prototype.equipItem = function(item) {
+    if (this._inv.hasItem(item)) {
+        // If item has count > 2, can't use the same item ref
+        const eqItem = this._getItemToEquip(item);
+        if (RG.isNullOrUndef[eqItem]) {
             RG.err('Inv.Inventory', 'equipItem',
-                'Cannot equip. Not in inventory.');
+                'equippedItem is null. Should not happen');
+            return false; // For suppressed errors
         }
-        return false;
-    };
 
-    const _getItemToEquip = item => {
-        const res = this._inv.removeItem(item);
+        if (this._eq.equipItem(eqItem)) {
+            return true;
+        }
+        else {
+            this._inv.addItem(eqItem); // Failed, add back to inv
+        }
+    }
+    else {
+        RG.err('Inv.Inventory', 'equipItem',
+            'Cannot equip. Not in inventory.');
+    }
+    return false;
+};
+
+RG.Inv.Inventory.prototype._getItemToEquip = function(item) {
+    const res = this._inv.removeItem(item);
+    if (res) {
+        const rmvItem = this._inv.getRemovedItem();
+        // rmvItem.setID(item.getID());
+        return rmvItem;
+    }
+    return null;
+};
+
+/* Equips up to N items of given type. */
+RG.Inv.Inventory.prototype.equipNItems = function(item, n) {
+    if (this._inv.hasItem(item)) {
+        const res = this._inv.removeNItems(item, n);
         if (res) {
-            const rmvItem = this._inv.getRemovedItem();
-            return rmvItem;
-        }
-        return null;
-    };
-
-    /* Equips up to N items of given type. */
-    this.equipNItems = (item, n) => {
-        if (this._inv.hasItem(item)) {
-            const res = this._inv.removeNItems(item, n);
-            if (res) {
-                const removedItem = this._inv.getRemovedItem();
-                if (this._eq.equipItem(removedItem)) {
-                    return true;
-                }
-                else {
-                    this._inv.addItem(removedItem);
-                }
+            const removedItem = this._inv.getRemovedItem();
+            if (this._eq.equipItem(removedItem)) {
+                return true;
+            }
+            else {
+                this._inv.addItem(removedItem);
             }
         }
-        return false;
-    };
+    }
+    return false;
+};
 
-    /* Unequips item and puts it back to inventory.*/
-    this.unequipItem = function(slotType, n, slotNumber) {
-        if (RG.isNullOrUndef([slotType])) {
-            let msg = 'Some params null/undef: ';
-            msg += `type: |${slotType}| n: |${n}| number: |${slotNumber}|`;
-            RG.err('InvInventory', 'unequipItem', msg);
-        }
-        const eqItem = this._eq.getItem(slotType);
-        if (!RG.isNullOrUndef([eqItem])) {
-            if (this._eq.unequipItem(slotType, n, slotNumber)) {
-                const rmvItems = this._eq.getUnequipped(slotType, slotNumber);
-                if (rmvItems !== null) {
-                    this.addItem(rmvItems);
-                    return true;
-                }
+/* Unequips item and puts it back to inventory.*/
+RG.Inv.Inventory.prototype.unequipItem = function(slotType, n, slotNumber) {
+    if (RG.isNullOrUndef([slotType])) {
+        let msg = 'Some params null/undef: ';
+        msg += `type: |${slotType}| n: |${n}| number: |${slotNumber}|`;
+        RG.err('InvInventory', 'unequipItem', msg);
+    }
+    const eqItem = this._eq.getItem(slotType);
+    if (!RG.isNullOrUndef([eqItem])) {
+        if (this._eq.unequipItem(slotType, n, slotNumber)) {
+            const rmvItems = this._eq.getUnequipped(slotType, slotNumber);
+            if (rmvItems !== null) {
+                this.addItem(rmvItems);
+                return true;
             }
         }
-        return false;
-    };
+    }
+    return false;
+};
 
     /* Unequips and returns N items. Doesn't add to inv.*/
-    this.unequipAndGetItem = (slotType, n, slotNumber) => {
-        const eqItem = this._eq.getItem(slotType);
-        if (!RG.isNullOrUndef([eqItem])) {
-            if (this._eq.unequipItem(slotType, n)) {
-                return this._eq.getUnequipped(slotType, slotNumber);
-            }
+RG.Inv.Inventory.prototype.unequipAndGetItem = function(
+    slotType, n, slotNumber
+) {
+    const eqItem = this._eq.getItem(slotType);
+    if (!RG.isNullOrUndef([eqItem])) {
+        if (this._eq.unequipItem(slotType, n)) {
+            return this._eq.getUnequipped(slotType, slotNumber);
         }
-        return null;
-    };
+    }
+    return null;
+};
 
-    this.getWeapon = () => {
-        const item = this._eq.getItem('hand');
-        if (!RG.isNullOrUndef([item])) {return item;}
-        return null;
-    };
+RG.Inv.Inventory.prototype.getWeapon = function() {
+    const item = this._eq.getItem('hand');
+    if (!RG.isNullOrUndef([item])) {return item;}
+    return null;
+};
 
-    this.getMissileWeapon = () => {
-        const item = this._eq.getItem('missileweapon');
-        if (!RG.isNullOrUndef([item])) {return item;}
-        return null;
-    };
+RG.Inv.Inventory.prototype.getMissileWeapon = function() {
+    const item = this._eq.getItem('missileweapon');
+    if (!RG.isNullOrUndef([item])) {return item;}
+    return null;
+};
 
-    this.getMissile = () => {
-        const item = this._eq.getItem('missile');
-        return item;
-    };
+RG.Inv.Inventory.prototype.getMissile = function() {
+    const item = this._eq.getItem('missile');
+    return item;
+};
 
-    this.getEquipped = slotType => this._eq.getItem(slotType);
-
-
+RG.Inv.Inventory.prototype.getEquipped = function(slotType) {
+    return this._eq.getItem(slotType);
 };
 
 module.exports = RG.Inv;
