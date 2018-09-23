2f0cc694dc6169c7e73090e3273bf52e987a071b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-09-23 21:47:08 +0300

    Added Quest and Task objects. Moved most of functions into QuestGen.

--- client/data/quest-gen.js
+++ client/data/quest-gen.js
@@ -2,72 +2,115 @@
 
 const prettybnf = require('prettybnf');
 const debug = require('debug')('bitn:quest-gen');
-const fs = require('fs');
+// const fs = require('fs');
 const RG = require('../src/rg');
 RG.Random = require('../src/random');
+
+const questGrammar = require('./quest-grammar');
 
 const RNG = RG.Random.getRNG();
 
 RNG.setSeed(Date.now());
+
+/* A task represents a part of a quest. */
+const Task = function(name) {
+    this.type = 'Task';
+    this.name = name;
+    this.isQuest = () => false;
+};
+
+/* A quest object which can be used to model quests. */
+const Quest = function() {
+    this.steps = []; // Atomics/sub-quests
+    this.type = 'Quest';
+
+    this.isQuest = () => true;
+
+    this.add = function(step) {
+        if (Array.isArray(step)) {
+            this.steps = this.steps.concat(step);
+        }
+        else {
+            this.steps.push(step);
+        }
+    };
+
+    this.numQuests = function() {
+        let sum = 1;
+        this.steps.forEach(step => {
+            if (step.isQuest && step.isQuest()) {
+                sum += 1;
+            }
+        });
+        return sum;
+    };
+
+    /* Returns the number of immediate tasks. */
+    this.numTasks = function() {
+        const numSubquests = this.numQuests() - 1;
+        return this.steps.length - numSubquests;
+    };
+};
+
 /* Code adapted from erratic by Daniel Connelly. */
 function extract(prop, o) {
     return o[prop];
 }
 
-function parse(grammar) {
-    var ast = prettybnf.parse(grammar);
-    var rules = {};
-    ast.productions.forEach(function(prod) {
-        rules[prod.lhs.text] = prod.rhs.map(extract.bind(null, 'terms'));
-    });
-    return rules;
-}
-
-function chooseRandomRule(things) {
-    if (Array.isArray(things)) {
-        // const result = things[Math.floor(Math.random() * things.length)];
-        const result = RNG.arrayGetRand(things);
+/* Chooses a random rule from arrOfRules. */
+function chooseRandomRule(arrOfRules) {
+    if (Array.isArray(arrOfRules)) {
+        const result = RNG.arrayGetRand(arrOfRules);
         debug('choose RANDOM', result);
         return result;
     }
     else {
-        debug('choose() not an ARRAY:', things);
+        debug(`chooseRandomRule() not an ARRAY: |${arrOfRules}`);
     }
     return null;
 }
 
-function generateTerm(rules, term) {
-    if (!term.text) {
-        const json = JSON.stringify(rules);
-        throw new Error('Null/undef term.text with rules|', json, '|');
-    }
-    if (term.type === 'terminal') {return term.text;}
-    debug('calling generate() with term.text', term.text);
-    return generateQuest(rules, term.text);
-}
 
-function generateQuest(rules, rule) {
-    const randRule = chooseRandomRule(rules[rule]);
-    if (Array.isArray(randRule)) {
-        return randRule.map(generateTerm.bind(null, rules)).join('|');
-    }
-    return randRule;
-}
+/* QuestGen object. */
+const QuestGen = function() {
+    this._init();
+};
 
-const g = fs.readFileSync('client/data/quest-grammar.g', 'utf8');
-const rules = parse(g);
+/* Can be used for creating quest grammar/rules from a string in BNF format. */
+QuestGen.parse = function(grammar) {
+    const ast = prettybnf.parse(grammar);
+    const rules = {};
+    ast.productions.forEach(prod => {
+        rules[prod.lhs.text] = prod.rhs.map(extract.bind(null, 'terms'));
+    });
+    return rules;
+};
 
-function genQuestWithConf(conf = {}) {
+QuestGen.prototype._init = function() {
+    this.stack = []; // Stack for quests
+    this.currQuest = null;
+    this.ruleHist = {};
+    this.startRule = 'QUEST';
+};
+
+QuestGen.rules = QuestGen.parse(questGrammar);
+
+QuestGen.prototype.genQuestWithConf = function(conf = {}) {
+    if (conf.debug) {debug.enabled = true;}
+    const questRules = conf.rules || QuestGen.rules;
+    const startRule = conf.startRule || 'QUEST';
+    this.startRule = startRule;
     let ok = false;
     let watchdog = 20;
     let quest = [];
-    const minLength = conf.minLength || 1;
     while (!ok) {
-        quest = generateQuest(rules, 'QUEST').split('|');
-        if (quest.length <= conf.maxLength) {
-            if (quest.length >= minLength) {
-                ok = true;
-            }
+        this._init();
+
+        debug('=== Generating new quest now ===');
+        quest = this.generateQuest(questRules, startRule).split('|');
+        ok = this._questMeetsReqs(quest, conf);
+        if (!ok) {
+            debug('QUEST DISCARDED');
         }
 
         if (--watchdog === 0) {
@@ -76,10 +119,87 @@ function genQuestWithConf(conf = {}) {
         }
     }
 
-    return quest;
+    // return quest;
+    return this.currQuest;
+};
+
+QuestGen.prototype._questMeetsReqs = function(quest, conf) {
+    let ok = true;
+
+    // Check if quest min/max length is met
+    const minLength = conf.minLength || 1;
+    const maxLength = conf.maxLength || -1;
+    if (quest.length <= maxLength || maxLength === -1) {
+        if (quest.length >= minLength) {
+            ok = true;
+            debug('MATCHING QUEST FOUND');
+        }
+        else {ok = false;}
+    }
+    else {ok = false;}
+
+    if (conf.minQuests) {
+        ok = ok && this.currQuest.numQuests() >= conf.minQuests;
+        console.log('AFTER MIN QUEST CHECK ok:', ok);
+    }
+    return ok;
+};
+
+QuestGen.prototype.generateQuest = function(rules, rule) {
+    if (!this.ruleHist[rule]) {this.ruleHist[rule] = 1;}
+    else {this.ruleHist[rule] += 1;}
+
+    if (rule === this.startRule) {
+        debug('New (sub)-quest will be generated');
+        this.currQuest = new Quest();
+        this.stack.push(this.currQuest);
+    }
+    const randRule = chooseRandomRule(rules[rule]);
+    if (Array.isArray(randRule)) {
+        const steps = randRule.map(this.generateTerm.bind(this, rules));
+        const tasks = steps.map(step => new Task(step));
+        this.currQuest.add(tasks);
+        this._checkIfQuestOver(rule);
+        return steps.join('|');
+    }
+    debug(`generateQuest end reached, return |${randRule}|`);
+    this._checkIfQuestOver(rule);
+    return randRule;
+};
+
+QuestGen.prototype.generateTerm = function(rules, term) {
+    if (!term.text) {
+        const json = JSON.stringify(rules);
+        throw new Error('Null/undef term.text with rules|', json, '|');
+    }
+    if (term.type === 'terminal') {return term.text;}
+    debug(`calling generate() with term.text |${term.text}|`);
+    return this.generateQuest(rules, term.text);
+};
+
+QuestGen.prototype._checkIfQuestOver = function(rule) {
+    if (rule === this.startRule) {
+        debug('Finishing current quest');
+        const qLen = this.stack.length;
+        if (qLen > 1) {
+            const subQuest = this.stack.pop();
+            this.currQuest = this.stack[this.stack.length - 1];
+            this.currQuest.add(subQuest);
+        }
+    }
+};
+
+const runningAsScript = !module.parent;
+if (runningAsScript) {
+    // The grammar is stored in the string g
+    // console.log(JSON.stringify(rules));
+    // console.log(generateQuest(rules, 'QUEST'));
+    const questGen = new QuestGen();
+    console.log(questGen.genQuestWithConf({maxLength: 10, minLength: 3}));
+    console.log(JSON.stringify(questGen.stack));
+    console.log(JSON.stringify(questGen.ruleHist));
 }
 
-// The grammar is stored in the string g
-// console.log(JSON.stringify(rules));
-// console.log(generateQuest(rules, 'QUEST'));
-console.log(genQuestWithConf({maxLength: 10, minLength: 3}));
+module.exports = {
+    Quest, Task, QuestGen
+};
