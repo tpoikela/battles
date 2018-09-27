372da83d9d8fdfc46d7771573a3f9e1ef89debb1
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-09-27 15:34:39 +0300

    Added unit test for QuestPopulate.

--- tests/client/src/quest-gen.spec.js
+++ tests/client/src/quest-gen.spec.js
@@ -1,8 +1,10 @@
 
 import { expect } from 'chai';
 
-const {Quest, QuestGen}
-    = require('../../../client/data/quest-gen');
+const {Quest, QuestGen, QuestPopulate}
+    = require('../../../client/src/quest-gen');
+
+const FactoryWorld = require('../../../client/src/factory.world');
 
 const questGram1 = '<QUEST> ::= "goto" "kill";';
 
@@ -49,5 +51,38 @@ describe('QuestGen', () => {
         const quest = questGen.genQuestWithConf(conf);
         console.log(JSON.stringify(quest));
         expect(quest.numTasks()).to.equal(6);
+    });
+});
+
+
+describe('QuestPopulate', () => {
+
+    it('is used to map quests to resources', () => {
+        const questPopul = new QuestPopulate();
+        const factWorld = new FactoryWorld();
+        const city = factWorld.createCity({
+            name: 'QuestCity', nQuarters: 1,
+            quarter: [
+                {name: 'QuestQuarter', nLevels: 1}
+            ]
+        });
+        const taskList = ['<goto>already_there', '<kill>kill'];
+        const quest = new Quest('Kill an actor', taskList);
+        questPopul.mapQuestToResources(quest, city, null);
+        questPopul.addQuestComponents(city);
+
+        const level = city.getLevels()[0]; // Only one
+        const actors = level.getActors();
+
+        let hasGiver = false;
+        let hasTarget = false;
+        actors.forEach(actor => {
+            console.log('Checking actor', actor.getName());
+            if (actor.has('QuestTarget')) {hasTarget = true;}
+            if (actor.has('QuestGiver')) {hasGiver = true;}
+        });
+
+        expect(hasGiver).to.equal(true);
+        expect(hasTarget).to.equal(true);
     });
 });
