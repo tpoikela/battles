c78a8597ca4a9c8dc65511cc139210e0ea0182a9
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-10-01 23:18:21 +0300

    Added checkQuestEmitMsg() and processComplComp().

--- client/src/system/system.quest.js
+++ client/src/system/system.quest.js
@@ -6,9 +6,9 @@ System.Base = require('./system.base');
 
 System.Quest = function(compTypes) {
     System.Base.call(this, RG.SYS.QUEST, compTypes);
+    this.compTypesAny = true; // Triggered on at least one component
 };
 RG.extend2(System.Quest, System.Base);
-
 
 System.Quest.prototype.updateEntity = function(ent) {
     if (ent.has('GiveQuest')) {
@@ -18,17 +18,22 @@ System.Quest.prototype.updateEntity = function(ent) {
     }
     if (ent.has('QuestCompleted')) {
         const complComp = ent.get('QuestCompleted');
-        this.processCompleteComp(ent, complComp);
+        this.processComplComp(ent, complComp);
         ent.remove(complComp);
     }
 };
 
+/* When a quest is given to an actor, this function processes it. */
 System.Quest.prototype.processGiveComp = function(ent, comp) {
     const giver = comp.getGiver();
     const giverComp = giver.get('QuestGiver');
+    if (giverComp.getHasGivenQuest()) {
+        return;
+    }
     const questData = giverComp.getQuestData();
     const questKeys = questData.keys();
     const questComp = new RG.Component.Quest();
+    giverComp.setQuestID(questComp.getID());
 
     if (questKeys.length === 0) {
         RG.err('System.Quest', 'processGiveComp',
@@ -46,16 +51,52 @@ System.Quest.prototype.processGiveComp = function(ent, comp) {
             questTarget = questData.next(key);
         }
     });
-    // questComp.setQuestData(questData);
+
+    giverComp.giveQuest(ent);
     questComp.setGiver(giver);
     ent.add(questComp);
+    this.checkQuestMsgEmits(ent, questComp);
     console.log('SystemQuest added Quest to', ent.getName());
 };
 
-/*
-System.Quest.prototype.processComplComp = function(ent, comp) {
-
+/* Checks if any messages should be shown to player after a quest is
+ * accepted. */
+System.Quest.prototype.checkQuestMsgEmits = function(ent, questComp) {
+    const entLevel = ent.getLevel();
+    const firstLevel = questComp.first('location');
+    const giverName = questComp.getGiver().getName();
+    let msg = `You accept the quest from ${giverName}.`;
+    if (firstLevel) {
+        if (entLevel.getID() === firstLevel.getID()) {
+            msg += ' You are already in the first quest location';
+        }
+        else {
+            msg += ' You should try to get to the first quest location';
+        }
+    }
+    RG.gameMsg({cell: ent.getCell(), msg});
 };
-*/
+
+System.Quest.prototype.processComplComp = function(ent, comp) {
+    const giver = comp.getGiver();
+    const questID = giver.get('QuestGiver').getQuestID();
+    const quests = ent.getList('Quest');
+    // Need to find matching quest first
+    const givenQuest = quests.find(quest => quest.getID() === questID);
+
+    if (givenQuest.isCompleted()) {
+        const giverComp = giver.get('QuestGiver');
+        const questData = giverComp.getQuestData();
+        // Give exp reward
+        const expPoints = giverComp.getDanger() * questData.numSteps();
+        const expComp = new RG.Component.ExpPoints(expPoints);
+        ent.add(expComp);
+
+        // Give reward, items + any other info
+    }
+    else {
+        RG.gameDanger({cell: ent.getCell(), msg: 'Quest is not completed!'});
+    }
+};
 
 module.exports = System.Quest;
