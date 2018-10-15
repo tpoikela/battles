2f9057672a51ce4b53d2782fd439940e6aeffd90
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-10-15 23:21:36 +0300

    Added onReportEvent(), onListenEvent() and onReadEvent().

--- client/src/system/system.quest.js
+++ client/src/system/system.quest.js
@@ -12,7 +12,10 @@ System.Quest = function(compTypes) {
     this.compTypesAny = true; // Triggered on at least one component
 
     this._eventTable = {
-        kill: this.onKillEvent = this.onKillEvent.bind(this)
+        kill: this.onKillEvent = this.onKillEvent.bind(this),
+        listen: this.onListenEvent = this.onListenEvent.bind(this),
+        read: this.onReadEvent = this.onReadEvent.bind(this),
+        report: this.onReportEvent = this.onReportEvent.bind(this)
     };
 };
 RG.extend2(System.Quest, System.Base);
@@ -20,7 +23,7 @@ RG.extend2(System.Quest, System.Base);
 System.Quest.prototype.updateEntity = function(ent) {
     if (ent.has('GiveQuest')) {
         const giveComp = ent.get('GiveQuest');
-        this.processGiveComp(ent, giveComp);
+        this.processGiveQuestComp(ent, giveComp);
         ent.remove(giveComp);
     }
     if (ent.has('QuestCompleted')) {
@@ -36,7 +39,7 @@ System.Quest.prototype.updateEntity = function(ent) {
 };
 
 /* When a quest is given to an actor, this function processes it. */
-System.Quest.prototype.processGiveComp = function(ent, comp) {
+System.Quest.prototype.processGiveQuestComp = function(ent, comp) {
     const giver = comp.getGiver();
     const giverComp = giver.get('QuestGiver');
     if (giverComp.getHasGivenQuest()) {
@@ -47,7 +50,7 @@ System.Quest.prototype.processGiveComp = function(ent, comp) {
     questComp.setQuestID(giverComp.getQuestID());
 
     if (questTargets.length === 0) {
-        RG.err('System.Quest', 'processGiveComp',
+        RG.err('System.Quest', 'processGiveQuestComp',
             `No keys in questData, giver: ${giver.getName}`);
     }
 
@@ -127,7 +130,7 @@ System.Quest.prototype.giveQuestReward = function(ent, comp) {
 };
 
 System.Quest.prototype.processQuestEvent = function(ent, qEvent) {
-    const targetType = qEvent.getTarget().getTargetType();
+    const targetType = qEvent.getEventType();
     if (typeof this._eventTable[targetType] === 'function') {
         this._eventTable[targetType](ent, qEvent);
     }
@@ -142,30 +145,103 @@ System.Quest.prototype.processQuestEvent = function(ent, qEvent) {
 System.Quest.prototype.onKillEvent = function(ent, qEvent) {
     const args = qEvent.getArgs();
     if (args && args.corpse) {
-        const actor = qEvent.getTarget();
+        const targetComp = qEvent.getTargetComp();
+        const actor = targetComp.getTarget();
         this.moveQuestTargetComp(actor, args.corpse);
 
-        const questComp = ent.get('Quest');
-        const targetObj = questComp.find(obj => obj.id === actor.getID());
-        this.setTargetCompleted(targetObj, questComp);
+        if (ent.has('Quest')) {
+            const questComp = ent.get('Quest');
+            const questTargets = questComp.getQuestTargets();
+            const targetObj = questTargets.find(obj => (
+                obj.id === actor.getID()));
+            this.setTargetCompleted(targetObj, questComp);
 
-        let msg = `${ent.getName()} has reached quest target of killing`;
-        msg += ` ${actor.getName()}.`;
-		RG.gameMsg({cell: ent.getCell(), msg});
+            let msg = `${ent.getName()} has reached quest target of killing`;
+            msg += ` ${actor.getName()}.`;
+            RG.gameMsg({cell: ent.getCell(), msg});
+        }
     }
     else {
         RG.err('System.Quest', 'onKillEvent',
-            `Args must contain corpse. Got: ${JSON.stringify(args)}`);
+            `Args must contain |corpse|. Got: ${JSON.stringify(args)}`);
+    }
+};
 
+System.Quest.prototype.onListenEvent = function(ent, qEvent) {
+    const args = qEvent.getArgs();
+    if (args && args.info) {
+        const info = args.info.getInfo();
+        const actor = args.src;
+        const msg = `${actor.getName()} tells about ${info}`;
+        RG.gameMsg({msg, cell: ent.getCell()});
+        if (ent.has('Quest')) {
+            ent.add(args.info.clone());
+            const questComp = ent.get('Quest');
+            const targetComp = qEvent.getTargetComp();
+            const actor = targetComp.getTarget();
+            const listenID = actor.getID();
+
+            const questTargets = questComp.getQuestTargets();
+            const targetObj = questTargets.find(obj => obj.id === listenID);
+            this.setTargetCompleted(targetObj, questComp);
+        }
+    }
+    else {
+        RG.err('System.Quest', 'onListenEvent',
+            `Args must contain |info|. Got: ${JSON.stringify(args)}`);
+    }
+};
+
+System.Quest.prototype.onReadEvent = function(ent, qEvent) {
+    const targetComp = qEvent.getTargetComp();
+    const questComp = ent.get('Quest');
+    if (questComp) {
+        const readEntity = targetComp.getTarget();
+        const questTargets = questComp.getQuestTargets();
+        const targetObj = questTargets.find(obj => (
+            obj.id === readEntity.getID()));
+        this.setTargetCompleted(targetObj, questComp);
+    }
+};
+
+System.Quest.prototype.onReportEvent = function(ent, qEvent) {
+    const targetComp = qEvent.getTargetComp();
+    const reportTarget = targetComp.getTarget();
+    if (reportTarget.has('QuestReport')) {
+        const reportComp = reportTarget.get('QuestReport');
+        const questInfo = qEvent.getArgs().info;
+        const tName = reportTarget.getName();
+
+        if (reportComp.getExpectInfoFrom() === questInfo.getGivenBy()) {
+            const questComp = ent.get('Quest');
+            const questTargets = questComp.getQuestTargets();
+            const targetReportObj = questTargets.find(obj => (
+                obj.id === reportTarget.getID()
+            ));
+            const msg = `${ent.getName()} reports info to ${tName}`;
+            RG.gameMsg({cell: ent.getCell(), msg});
+
+            this.setTargetCompleted(targetReportObj, questComp);
+        }
+        else {
+            const msg = `${tName} is not interested in this info`;
+            RG.gameMsg({cell: ent.getCell(), msg});
+        }
     }
 };
 
 /* Moves QuestTarget from one entity to another. */
 System.Quest.prototype.moveQuestTargetComp = function(srcEnt, destEnt) {
-    const qTarget = srcEnt.get('QuestTarget');
-    qTarget.changeEntity(destEnt);
-    qTarget.setIsCompleted(true);
-    qTarget.setTarget(destEnt);
+    try {
+        const qTarget = srcEnt.get('QuestTarget');
+        qTarget.changeEntity(destEnt);
+        qTarget.setIsCompleted(true);
+        qTarget.setTarget(destEnt);
+    }
+    catch (e) {
+        console.log('srcEnt is', srcEnt);
+        console.log(e.message);
+    }
 };
 
 
