5f25fdf4353998207916b18a89e13a5c281fb265
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-10-16 23:48:27 +0300

    Added more book/city names.

--- client/data/name-gen.js
+++ client/data/name-gen.js
@@ -80,15 +80,17 @@ Names.place.unique = {
     city: {
         first: [
             'Stag', 'Small', 'Mud', 'Ebon', 'Silk', 'Spirit', 'Basin',
-            'Shadow', 'Gold', 'Snow', 'Frost', 'Ice', 'Hound', 'Moon',
-            'Dire', 'Ever', 'Iron', 'Ruby', 'Star', 'Crystal', 'Glimmer',
-            'Winters', 'Raven', 'Pine', 'Ever', 'Never', 'Rune',
-            'Glace', 'Lumen', 'Confer'
+            'Shadow', 'Gold', 'Silver', 'Snow', 'Frost', 'Ice', 'Hound',
+            'Moon', 'Dire', 'Ever', 'Iron', 'Ruby', 'Star', 'Crystal',
+            'Glimmer', 'Winters', 'Raven', 'Pine', 'Ever', 'Never', 'Rune',
+            'Glace', 'Lumen', 'Confer', 'Flamen', 'Jotun', 'Troll', 'Winds',
+            'Oaken', 'Willow', 'Anvil', 'Ashen', 'Kosken', 'Storm'
         ],
         second: [
             'guard', 'point', 'fell', 'mire', 'shield', 'crest', 'yard',
             'minster', 'swallow', 'grasp', 'cliff', 'cross', 'host', 'barrow',
-            'vein', 'view', 'home', 'gard', 'wall'
+            'vein', 'view', 'home', 'gard', 'wall', 'heim', 'creek', 'gasp',
+            'mane', 'thorne', 'keep'
         ]
     }
 };
@@ -114,12 +116,18 @@ Names.getVillageType = () => {
     return RNG.arrayGetRand(['Village', 'Hamlet', 'Town', 'Township']);
 };
 
+Names.alreadyUsed = new Set(['']);
+
 Names.getUniqueName = type => {
     const names = Names.place.unique[type];
     if (names) {
-        const first = RNG.arrayGetRand(names.first);
-        const second = RNG.arrayGetRand(names.second);
-        return first + second;
+        let fullName = '';
+        while (Names.alreadyUsed.has(fullName)) {
+            const first = RNG.arrayGetRand(names.first);
+            const second = RNG.arrayGetRand(names.second);
+            fullName = first + second;
+        }
+        return fullName;
     }
     else {
         RG.err('name-gen.js', 'Names.getUniqueName',
@@ -135,26 +143,40 @@ Names.getGenericPlaceName = (type) => {
 
 Names.actorCount = 0;
 Names.getActorName = () => {
-    return 'RandActor' + Names.actorCount++;
+    let fullName = 'RandActor' + Names.actorCount++;
+    while (Names.alreadyUsed.has(fullName)) {
+        fullName = 'RandActor' + Names.actorCount++;
+    }
+    return fullName;
 };
 
 const bookNameGen = {
-    prefix: ['old', 'ancient', 'dusty', 'worn', 'well-preserved', 'heavy',
-        'light'],
-    prefix2: ['rune-covered', 'worm-ridden', 'leather', 'hard-cover',
-        'metal-plated', 'decorated', 'exquisite'],
-    substantive: ['tome', 'book', 'tract', 'codex', 'opus', 'handbook',
-        'volume', 'treatise']
+    prefix: [
+        'old', 'ancient', 'dusty', 'worn', 'well-preserved', 'heavy',
+        'light', 'thick', 'thin'
+    ],
+    prefix2: [
+        'rune-covered', 'worm-ridden', 'leather', 'hard-cover',
+        'metal-plated', 'decorated', 'exquisite', 'engraved', 'plain',
+        'ordinary', 'aesthetic', 'ornamental', 'skeletal'
+    ],
+    substantive: [
+        'tome', 'book', 'tract', 'codex', 'opus', 'handbook',
+        'volume', 'treatise', 'grimoire', 'rariora', 'compendium',
+        'epitome', 'manuscript'
+    ]
 };
 
-Names.bookCount = 0;
 Names.getBookName = () => {
-    let prefix = RNG.arrayGetRand(bookNameGen.prefix);
-    prefix = prefix.capitalize();
-    const prefix2 = RNG.arrayGetRand(bookNameGen.prefix2);
-    const subst = RNG.arrayGetRand(bookNameGen.substantive);
-    return `${prefix} ${prefix2} ${subst}`;
-    // return 'Book ' + Names.bookCount++;
+    let fullName = '';
+    while (Names.alreadyUsed.has(fullName)) {
+        let prefix = RNG.arrayGetRand(bookNameGen.prefix);
+        prefix = prefix.capitalize();
+        const prefix2 = RNG.arrayGetRand(bookNameGen.prefix2);
+        const subst = RNG.arrayGetRand(bookNameGen.substantive);
+        fullName = `${prefix} ${prefix2} ${subst}`;
+    }
+    return fullName;
 };
 
 module.exports = Names;
