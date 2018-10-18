c3a9b1a454f49a6cbe2ad8d8f95b0d632a6f0af6
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-10-18 18:41:43 +0300

    Added Elements + cleaned up unneeded lines.

--- client/data/battles_objects.js
+++ client/data/battles_objects.js
@@ -1,47 +1,13 @@
-
-/* eslint max-len: 100 */
-
-/**
- * THis file contains definitions for in-game objects, monsters and levels. It's
- * rather human-readable so it should be easy to add new stuff in. All contents
- * are used for procedural generation.
- */
-
-// Some info on attributes:
-//      dontCreate: true - Use with base classes, prevents object creation
-//      base: xxx        - Use xxx as a base for the object
-//      danger - For rand generation for actors, higher values means less often
-//      value  - For rand gen (+buy/sell) for items, higher means less often
-//      cssClass         - Used for rendering purposes.
+/* Include file for game objects like actors, items and elements. */
 
 const Items = require('./items');
 const Actors = require('./actors');
+const Elements = require('./elements');
 
 const RGObjects = {
-
     actors: Actors,
-    items: Items
-
-    //
-    /*
-    levels: [
-        {
-
-        },
-
-    ],
-   */
-
-    // Dungeons contains multiple levels. Any levels specified above can be used
-    // in the dungeon.
-    /*
-    dungeons: [
-
-    ],
-   */
-
+    items: Items,
+    elements: Elements
 };
 
-
 module.exports = RGObjects;
-
