a4cf913b1308f0441f9f41087baf3f4c72fc9cda
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-10-19 19:57:25 +0300

    Added loadScript() and try-catch blocks for onLoad()/onRemove() for plugins.

--- client/gui/plugin-manager.js
+++ client/gui/plugin-manager.js
@@ -1,12 +1,16 @@
+/* Contains code for handling plugin loading/removing and status management. */
 
+/* Each plugin is associated with an entry which stores the plugin code. */
 export class PluginEntry {
 
     constructor(json) {
-        this._enabled = true;
+        this._enabled = false;
         this._type = json.type;
         this._data = json.data;
         this._name = json.name;
         this._description = json.description;
+        this._status = 'UNLOADED';
+        this._errorMsg = '';
 
         this._onLoad = json.onLoad;
         this._onRemove = json.onRemove;
@@ -14,10 +18,26 @@ export class PluginEntry {
 
     getData() {return this._data;}
 
+    hasError() {
+        return (/ERROR/).test(this._status);
+    }
+
+    getErrorMsg() {
+        return this._errorMsg;
+    }
+
     disable() {
         if (this._enabled) {
             if (typeof this._onRemove === 'function') {
-                this._onRemove();
+                try {
+                    this._onRemove();
+                    this._status = 'UNLOADED';
+                    this._errorMsg = '';
+                }
+                catch (e) {
+                    this._errorMsg = e.message;
+                    this._status = 'REMOVE ERROR';
+                }
             }
             this._enabled = false;
         }
@@ -26,7 +46,15 @@ export class PluginEntry {
     enable() {
         if (!this._enabled) {
             if (typeof this._onLoad === 'function') {
-                this._onLoad();
+                try {
+                    this._onLoad();
+                    this._status = 'LOADED';
+                    this._errorMsg = '';
+                }
+                catch (e) {
+                    this._errorMsg = e.message;
+                    this._status = 'LOAD ERROR';
+                }
             }
             this._enabled = true;
         }
@@ -42,10 +70,12 @@ export class PluginEntry {
 
 }
 
+/* Manager contains list of plugins that have been loaded. */
 export default class PluginManager {
 
     constructor() {
         this._plugins = [];
+        this._errorMsg = '';
     }
 
     readJSON(json) {
@@ -101,6 +131,27 @@ export default class PluginManager {
         const plugin = this.findPlugin(name);
         if (plugin) {
             plugin.enable();
+        }
+    }
+
+    /* Loads a script using eval. */
+    loadScript(text) {
+        try {
+            /* eslint-disable */
+            let pluginData = null;
+            eval(text); // EVIL!!
+            if (pluginData) {
+                pluginData.data = text;
+                this.addPlugin(pluginData);
+                this._errorMsg = '';
+            }
+            else {
+                this._errorMsg = 'no pluginData specified in the script';
+            }
+            /* eslint-enable */
+        }
+        catch (e) {
+            this._errorMsg = e.message;
         }
     }
 }
