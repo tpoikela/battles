eb34a5c6309a091e556589be83148de7b896e18c
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-10-26 12:40:09 +0300

    Modified production build to have uglifyify.

--- bin/build_production.sh
+++ bin/build_production.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 
 npm run build:sass
-npm run create-config-prod && NODE_ENV=production ./browserify ./client/jsx/app.jsx --extension .jsx -t babelify | uglifyjs --mangle --compress > ./build/bundle.js
+npm run create-config-prod
+NODE_ENV=production ./browserify ./client/jsx/app.jsx --extension .jsx -t babelify -t uglifyify | uglifyjs --compress --mangle > ./build/bundle.js
