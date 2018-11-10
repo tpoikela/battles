666813c9a4f496dc1d292fdaf2345458b9da5e3d
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-11-10 16:41:57 +0200

    Changed into class format.

--- client/src/system/system.base.ts
+++ client/src/system/system.base.ts
@@ -1,8 +1,11 @@
 
-const RG = require('../rg');
-const Component = require('../component');
-const debug = require('debug')('bitn:System');
-const EventPool = require('../eventpool');
+import RG from '../rg';
+import * as Component from '../component';
+import {Entity} from  '../entity';
+import {EventPool} from '../eventpool';
+
+const dbg = require('debug');
+const debug = dbg('bitn:System');
 
 const POOL = EventPool.getPool();
 
@@ -13,134 +16,150 @@ const POOL = EventPool.getPool();
  * @param {array}  compTypes - Types of comps to listen to
  */
 //---------------------------------------------------
-const SystemBase = function(type, compTypes, pool) {
-    if (!Array.isArray(compTypes)) {
-        RG.err('System.Base', 'new',
-            '2nd arg must be an array of component types');
-    }
+export abstract class SystemBase {
 
-    this.type = type; // Type of the system
-    this.compTypes = compTypes; // Required comps in entity
-    this.entities = {}; // Entities requiring processing
+    public type: string; // Type of the system
+    public compTypes: string[];
+    public entities: {[key: string]: Entity};
 
     // If set to true, only one comp has to match the compTypes, otherwise all
     // components in compTypes must be present
-    this.compTypesAny = false;
+    public compTypesAny: boolean;
 
     /* Listens to add/removes for each component type in compTypes.*/
-    this.hasNotify = true;
+    public hasNotify: boolean;
 
-    // Add a listener for each specified component type
-    for (let i = 0; i < this.compTypes.length; i++) {
-        if (!Component.hasOwnProperty(this.compTypes[i])) {
+    public debugEnabled: boolean;
+
+    constructor(type: string, compTypes: string[], pool?: EventPool) {
+        if (!Array.isArray(compTypes)) {
             RG.err('System.Base', 'new',
-                `Comp type ${this.compTypes[i]} not in Component`);
+                '2nd arg must be an array of component types');
         }
 
-        if (pool) {
-            pool.listenEvent(this.compTypes[i], this);
+        this.type = type; // Type of the system
+        this.compTypes = compTypes; // Required comps in entity
+        this.entities = {}; // Entities requiring processing
+
+        // If set to true, only one comp has to match the compTypes, otherwise all
+        // components in compTypes must be present
+        this.compTypesAny = false;
+
+        /* Listens to add/removes for each component type in compTypes.*/
+        this.hasNotify = true;
+
+        // Add a listener for each specified component type
+        for (let i = 0; i < this.compTypes.length; i++) {
+            if (!Component.hasOwnProperty(this.compTypes[i])) {
+                RG.err('System.Base', 'new',
+                    `Comp type ${this.compTypes[i]} not in Component`);
+            }
+
+            if (pool) {
+                pool.listenEvent(this.compTypes[i], this);
+            }
+            else {
+                POOL.listenEvent(this.compTypes[i], this);
+            }
         }
-        else {
-            POOL.listenEvent(this.compTypes[i], this);
+
+        this.debugEnabled = debug.enabled;
+    }
+
+    addEntity(entity: Entity) {
+        this.entities[entity.getID()] = entity;
+    }
+
+    removeEntity(entity: Entity) {
+        delete this.entities[entity.getID()];
+    }
+
+    notify(evtName, obj) {
+        if (obj.hasOwnProperty('add')) {
+            if (this.hasCompTypes(obj.entity)) {this.addEntity(obj.entity);}
         }
-    }
-
-    this.debugEnabled = debug.enabled;
-};
-
-SystemBase.prototype.addEntity = function(entity) {
-    this.entities[entity.getID()] = entity;
-};
-
-SystemBase.prototype.removeEntity = function(entity) {
-    delete this.entities[entity.getID()];
-};
-
-SystemBase.prototype.notify = function(evtName, obj) {
-    if (obj.hasOwnProperty('add')) {
-        if (this.hasCompTypes(obj.entity)) {this.addEntity(obj.entity);}
-    }
-    else if (obj.hasOwnProperty('remove')) {
-        // Must check if any needed comps are still present, before removing
-        // the entity
-        if (!this.hasCompTypes(obj.entity)) {
-            this.removeEntity(obj.entity);
-        }
-    }
-};
-
-/* Returns true if entity has all required component types, or if
- * compTypesAny if set, if entity has any required component. */
-SystemBase.prototype.hasCompTypes = function(entity) {
-    const compTypes = this.compTypes;
-    if (this.compTypesAny === false) { // All types must be present
-        return entity.hasAll(compTypes);
-    }
-    else { // Only one compType has to be present
-        return entity.hasAny(compTypes);
-    }
-};
-
-/* Returns true if there is at least 1 entity to process. */
-SystemBase.prototype.hasEntities = function() {
-    return Object.keys(this.entities).length > 0;
-};
-
-SystemBase.prototype.update = function() {
-    for (const e in this.entities) {
-        if (!e) {continue;}
-        this.updateEntity(this.entities[e]);
-    }
-};
-
-/* For printing out debug information. */
-SystemBase.prototype.dbg = function(msg) {
-    if (debug.enabled) {
-        const nEnt = Object.keys(this.entities).length;
-        let descr = `[System ${this.type.toString()}]`;
-        descr += ` nEntities: ${nEnt}`;
-        debug(`${descr} ${msg}`);
-    }
-};
-
-//---------------------------------------------------------
-// Non-member functions used for utility in other systems
-//---------------------------------------------------------
-
-/* For adding skills experience components. */
-SystemBase.addSkillsExp = function addSkillsExp(att, skill, pts = 1) {
-    if (att.has('Skills')) {
-        const comp = new RG.Component.SkillsExp();
-        comp.setSkill(skill);
-        comp.setPoints(pts);
-        att.add(comp);
-    }
-};
-
-/* After succesful hit, adds the given comp to specified entity ent. */
-SystemBase.addCompToEntAfterHit = function addCompToEntAfterHit(
-    comp, ent, src
-) {
-    const compClone = comp.clone();
-
-    if (compClone.hasOwnProperty('duration')) {
-        const compDur = compClone.rollDuration();
-        const expiration = new RG.Component.Expiration();
-        expiration.addEffect(compClone, compDur);
-        ent.add(expiration);
-    }
-
-    // Source not present in negative buffs like StatsMods/CombatMods,
-    // but needed for Poison etc damage
-    if (compClone.getSource) {
-        const compSrc = compClone.getSource();
-        if (RG.isNullOrUndef([compSrc])) {
-            compClone.setSource(src);
+        else if (obj.hasOwnProperty('remove')) {
+            // Must check if any needed comps are still present, before removing
+            // the entity
+            if (!this.hasCompTypes(obj.entity)) {
+                this.removeEntity(obj.entity);
+            }
         }
     }
 
-    ent.add(compClone);
-};
+    /* Returns true if entity has all required component types, or if
+     * compTypesAny if set, if entity has any required component. */
+    hasCompTypes(entity): boolean {
+        const compTypes = this.compTypes;
+        if (this.compTypesAny === false) { // All types must be present
+            return entity.hasAll(compTypes);
+        }
+        else { // Only one compType has to be present
+            return entity.hasAny(compTypes);
+        }
+    }
 
+    /* Returns true if there is at least 1 entity to process. */
+    hasEntities(): boolean {
+        return Object.keys(this.entities).length > 0;
+    }
 
-module.exports = SystemBase;
+    update(): void {
+        for (const e in this.entities) {
+            if (!e) {continue;}
+            this.updateEntity(this.entities[e]);
+        }
+    }
+
+    updateEntity(e: Entity): void {
+        RG.err('SystemBase', 'updateEntity', 
+            'Not implemented in the base class');
+    }
+
+    /* For printing out debug information. */
+    dbg(msg): void {
+        if (debug.enabled) {
+            const nEnt = Object.keys(this.entities).length;
+            let descr = `[System ${this.type.toString()}]`;
+            descr += ` nEntities: ${nEnt}`;
+            debug(`${descr} ${msg}`);
+        }
+    }
+
+    //---------------------------------------------------------
+    // Non-member functions used for utility in other systems
+    //---------------------------------------------------------
+
+    /* For adding skills experience components. */
+    static addSkillsExp(att, skill, pts = 1): void {
+        if (att.has('Skills')) {
+            const comp = new Component.SkillsExp();
+            comp.setSkill(skill);
+            comp.setPoints(pts);
+            att.add(comp);
+        }
+    }
+
+    /* After succesful hit, adds the given comp to specified entity ent. */
+    static addCompToEntAfterHit(comp, ent, src): void {
+        const compClone = comp.clone();
+
+        if (compClone.hasOwnProperty('duration')) {
+            const compDur = compClone.rollDuration();
+            const expiration = new Component.Expiration();
+            expiration.addEffect(compClone, compDur);
+            ent.add(expiration);
+        }
+
+        // Source not present in negative buffs like StatsMods/CombatMods,
+        // but needed for Poison etc damage
+        if (compClone.getSource) {
+            const compSrc = compClone.getSource();
+            if (RG.isNullOrUndef([compSrc])) {
+                compClone.setSource(src);
+            }
+        }
+
+        ent.add(compClone);
+    }
+}
