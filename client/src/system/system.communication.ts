62cfaae510ebd2672fe04e10b2a5ac260b3d054b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-11-10 16:43:06 +0200

    Changed into class format.

--- client/src/system/system.communication.ts
+++ client/src/system/system.communication.ts
@@ -1,16 +1,29 @@
 
-const RG = require('../rg');
-
-const System = {};
-System.Base = require('./system.base');
+import RG from '../rg';
+import {Entity} from  '../entity';
+import {SystemBase} from './system.base';
+import {Communication} from '../component';
+import {EventPool} from '../eventpool';
 
 /* Processes entities with communication component.*/
-System.Communication = function(compTypes) {
-    System.Base.call(this, RG.SYS.COMMUNICATION, compTypes);
+export class SystemCommunication extends SystemBase { 
+
+    private _msgFunc: {[key: string]: (ent, msg) => void};
+
+    constructor(compTypes, pool?: EventPool) {
+        super('Communication', compTypes, pool);
+
+        // Dispatch table for different messages
+        this._msgFunc = {
+            Enemies: this.processEnemies.bind(this),
+            Shout: this.processShout.bind(this)
+        }
+
+    }
 
     // Each entity here has received communication and must capture its
     // information contents
-    this.updateEntity = function(ent) {
+    updateEntity(ent: Entity): void {
         const comComp = ent.get('Communication');
         const messages = comComp.getMsg();
         for (let i = 0; i < messages.length; i++) {
@@ -19,17 +32,17 @@ System.Communication = function(compTypes) {
         ent.remove('Communication');
     };
 
-    this.processMessage = (ent, msg) => {
-        if (_msgFunc.hasOwnProperty(msg.type)) {
-            _msgFunc[msg.type](ent, msg);
+    processMessage(ent, msg) {
+        if (this._msgFunc.hasOwnProperty(msg.type)) {
+            this._msgFunc[msg.type](ent, msg);
         }
         else {
             RG.err('CommunicationSystem', 'processMessage',
                 'No function for msg type |' + msg.type + '| in dtable.');
         }
-    };
+    }
 
-    this.processEnemies = (ent, msg) => {
+    processEnemies(ent, msg) {
         const enemies = msg.enemies;
         const srcName = msg.src.getName();
         for (let i = 0; i < enemies.length; i++) {
@@ -39,23 +52,15 @@ System.Communication = function(compTypes) {
             msg: `${srcName} seems to communicate with ${ent.getName()}`
         };
         RG.gameInfo(msgObj);
-    };
+    }
 
-    this.processShout = (ent, msg) => {
+    processShout(ent, msg) {
         const shoutMsg = msg.shout;
         const srcName = msg.src.getName();
         const msgObj = {cell: msg.src.getCell(),
             msg: `${srcName} shouts ${shoutMsg}`};
         RG.gameInfo(msgObj);
-    };
+    }
 
-    // Dispatch table for different messages
-    const _msgFunc = {
-        Enemies: this.processEnemies,
-        Shout: this.processShout
-    };
+}
 
-};
-RG.extend2(System.Communication, System.Base);
-
-module.exports = System.Communication;
