aa0bee49553dbb9ab62bfabb148180176de622be
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-11-11 19:00:47 +0200

    Converted area-effects into class.

--- client/src/system/system.area-effects.ts
+++ client/src/system/system.area-effects.ts
@@ -1,14 +1,38 @@
 
-const RG = require('../rg');
+import RG from '../rg';
+import {SystemBase} from './system.base';
 
-const System = {};
-System.Base = require('./system.base');
+export class SystemAreaEffects extends SystemBase {
+    public radRange: number;
 
-System.AreaEffects = function(compTypes) {
-    System.Base.call(this, RG.SYS.AREA_EFFECTS, compTypes);
-    this.radRange = 1;
+    constructor(compTypes, pool?) {
+        super(RG.SYS.AREA_EFFECTS, compTypes, pool);
+        this.radRange = 1;
+    }
 
-    this.updateEntity = function(ent) {
+    private _createRadiationComps(ent, compName, srcName) {
+        const map = ent.getLevel().getMap();
+        const cell = ent.getCell();
+        const [x, y] = cell.getXY();
+        const radiationBox = RG.Geometry.getBoxAround(x, y, this.radRange);
+        radiationBox.forEach(xy => {
+            if (map.hasXY(xy[0], xy[1])) {
+                const cell = map.getCell(xy[0], xy[1]);
+                if (cell.hasActors()) {
+                    const actors = cell.getActors();
+                    actors.forEach(actor => {
+                        // Name check prevents slow down when lots of fire
+                        // actors are present
+                        if (actor.getName() !== srcName) {
+                            actor.add(new RG.Component[compName]());
+                        }
+                    });
+                }
+            }
+        });
+    };
+
+    updateEntity(ent) {
         const flameComps = ent.getList('Flame');
         let isFire = false;
         let isIce = false;
@@ -40,31 +64,5 @@ System.AreaEffects = function(compTypes) {
         else if (isIce) {
             this._createRadiationComps(ent, 'Coldness', 'Ice flame');
         }
-    };
-
-    this._createRadiationComps = function(ent, compName, srcName) {
-        const map = ent.getLevel().getMap();
-        const cell = ent.getCell();
-        const [x, y] = cell.getXY();
-        const radiationBox = RG.Geometry.getBoxAround(x, y, this.radRange);
-        radiationBox.forEach(xy => {
-            if (map.hasXY(xy[0], xy[1])) {
-                const cell = map.getCell(xy[0], xy[1]);
-                if (cell.hasActors()) {
-                    const actors = cell.getActors();
-                    actors.forEach(actor => {
-                        // Name check prevents slow down when lots of fire
-                        // actors are present
-                        if (actor.getName() !== srcName) {
-                            actor.add(new RG.Component[compName]());
-                        }
-                    });
-                }
-            }
-        });
-    };
-
-};
-RG.extend2(System.AreaEffects, System.Base);
-
-module.exports = System.AreaEffects;
+    }
+}
