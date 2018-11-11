2d19164f0316d82df23c34b8a9d5fd231c552700
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-11-11 18:52:20 +0200

    Added imports and exports.

--- client/src/system/index.ts
+++ client/src/system/index.ts
@@ -9,33 +9,93 @@ import {SystemBase} from './system.base';
 export {SystemBase} from './system.base';
 System.Base = SystemBase;
 
-System.Animation = require('./system.animation');
-System.AreaEffects = require('./system.area-effects');
-System.Attack = require('./system.attack');
-System.BaseAction = require('./system.base-action');
-System.Battle = require('./system.battle');
-System.Chat = require('./system.chat');
+import {SystemAnimation} from './system.animation';
+export {SystemAnimation} from './system.animation';
+System.Animation = SystemAnimation;
+
+import {SystemAreaEffects} from './system.area-effects';
+export {SystemAreaEffects} from './system.area-effects';
+System.AreaEffects = SystemAreaEffects;
+
+import {SystemAttack} from './system.attack';
+export {SystemAttack} from './system.attack';
+System.Attack = SystemAttack;
+
+import {SystemBaseAction} from './system.base-action';
+export {SystemBaseAction} from './system.base-action';
+System.BaseAction = SystemBaseAction;
+
+import {SystemBattle} from './system.battle';
+export {SystemBattle} from './system.battle';
+System.Battle = SystemBattle;
+
+import {SystemChat} from './system.chat';
+export {SystemChat} from './system.chat';
+System.Chat = SystemChat;
+
 import {SystemCommunication} from './system.communication';
+export {SystemCommunication} from './system.communication';
 System.Communication = System.Communication;
-System.Damage = require('./system.damage');
-System.Disability = require('./system.disability');
-System.Effects = require('./system.effects');
-System.Equip = require('./system.equip');
-System.Events = require('./system.events');
-System.ExpPoints = require('./system.exp-points');
+
+import {SystemDamage} from './system.damage';
+export {SystemDamage} from './system.damage';
+System.Damage = SystemDamage;
+
+import {SystemDisability} from './system.disability';
+export {SystemDisability} from './system.disability';
+System.Disability = SystemDisability;
+
+import {SystemEffects} from './system.effects';
+export {SystemEffects} from './system.effects';
+System.Effects = SystemEffects;
+
+import {SystemEquip} from './system.equip';
+export {SystemEquip} from './system.equip';
+System.Equip = SystemEquip;
+
+import {SystemEvents} from './system.events';
+export {SystemEvents} from './system.events';
+System.Events = SystemEvents;
+
+import {SystemExpPoints} from './system.exp-points';
+export {SystemExpPoints} from './system.exp-points';
+System.ExpPoints = SystemExpPoints;
 
 import {SystemHunger} from './system.hunger';
 export {SystemHunger} from './system.hunger';
 System.Hunger = SystemHunger;
 
-System.Missile = require('./system.missile');
-System.Movement = require('./system.movement');
-System.Quest = require('./system.quest');
-System.Shop = require('./system.shop');
-System.Skills = require('./system.skills');
-System.SpellCast = require('./system.spell-cast');
-System.SpellEffect = require('./system.spell-effect');
-System.SpiritBind = require('./system.spirit-bind');
+import {SystemMissile} from './system.missile';
+export {SystemMissile} from './system.missile';
+System.Missile = SystemMissile;
+
+import {SystemMovement} from './system.movement';
+export {SystemMovement} from './system.movement';
+System.Movement = SystemMovement;
+
+import {SystemQuest} from './system.quest';
+export {SystemQuest} from './system.quest';
+System.Quest = SystemQuest;
+
+import {SystemShop} from './system.shop';
+export {SystemShop} from './system.shop';
+System.Shop = SystemShop;
+
+import {SystemSkills} from './system.skills';
+export {SystemSkills} from './system.skills';
+System.Skills = SystemSkills;
+
+import {SystemSpellCast} from './system.spell-cast';
+export {SystemSpellCast} from './system.spell-cast';
+System.SpellCast = SystemSpellCast;
+
+import {SystemSpellEffect} from './system.spell-effect';
+export {SystemSpellEffect} from './system.spell-effect';
+System.SpellEffect = SystemSpellEffect;
+
+import {SystemSpiritBind} from './system.spirit-bind';
+export {SystemSpiritBind} from './system.spirit-bind';
+System.SpiritBind = SystemSpiritBind;
 
 import {SystemTimeEffects} from './system.time-effects';
 export {SystemTimeEffects} from './system.time-effects';
@@ -67,5 +127,3 @@ System.UndefineSystem = function(sysName) {
     delete RG.SYS[nameCaps];
     delete System[sysName];
 };
-
-module.exports = System;
