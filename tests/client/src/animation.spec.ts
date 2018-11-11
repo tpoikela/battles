a7f809b431bf5b6d8e71f38a65c829d5ca63a9ee
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-11-11 11:02:53 +0200

    Added 1 unit test.

--- tests/client/src/animation.spec.ts
+++ tests/client/src/animation.spec.ts
@@ -1,13 +1,30 @@
 
 import { expect } from 'chai';
 
-const Animation = require('../../../client/src/animation').Animation;
+import {Animation, Frame} from '../../../client/src/animation';
 
 describe('Animation', () => {
+
+    it('consists of several frames', () => {
+        const fr: Frame = {
+            '0,0': {char: 'X', className: 'KKK'},
+            '0,1': {char: 'W', className: 'WWW'}
+        }
+        const anim1 = new Animation();
+        anim1.addFrame(fr);
+        expect(anim1.hasFrames()).to.equal(true);
+
+        const coordMap = {'0,0': true};
+        expect(anim1.hasCoord(coordMap)).to.equal(true);
+
+        const coordMap2 = {'2,2': true};
+        expect(anim1.hasCoord(coordMap2)).to.equal(false);
+    });
+
     it('can be combined with other animations', () => {
         const anim1 = new Animation();
         const anim2 = new Animation();
-        const fr1 = {'1,2': {cell: '*', className: 'xxx'}};
+        const fr1 = {'1,2': {char: '*', className: 'xxx'}};
 
         anim1.slowDown = 1;
         anim1.addFrame(fr1);
