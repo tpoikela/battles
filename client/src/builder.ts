a5e74a4ae45e581c45c0e774f52e281a34ce87fe
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-11-16 19:49:28 +0100

    Moved splitLevel() from Geometry.

--- client/src/builder.ts
+++ client/src/builder.ts
@@ -1,6 +1,9 @@
 
+import RG from './rg';
 import {ELEM} from '../data/elem-constants';
 import {CellMap} from './map';
+import {Level} from './level';
+import {FactoryLevel} from './factory.level';
 
 export interface CoordXY {
     x: number;
@@ -9,7 +12,7 @@ export interface CoordXY {
 
 export const Builder: any = {};
 
-Builder.addPathToMap = function(map: CellMap, coord: CoordXY[]) {
+Builder.addPathToMap = function(map: CellMap, coord: CoordXY[]): CoordXY[] {
     const chosenCoord = [];
     for (let j = 0; j < coord.length; j++) {
         const c = coord[j];
@@ -31,3 +34,83 @@ Builder.addPathToMap = function(map: CellMap, coord: CoordXY[]) {
     return chosenCoord;
 };
 
+/* Splits a larger level into a matrix of X by Y sublevels. This does
+* not preserve the original level for efficiency reasons, but extracts
+* all entities and moves them to smaller levels. */
+Builder.splitLevel = function(level: Level, conf): Level[] {
+    const levels = [];
+    const width = level.getMap().cols / conf.nLevelsX;
+    const height = level.getMap().rows / conf.nLevelsY;
+
+    for (let x = 0; x < conf.nLevelsX; x++) {
+        const levelCol = [];
+        for (let y = 0; y < conf.nLevelsY; y++) {
+            const subLevel = new FactoryLevel().createLevel('empty', width, height);
+            levelCol.push(subLevel);
+        }
+        levels.push(levelCol);
+    }
+
+    const getSubLevel = (x, y) => {
+        const subIndexX = Math.floor(x / width);
+        const subIndexY = Math.floor(y / height);
+        return levels[subIndexX][subIndexY];
+    };
+
+    const getSubX = x => x % width;
+    const getSubY = y => y % height;
+
+    // Move all the elements
+    const map = level.getMap();
+    for (let x = 0; x < map.cols; x++) {
+        for (let y = 0; y < map.rows; y++) {
+            // Get correct sub-level
+            const subLevel = getSubLevel(x, y);
+            const subX = getSubX(x);
+            const subY = getSubY(y);
+            subLevel.getMap().setBaseElemXY(
+                subX, subY, map.getBaseElemXY(x, y));
+
+            // Translate x,y to sub-level x,y
+        }
+    }
+
+    // Move actors
+    const actors = level.getActors().slice();
+    actors.forEach(actor => {
+        const aX = actor.getX();
+        const aY = actor.getY();
+        if (level.removeActor(actor)) {
+            const subLevel = getSubLevel(aX, aY);
+            const subX = getSubX(aX);
+            const subY = getSubY(aY);
+            subLevel.addActor(actor, subX, subY);
+        }
+        else {
+            RG.warn('Geometry', 'splitLevel',
+                `removeActor failed on ${JSON.stringify(actor)}`);
+        }
+    });
+
+    // Move items
+    const items = level.getItems().slice();
+    items.forEach(item => {
+        const aX = item.getX();
+        const aY = item.getY();
+        level.removeItem(item, aX, aY);
+
+        const subLevel = getSubLevel(aX, aY);
+        const subX = getSubX(aX);
+        const subY = getSubY(aY);
+        subLevel.addItem(item, subX, subY);
+    });
+
+    // Warn about existing stairs
+    const stairs = level.getStairs();
+    if (stairs.length > 0) {
+        RG.warn('Geometry', 'splitLevel',
+            'Function does not move stairs correctly (yet).');
+    }
+
+    return levels;
+}
--- client/src/geometry.ts
+++ client/src/geometry.ts
@@ -1,7 +1,6 @@
 
 import RG from './rg';
 import {Random} from './random';
-// import {FactoryLevel} from './factory.level';
 
 const RNG = Random.getRNG();
 
@@ -279,87 +278,6 @@ export const Geometry: any = {
         return nFound;
     },
 
-    /* Splits a larger level into a matrix of X by Y sublevels. This does
-    * not preserve the original level for efficiency reasons, but extracts
-    * all entities and moves them to smaller levels. */
-    splitLevel: function(level, conf) {
-        const levels = [];
-        const width = level.getMap().cols / conf.nLevelsX;
-        const height = level.getMap().rows / conf.nLevelsY;
-
-        for (let x = 0; x < conf.nLevelsX; x++) {
-            const levelCol = [];
-            for (let y = 0; y < conf.nLevelsY; y++) {
-                /* const subLevel = new FactoryLevel().createLevel('empty', width, height);
-                levelCol.push(subLevel);
-                */
-            }
-            levels.push(levelCol);
-        }
-
-        const getSubLevel = (x, y) => {
-            const subIndexX = Math.floor(x / width);
-            const subIndexY = Math.floor(y / height);
-            return levels[subIndexX][subIndexY];
-        };
-
-        const getSubX = x => x % width;
-        const getSubY = y => y % height;
-
-        // Move all the elements
-        const map = level.getMap();
-        for (let x = 0; x < map.cols; x++) {
-            for (let y = 0; y < map.rows; y++) {
-                // Get correct sub-level
-                const subLevel = getSubLevel(x, y);
-                const subX = getSubX(x);
-                const subY = getSubY(y);
-                subLevel.getMap().setBaseElemXY(
-                    subX, subY, map.getBaseElemXY(x, y));
-
-                // Translate x,y to sub-level x,y
-            }
-        }
-
-        // Move actors
-        const actors = level.getActors().slice();
-        actors.forEach(actor => {
-            const aX = actor.getX();
-            const aY = actor.getY();
-            if (level.removeActor(actor)) {
-                const subLevel = getSubLevel(aX, aY);
-                const subX = getSubX(aX);
-                const subY = getSubY(aY);
-                subLevel.addActor(actor, subX, subY);
-            }
-            else {
-                RG.warn('Geometry', 'splitLevel',
-                    `removeActor failed on ${JSON.stringify(actor)}`);
-            }
-        });
-
-        // Move items
-        const items = level.getItems().slice();
-        items.forEach(item => {
-            const aX = item.getX();
-            const aY = item.getY();
-            level.removeItem(item, aX, aY);
-
-            const subLevel = getSubLevel(aX, aY);
-            const subX = getSubX(aX);
-            const subY = getSubY(aY);
-            subLevel.addItem(item, subX, subY);
-        });
-
-        // Warn about existing stairs
-        const stairs = level.getStairs();
-        if (stairs.length > 0) {
-            RG.warn('Geometry', 'splitLevel',
-                'Function does not move stairs correctly (yet).');
-        }
-
-        return levels;
-    },
 
     /* Tiles the list of levels to main level l1. Tiled levels placed
      * side-by-side and aligned based on the conf. 'alignRight' will be
