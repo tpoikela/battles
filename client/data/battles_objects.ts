531022b1165e2349d76c7c5a50ac362097109bd2
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-11-17 23:23:29 +0200

    Corrected Actors -> ActorsData.

--- client/data/battles_objects.ts
+++ client/data/battles_objects.ts
@@ -1,11 +1,11 @@
 /* Include file for game objects like actors, items and elements. */
 
 import Items from './items';
-import Actors from './actors';
+import {ActorsData} from './actors';
 import Elements from './elements';
 
 export const Objects = {
-    actors: Actors,
+    actors: ActorsData,
     items: Items,
     elements: Elements
 };
