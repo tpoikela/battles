2e590b5d708000da34e9014cdbd0b47fd09db2ba
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-11-20 19:24:15 +0200

    Removed bind from functions, and added arrow function.

--- tests/client/src/verify.spec.ts
+++ tests/client/src/verify.spec.ts
@@ -15,14 +15,18 @@ describe('Verify', () => {
     it('can verify inputs args', () => {
         const conf = new Verify.Conf('Test module');
 
-        let func = conf.verifyConf.bind(conf, 'TestFunc', {aaa: 'aaa'}, ['aaa']);
+        let func = () => {
+            conf.verifyConf('TestFunc', {aaa: 'aaa'}, ['aaa']);
+        };
         expect(func).to.not.throw(Error);
 
-        func = conf.verifyConf.bind(conf, 'TestFunc', {bbb: 'aaa'}, ['aaa']);
+        func = () => {
+            conf.verifyConf('TestFunc', {bbb: 'aaa'}, ['aaa']);
+        };
         expect(func).to.throw(Error);
     });
 
-    it('can verify reqs with |', function() {
+    it('can verify reqs ORed with |', () => {
         const conf = new Verify.Conf('Verif split');
         let ok = conf.verifyReq({a: 1, b: 2, ccc: 3}, 'zzz|bbb|ccc');
         expect(ok).to.equal(true);
