d9691f18e443a36461f18cdd2b8009a28fe8ec4d
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2018-11-24 18:27:54 +0200

    Added from/toJSON().

--- client/gui/plugin-manager.ts
+++ client/gui/plugin-manager.ts
@@ -1,5 +1,10 @@
 /* Contains code for handling plugin loading/removing and status management. */
 
+import RG from '../src/rg';
+import {ObjectShell} from '../src/objectshellparser';
+
+import * as AllCode from '../src/battles';
+
 export interface PluginData {
     [key: string]: any;
 }
@@ -14,6 +19,7 @@ export class PluginEntry {
     public _description: string;
     public _status: string;
     public _errorMsg: string;
+    public _fileType: string;
 
     public _onLoad: () => void;
     public _onRemove: () => void;
@@ -24,6 +30,7 @@ export class PluginEntry {
         this._data = json.data;
         this._name = json.name;
         this._description = json.description;
+        this._fileType = json.fileType;
         this._status = 'UNLOADED';
         this._errorMsg = '';
 
@@ -31,7 +38,7 @@ export class PluginEntry {
         this._onRemove = json.onRemove;
     }
 
-    public getData() {return this._data;}
+    public getData(): PluginData {return this._data;}
 
     public hasError() {
         return (/ERROR/).test(this._status);
@@ -41,7 +48,7 @@ export class PluginEntry {
         return this._errorMsg;
     }
 
-    public disable() {
+    public disable(): void {
         if (this._enabled) {
             if (typeof this._onRemove === 'function') {
                 try {
@@ -51,14 +58,14 @@ export class PluginEntry {
                 }
                 catch (e) {
                     this._errorMsg = e.message;
-                    this._status = 'REMOVE ERROR';
+                    this._status = 'UNLOAD ERROR';
                 }
             }
             this._enabled = false;
         }
     }
 
-    public enable() {
+    public enable(): void {
         if (!this._enabled) {
             if (typeof this._onLoad === 'function') {
                 try {
@@ -75,34 +82,114 @@ export class PluginEntry {
         }
     }
 
-    public getName() {
+    public getName(): string {
         return this._name;
+    }
+
+    public getType(): string {
+        return this._type;
     }
 
     public isEnabled() {
         return this._enabled;
     }
 
+    public toJSON() {
+        return {
+            type: this._type,
+            fileType: this._fileType,
+            name: this._name,
+            data: this._data,
+            description: this._description
+        };
+    }
+
 }
 
 /* Manager contains list of plugins that have been loaded. */
-export default class PluginManager {
-    public _plugins: PluginEntry[];
-    public _errorMsg: string;
+export class PluginManager {
+
+    /* Returns a plugin manager from json. */
+    public static fromJSON(jsonArr): PluginManager {
+        const pm = new PluginManager();
+        jsonArr.forEach(plugin => {
+            if (plugin.fileType === 'json') {
+                pm.readJSON(plugin);
+            }
+            else {
+                pm.loadScript(plugin.data);
+            }
+        });
+        pm.enableAll();
+        /* TODO */
+        return pm;
+    }
+
+    private _plugins: PluginEntry[];
+    private _errorMsg: string;
+    private _readCallbacks: {[key: string]: (entry: PluginEntry) => void};
+    private _globalRefsWereSet: boolean;
 
     constructor() {
         this._plugins = [];
         this._errorMsg = '';
+        this._readCallbacks = {
+            [RG.TYPE_ACTOR]: this.parseShellsOnRead,
+            [RG.TYPE_ITEM]: this.parseShellsOnRead,
+            [RG.TYPE_ELEM]: this.parseShellsOnRead
+        };
+        this._globalRefsWereSet = false;
     }
 
-    public readJSON(json) {
-        const entry = new PluginEntry(json);
-        this._plugins.push(entry);
+    /* Loads plugin data in JSON format. */
+    public readJSON(json): PluginEntry {
+        json.fileType = 'json';
+        const entry = this.addPlugin(json);
+        const type = entry.getType();
+
+        if (this._readCallbacks[type]) {
+            this._readCallbacks[type](entry);
+        }
+        return entry;
+    }
+
+    /* Loads a script using eval. */
+    public loadScript(text: string): PluginEntry {
+        let entry: PluginEntry = null;
+        try {
+            this.setGlobalRefs();
+            /* tslint:disable */
+            let pluginData = null;
+            // Note that eval'ed text must set the pluginData
+            eval(text);
+            /* tslint:enable */
+            if (pluginData) {
+                pluginData.data = text;
+                pluginData.fileType = 'script';
+                entry = this.addPlugin(pluginData);
+                this._errorMsg = '';
+            }
+            else {
+                this._errorMsg = 'no pluginData specified in the script';
+            }
+
+            if (this._globalRefsWereSet) {
+                this.unsetGlobalRefs();
+            }
+            /* eslint-enable */
+        }
+        catch (e) {
+            this._errorMsg = e.message;
+            if (this._globalRefsWereSet) {
+                this.unsetGlobalRefs();
+            }
+            throw e;
+        }
         return entry;
     }
 
     /* Returns true if any of the plugins are enabled. */
-    public anyPluginsEnabled() {
+    public anyPluginsEnabled(): boolean {
         for (let i = 0; i < this._plugins.length; i++) {
             if (this._plugins[i].isEnabled()) {
                 return true;
@@ -111,25 +198,26 @@ export default class PluginManager {
         return false;
     }
 
-    public findPlugin(name) {
+    public findPlugin(name): PluginEntry | null {
         return this._plugins.find(p => p._name === name);
     }
 
-    public getPlugins() {
+    public getPlugins(): PluginEntry[] {
         return this._plugins.slice();
     }
 
-    public getPluginNames() {
+    public getPluginNames(): string[] {
         return this._plugins.map(p => p._name);
     }
 
-    public addPlugin(pluginData) {
+    public addPlugin(pluginData): PluginEntry {
+        this.validateType(pluginData);
         const entry = new PluginEntry(pluginData);
         this._plugins.push(entry);
         return entry;
     }
 
-    public deletePlugin(name) {
+    public deletePlugin(name: string): void {
         const index = this._plugins.findIndex(p => p.getName() === name);
         if (index >= 0) {
             this._plugins[index].disable();
@@ -137,38 +225,109 @@ export default class PluginManager {
         }
     }
 
-    public disablePlugin(name) {
-        const plugin = this.findPlugin(name);
+    public disablePlugin(name: string): void {
+        const plugin: PluginData = this.findPlugin(name);
         if (plugin) {
+            this.setGlobalRefs();
             plugin.disable();
+            this.unsetGlobalRefs();
         }
     }
 
-    public enablePlugin(name) {
-        const plugin = this.findPlugin(name);
+    public enablePlugin(name: string): void {
+        const plugin: PluginData = this.findPlugin(name);
         if (plugin) {
+            this.setGlobalRefs();
             plugin.enable();
+            this.unsetGlobalRefs();
         }
     }
 
-    /* Loads a script using eval. */
-    public loadScript(text) {
-        try {
-            /* eslint-disable */
-            const pluginData = null;
-            eval(text); // EVIL!!
-            if (pluginData) {
-                pluginData.data = text;
-                this.addPlugin(pluginData);
-                this._errorMsg = '';
+    public enableAll(): void {
+        this.setGlobalRefs();
+        this._plugins.forEach(plugin => {
+            plugin.enable();
+        });
+        this.unsetGlobalRefs();
+    }
+
+    public disableAll(): void {
+        this.setGlobalRefs();
+        this._plugins.forEach(plugin => {
+            plugin.disable();
+        });
+        this.unsetGlobalRefs();
+    }
+
+    public getError(): string {
+        return this._errorMsg;
+    }
+
+
+    /* Serialize the plugin manager. */
+    public toJSON(): any {
+        return this._plugins.map(p => p.toJSON());
+    }
+
+    protected setGlobalRefs(): void {
+        if (typeof window !== 'undefined') {
+            if (!(window as any).RG) {
+                console.log('SETTING GLOBAL REFS');
+                this._globalRefsWereSet = true;
+                (window as any).RG = AllCode;
             }
-            else {
-                this._errorMsg = 'no pluginData specified in the script';
-            }
-            /* eslint-enable */
         }
-        catch (e) {
-            this._errorMsg = e.message;
+        else if (typeof global !== 'undefined') {
+            if (!(global as any).RG) {
+                this._globalRefsWereSet = true;
+                (global as any).RG = AllCode;
+            }
         }
     }
+
+    protected unsetGlobalRefs(): void {
+        this._globalRefsWereSet = false;
+        if (typeof window !== 'undefined') {
+            (window as any).RG = undefined;
+        }
+        else if (typeof global !== 'undefined') {
+            (global as any).RG = undefined;
+        }
+    }
+
+    protected withGlobalRefs(cb: () => void): void {
+        this.setGlobalRefs();
+        cb();
+        this.unsetGlobalRefs();
+    }
+
+    protected parseShellsOnRead = (entry: PluginEntry): void => {
+        const data = entry.getData();
+        const type = entry.getType();
+        const types = [RG.TYPE_ACTOR, RG.TYPE_ITEM, RG.TYPE_ELEM];
+        types.forEach(propType => {
+            if (type === propType) {
+                const parser = ObjectShell.getParser();
+                parser.parseShellCateg(propType, data);
+            }
+        });
+    }
+
+    protected validateType(pluginData): void {
+        const types = [
+            'plugin', 'items', 'actors', 'elements', 'system'
+        ];
+        if (!pluginData.type) {
+            throw new Error('pluginData must have type specified');
+        }
+        const index = types.indexOf(pluginData.type);
+        if (index < 0) {
+            let msg = 'type must be any of the following: ';
+            msg += JSON.stringify(types);
+            msg += ` but got |${pluginData.type}|`;
+            throw new Error(msg);
+        }
+    }
+
+
 }
