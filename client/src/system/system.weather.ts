5acb0b041312e667cf8a3cc29314a734af30d952
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-01-24 22:46:46 +0100

    Added handleMeltSnow().

--- client/src/system/system.weather.ts
+++ client/src/system/system.weather.ts
@@ -4,6 +4,9 @@ import * as Component from '../component';
 import {SystemBase} from './system.base';
 import {EventPool} from '../eventpool';
 import {MapGenerator} from '../map.generator';
+import {Random} from '../random';
+
+const RNG = Random.getRNG();
 
 export class SystemWeather extends SystemBase {
 
@@ -13,18 +16,20 @@ export class SystemWeather extends SystemBase {
         super(RG.SYS.WEATHER, compTypes, pool);
 
         this._effTable = {
-            snowStorm: this.handleSnowStorm = this.handleSnowStorm.bind(this)
+            snowStorm: this.handleSnowStorm = this.handleSnowStorm.bind(this),
+            warm: this.handleMeltSnow = this.handleMeltSnow.bind(this)
         };
     }
 
     public updateEntity(ent): void {
         if (ent.has('WeatherEffect')) {
+            console.log('Updating WeatherEffect');
             const eff = ent.get('WeatherEffect');
             const effName = eff.getEffectType();
             if (this._effTable[effName]) {
                 this._effTable[effName](ent, eff);
             }
-            ent.remove(eff);
+            ent.removeAll('WeatherEffect');
         }
     }
 
@@ -32,6 +37,20 @@ export class SystemWeather extends SystemBase {
         const level = ent.getLevel();
         const map = level.getMap();
         MapGenerator.addRandomSnow(map, 0.1);
+    }
+
+    protected handleMeltSnow(ent, comp): void {
+        const ratio = 0.05;
+        const map = ent.getLevel().getMap();
+        const snowCells = map.getFree().filter(c => c.getBaseElem().has('Snowy'));
+        snowCells.forEach(cell => {
+            const meltSnow = RNG.getUniform();
+            if (meltSnow < ratio) {
+                const baseType = cell.getBaseElem().getType();
+                const newElem = MapGenerator.snowMeltMap[baseType];
+                cell.setBaseElem(newElem);
+            }
+        });
     }
 
 }
