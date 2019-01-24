9fd911264d01c831862694ec068a8f7cdb778fb3
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-01-24 22:45:18 +0100

    Removed nextAction from Actor.Weather.

--- client/src/actor.virtual.ts
+++ client/src/actor.virtual.ts
@@ -12,24 +12,14 @@ export class VirtualActor extends BaseActor {
         super(name);
         this._brain = new BrainVirtual(this);
     }
+
 }
 
 export class WeatherActor extends BaseActor {
 
-    private updateFreq: number;
-
     constructor(name: string) { // {{{2
         super(name);
         this._brain = new BrainWeather(this);
-        this.updateFreq = 10;
-    }
-
-    public nextAction(obj?): Time.Action | null {
-        if (--this.updateFreq === 0) {
-            return super.nextAction(obj);
-            this.updateFreq = 10;
-        }
-        return new Time.Action(0, ACTOR_NO_ACTION);
     }
 
 }
