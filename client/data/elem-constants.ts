9c0f345106e06e195c524be48c1995b8977b2c5c
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-02-01 16:19:03 +0200

    Added new elem constants for snowy elements.

--- client/data/elem-constants.ts
+++ client/data/elem-constants.ts
@@ -6,46 +6,51 @@ import {ElementBase, ElementWall} from '../src/element';
 
 export const ELEM: {[key: string]: ElementBase} = {};
 
+const frz = Object.freeze;
+
 const parser = ObjectShell.getParser();
 // Constant elements which can be used by all levels. freeze()
 // used to prevent any mutations. Note that elements with any state
 // in them should not be shared (unless state is common for all)
-ELEM.BRIDGE = Object.freeze(parser.createElement('bridge'));
-ELEM.CHASM = Object.freeze(parser.createElement('chasm'));
+ELEM.BRIDGE = frz(parser.createElement('bridge'));
+ELEM.CHASM = frz(parser.createElement('chasm'));
 
-ELEM.GRASS = Object.freeze(parser.createElement('grass'));
-ELEM.GRASS_SNOW = Object.freeze(parser.createElement('snowy grass'));
-ELEM.HIGH_ROCK = Object.freeze(parser.createElement('highrock'));
-ELEM.LAVA = Object.freeze(parser.createElement('lava'));
-ELEM.PATH = Object.freeze(parser.createElement('path'));
-ELEM.ROAD = Object.freeze(parser.createElement('road'));
-ELEM.SKY = Object.freeze(parser.createElement('sky'));
-ELEM.SNOW = Object.freeze(parser.createElement('snow'));
-ELEM.SNOW_DEEP = Object.freeze(parser.createElement('deep snow'));
-ELEM.SNOW_LIGHT = Object.freeze(parser.createElement('light snow'));
-ELEM.STONE = Object.freeze(parser.createElement('stone'));
-ELEM.TREE = Object.freeze(parser.createElement('tree'));
-ELEM.TREE_SNOW = Object.freeze(parser.createElement('snow-covered tree'));
+ELEM.GRASS = frz(parser.createElement('grass'));
+ELEM.GRASS_SNOW = frz(parser.createElement('snowy grass'));
+ELEM.HIGH_ROCK = frz(parser.createElement('highrock'));
+ELEM.LAVA = frz(parser.createElement('lava'));
+ELEM.PATH = frz(parser.createElement('path'));
+ELEM.ROAD = frz(parser.createElement('road'));
+ELEM.SKY = frz(parser.createElement('sky'));
+ELEM.SNOW = frz(parser.createElement('snow'));
+ELEM.SNOW_TRACKS = frz(parser.createElement('snow with tracks'));
+ELEM.SNOW_DEEP = frz(parser.createElement('deep snow'));
+ELEM.SNOW_DEEP_TRACKS = frz(parser.createElement('deep snow with tracks'));
+ELEM.SNOW_LIGHT = frz(parser.createElement('light snow'));
+ELEM.SNOW_LIGHT_TRACKS = frz(parser.createElement('light snow with tracks'));
+ELEM.STONE = frz(parser.createElement('stone'));
+ELEM.TREE = frz(parser.createElement('tree'));
+ELEM.TREE_SNOW = frz(parser.createElement('snow-covered tree'));
 
-ELEM.FLOOR = Object.freeze(parser.createElement('floor'));
-ELEM.FLOOR_CASTLE = Object.freeze(parser.createElement('floorcastle'));
-ELEM.FLOOR_CAVE = Object.freeze(parser.createElement('floorcave'));
-ELEM.FLOOR_CRYPT = Object.freeze(parser.createElement('floorcrypt'));
-ELEM.FLOOR_HOUSE = Object.freeze(parser.createElement('floorhouse'));
-ELEM.FLOOR_WOODEN = Object.freeze(parser.createElement('floorwooden'));
+ELEM.FLOOR = frz(parser.createElement('floor'));
+ELEM.FLOOR_CASTLE = frz(parser.createElement('floorcastle'));
+ELEM.FLOOR_CAVE = frz(parser.createElement('floorcave'));
+ELEM.FLOOR_CRYPT = frz(parser.createElement('floorcrypt'));
+ELEM.FLOOR_HOUSE = frz(parser.createElement('floorhouse'));
+ELEM.FLOOR_WOODEN = frz(parser.createElement('floorwooden'));
 
-ELEM.WALL = Object.freeze(new ElementWall('wall'));
-ELEM.WALL_CASTLE = Object.freeze(new ElementWall('wallcastle'));
-ELEM.WALL_CAVE = Object.freeze(new ElementWall('wallcave'));
-ELEM.WALL_CRYPT = Object.freeze(new ElementWall('wallcrypt'));
-ELEM.WALL_ICE = Object.freeze(new ElementWall('wallice'));
-ELEM.WALL_WOODEN = Object.freeze(new ElementWall('wallwooden'));
-ELEM.WALL_MOUNT = Object.freeze(new ElementWall('wallmount'));
+ELEM.WALL = frz(new ElementWall('wall'));
+ELEM.WALL_CASTLE = frz(new ElementWall('wallcastle'));
+ELEM.WALL_CAVE = frz(new ElementWall('wallcave'));
+ELEM.WALL_CRYPT = frz(new ElementWall('wallcrypt'));
+ELEM.WALL_ICE = frz(new ElementWall('wallice'));
+ELEM.WALL_WOODEN = frz(new ElementWall('wallwooden'));
+ELEM.WALL_MOUNT = frz(new ElementWall('wallmount'));
 
-// ELEM.WATER = Object.freeze(new Element.Water());
-ELEM.WATER = Object.freeze(parser.createElement('water'));
-ELEM.WATER_FROZEN = Object.freeze(parser.createElement('frozen water'));
-ELEM.FORT = Object.freeze(parser.createElement('fort'));
+// ELEM.WATER = frz(new Element.Water());
+ELEM.WATER = frz(parser.createElement('water'));
+ELEM.WATER_FROZEN = frz(parser.createElement('frozen water'));
+ELEM.FORT = frz(parser.createElement('fort'));
 
 export const ELEM_MAP: any = {};
 
