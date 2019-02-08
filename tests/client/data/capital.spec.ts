6abce839c3d189a9547cc319cb38ddf71e6d45f5
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-02-08 10:56:08 +0200

    Fixed imports after ts conversion.

--- tests/client/data/capital.spec.ts
+++ tests/client/data/capital.spec.ts
@@ -1,18 +1,15 @@
 
 import { expect } from 'chai';
-import Capital from '../../../client/data/capital';
-
-const RG = require('../../../client/src/battles');
+import * as RG from '../../../client/src/battles';
+import {Capital} from '../../../client/data/capital';
 
 describe('Capital', () => {
-    it('can be created without errors', () => {
-        const capitalLevel = new Capital(100, 300).getLevel();
-        expect(capitalLevel).to.exist;
-    });
 
     it('can be simulated without errors', () => {
         const capitalLevel = new Capital(100, 300).getLevel();
-        const game = new RG.Game.Main();
+        expect(capitalLevel).to.not.be.empty;
+
+        const game = new RG.GameMain();
         game.addActiveLevel(capitalLevel);
         for (let i = 0; i < 100; i++) {
             expect(() => {game.simulateGame();}).not.to.throw();
