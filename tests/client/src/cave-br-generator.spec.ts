0ace465c08251623e42ed8bd1c37fff712d1ba0d
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-02-19 11:08:03 +0200

    Added for-loop to generate multiple levels to try the speed.

--- tests/client/src/cave-br-generator.spec.ts
+++ tests/client/src/cave-br-generator.spec.ts
@@ -5,15 +5,19 @@ import {createCaveLevel} from '../../../client/src/cave-br-generator';
 import {Level} from '../../../client/src/level';
 import {Random} from '../../../client/src/random';
 
-describe('createCaveLevel', () => {
+describe('createCaveLevel', function() {
+    this.timeout(5000);
+
     it('creates cave levels', () => {
-        const conf: any = {
-            connectAll: true,
-            numBranches: 30,
-            connectedRatio: 0.50
-        };
-        const n = 1;
-        const level: Level = createCaveLevel(n * 120, n * 120, conf);
-        level.debugPrintInASCII();
+        for (let i = 0; i < 200; i++) {
+            const conf: any = {
+                connectAll: true,
+                numBranches: 30,
+                connectedRatio: 0.50
+            };
+            const n = 1;
+            const level: Level = createCaveLevel(n * 120, n * 120, conf);
+            // level.debugPrintInASCII();
+        }
     });
 });
