942c46fb568490e771fb3250c68862b565a949b9
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-02-19 11:04:32 +0200

    Added import Random.

--- tests/client/src/cave-br-generator.spec.ts
+++ tests/client/src/cave-br-generator.spec.ts
@@ -3,17 +3,17 @@ import { expect } from 'chai';
 import RG from '../../../client/src/rg';
 import {createCaveLevel} from '../../../client/src/cave-br-generator';
 import {Level} from '../../../client/src/level';
-
+import {Random} from '../../../client/src/random';
 
 describe('createCaveLevel', () => {
     it('creates cave levels', () => {
         const conf: any = {
             connectAll: true,
             numBranches: 30,
-            connectedRatio: 0.00
+            connectedRatio: 0.50
         };
         const n = 1;
-        const level: Level = createCaveLevel(n * 150, n * 50, conf);
+        const level: Level = createCaveLevel(n * 120, n * 120, conf);
         level.debugPrintInASCII();
     });
 });
