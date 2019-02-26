a073a85afa18a557bf41dccc90ff72eeecc84f15
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-02-26 20:16:10 +0200

    Added unit test for combat functions.

--- tests/client/src/rg.spec.ts
+++ tests/client/src/rg.spec.ts
@@ -1,6 +1,9 @@
 
 import { expect } from 'chai';
 import RG from '../../../client/src/rg';
+import {SentientActor} from '../../../client/src/actor';
+import {Inventory} from '../../../client/src/inv';
+import * as Item from '../../../client/src/item';
 
 describe('RG', () => {
     it('it manages cell styles and chars', () => {
@@ -9,5 +12,20 @@ describe('RG', () => {
         const style = RG.getCssClass(RG.TYPE_ITEM, 'weapon');
         expect(style).to.equal('cell-item-magic');
 
+    });
+
+    it('has functions for combat range/damage', () => {
+        const archer = new SentientActor('archer');
+
+        let rr = RG.getMeleeAttackRange(archer);
+        expect(rr).to.equal(1);
+
+        const potion = new Item.Potion('potion');
+        Inventory.equipAnyItem(archer, potion);
+        rr = RG.getMeleeAttackRange(archer);
+        expect(rr).to.equal(1);
+
+        const meleeInfo = RG.getMeleeAttackInfo(archer);
+        expect(meleeInfo).to.match(/D: \d+/);
     });
 });
