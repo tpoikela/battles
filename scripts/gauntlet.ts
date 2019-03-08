921b230c5e6734e4b80341fde2e303209a6b67cc
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-03-08 22:35:15 +0100

    Renamed file to .ts.

--- scripts/gauntlet.ts
+++ scripts/gauntlet.ts
@@ -2,19 +2,20 @@
 /* This script determines the relative strength of the actors by making them
  * fight each other. */
 
-require('babel-register');
-
-// const RG = require('../client/src/battles');
+import {ActorGen} from '../client/data/actor-gen';
 
 const ActorBattles = require('../tests/actor-battles');
-const Actors = require('../client/data/actors.js');
+// const Actors = require('../client/data/actors.js');
 
 const matchLimit = process.argv[2] || 2000;
 const nRounds = 3;
 
-const shells = Actors.filter(a => !((/spirit/i).test(a.name)));
+// const shells = Actors.filter(a => !((/spirit/i).test(a.name)));
+// const monitorActor = 'lich';
 
-const monitorActor = 'lich';
+const shells = ActorGen.genActors(100);
+const monitorActor = shells[0].name;
+
 const ab = new ActorBattles({monitorActor, matchLimit, shells});
 
 ab.runSweep(nRounds);
@@ -26,5 +27,3 @@ const durationMs = ab.getDuration();
 const matchesPerSec = numMatches / (durationMs / 1000);
 console.log('Total duration ' + (durationMs / 1000) + ' s');
 console.log('Matches per sec: ' + matchesPerSec);
-
-
