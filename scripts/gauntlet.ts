25c0297194a803a864f108a740adfacf0e545fad
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-03-10 21:08:01 +0100

    Added minDanger filter.

--- scripts/gauntlet.ts
+++ scripts/gauntlet.ts
@@ -10,13 +10,18 @@ const ActorBattles = require('../tests/actor-battles');
 const matchLimit = process.argv[2] || 2000;
 const nRounds = 3;
 
+const minDanger = 5;
+
 // const shells = Actors.filter(a => !((/spirit/i).test(a.name)));
 // const monitorActor = 'lich';
 
 let shells = ActorGen.genActors(100);
 const monitorActor = shells[0].name;
-
 shells = shells.concat(ActorsData);
+
+console.log('Before filter: ', shells.length, ' shells');
+shells = shells.filter(shell => shell.danger >= minDanger);
+console.log('AFTER filter: ', shells.length, ' shells');
 
 const ab = new ActorBattles({monitorActor, matchLimit, shells});
 
