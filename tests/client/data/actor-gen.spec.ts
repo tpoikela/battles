ed5acede63313530991e119d1fc2b0d2c272d230
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-03-19 22:31:59 +0100

    Added unit tests for actor-gen.

---
+++ tests/client/data/actor-gen.spec.ts
@@ -0,0 +1,31 @@
+
+import { expect } from 'chai';
+import {ActorGen} from '../../../client/data/actor-gen';
+import {ObjectShell} from '../../../client/src/objectshellparser';
+import {Constraints} from '../../../client/src/constraints';
+
+describe('ActorGen', () => {
+
+    let fact = null;
+    let parser = null;
+
+    beforeEach(() => {
+        parser = new ObjectShell.Parser();
+        fact = new Constraints();
+    });
+
+    it('description', () => {
+        const actors = ActorGen.genActors(50);
+        parser.parseShellData({actors});
+        const races = ActorGen.getRaces();
+
+        const cc = [
+            {op: 'lt', prop: 'danger', value: 10},
+            {op: 'eq', prop: 'type', value: races}
+        ];
+        const cFunc = fact.getConstraints(cc);
+        const filtered = actors.filter(cFunc);
+        expect(filtered.length).to.be.above(0);
+        console.log(filtered);
+    });
+});
