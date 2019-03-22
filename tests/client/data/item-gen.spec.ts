c3b2664f4244f3031208fd8b81cdf0f6514c755e
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-03-22 23:23:44 +0100

    Added unit test for ItemGen.buildShell().

--- tests/client/data/item-gen.spec.ts
+++ tests/client/data/item-gen.spec.ts
@@ -3,13 +3,15 @@ import { expect } from 'chai';
 import {ItemGen} from '../../../client/data/item-gen';
 import {mixNewShell} from '../../../client/data/shell-utils';
 
+const {shellProps} = ItemGen;
+
 describe('ItemGen', () => {
 
     it('can generate items from shells', () => {
         const baseShell = {
             damage: '1d6'
         };
-        const daggerShell = {damage: '1d6'};
+        const daggerShell = {damage: '1d6', weight: 1.2};
         const sharpShell = {damage: '5'};
         const allShells = [baseShell, daggerShell, sharpShell];
         const sharpDaggerShell = mixNewShell(allShells);
@@ -18,6 +20,31 @@ describe('ItemGen', () => {
         const adder = {addDamage: '2d10 + 3'};
         const addedDaggerShell = mixNewShell([daggerShell, adder]);
         expect(addedDaggerShell.damage).to.equal('3d16 + 3');
+
+        const heavy = {weight: 0.5};
+
+        const heavyDagger = mixNewShell([daggerShell, heavy]);
+        expect(heavyDagger.weight).to.equal(0.6);
+        const heavyDagger2 = mixNewShell([heavy, daggerShell]);
+        expect(heavyDagger2.weight).to.equal(0.6);
+
+        const moreDmg = {damage: '1d2 + 1'};
+        const fortDagger = mixNewShell([daggerShell, moreDmg]);
+        expect(fortDagger.damage).to.equal('2d4 + 1');
+    });
+
+    it('can build shells from a name map', () => {
+        const nameMap = {
+            type: 'weapon', name: 'sword',
+            prefix: 'light', suffix: 'ofVoid',
+            material: 'void'
+        };
+        const newShell = ItemGen.buildShell(nameMap);
+        expect(newShell.name).to.match(/light void sword of Void/);
+
+        nameMap.material = 'iron';
+        const ironSword = ItemGen.buildShell(nameMap);
+        expect(ironSword.damage).to.equal('2d5 + 3');
     });
 
     it('has function generate random item shells', () => {
