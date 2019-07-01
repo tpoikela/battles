0408e558fdf214347dc8da9129f7954305dacee3
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-07-01 21:49:11 +0300

    Added system.attack-missile

---
+++ client/src/system/system.attack-missile.ts
@@ -0,0 +1,88 @@
+
+import RG from '../rg';
+import {SystemBase} from './system.base';
+import * as Component from '../component';
+// import {Cell} from '../map.cell';
+type ItemAmmo = import('../item').Ammo;
+type MissileWeapon = import('../item').MissileWeapon;
+
+export class SystemAttackMissile extends SystemBase {
+
+    constructor(compTypes, pool?) {
+        super(RG.SYS.ATTACK_MISSILE, compTypes, pool);
+    }
+
+    public updateEntity(ent): void {
+        const attComp = ent.get('AttackMissile');
+        const target = attComp.getTarget();
+        const actor = attComp.getAttacker();
+        const invEq = actor.getInvEq();
+        let fireRate = 1;
+        if (actor.has('DoubleShot')) {
+            fireRate = 2;
+        }
+
+        // Fires one missile comp each firerate increment
+        for (let i = 0; i < fireRate; i++) {
+
+            const missile = invEq.unequipAndGetItem('missile', 1, 0);
+            if (!RG.isNullOrUndef([missile])) {
+
+                // Check for missile weapon for ammunition
+                if (missile.has('Ammo')) {
+                    const missWeapon = invEq.getEquipment()
+                        .getEquipped('missileweapon') as unknown;
+                    if (missWeapon === null) {
+                        const msg = 'No missile weapon equipped.';
+                        this.cmdNotPossible(ent, msg);
+                    }
+                    else { // Check ammo/weapon compatibility
+                        const ammo = missile as unknown;
+                        const ammoType = (ammo as ItemAmmo).getAmmoType();
+                        const weaponType = (missWeapon as MissileWeapon).getWeaponType();
+                        if (actor.has('MixedShot')) {
+                            const re = /bow/;
+                            if (!re.test(ammoType) || !re.test(weaponType)) {
+                                if (ammoType !== weaponType) {
+                                    const msg = 'Ammo/weapon not compatible.';
+                                    this.cmdNotPossible(ent, msg);
+                                }
+                            }
+                        }
+                        else if (ammoType !== weaponType) {
+                            const msg = 'Ammo/weapon not compatible.';
+                            this.cmdNotPossible(ent, msg);
+                        }
+                    }
+                }
+
+                if (!RG.isNullOrUndef([target])) {
+                    const [x, y] = target.getXY();
+                    const mComp = new Component.Missile(actor);
+                    mComp.setTargetXY(x, y);
+                    mComp.setDamage(RG.getMissileDamage(actor, missile));
+                    mComp.setAttack(RG.getMissileAttack(actor, missile));
+                    mComp.setRange(RG.getMissileRange(actor, missile));
+                    missile.add(mComp);
+                    ent.get('Action').setEnergy(RG.energy.MISSILE);
+                }
+                else {
+                    RG.err('System.AttackMissile', 'updateEntity',
+                        'No x,y given for missile.');
+                }
+            }
+            else {
+                this.cmdNotPossible(ent, 'No missile equipped');
+            }
+        }
+        ent.remove(attComp);
+    }
+
+    protected cmdNotPossible(ent: any, msg: string): void {
+        ent.get('Action').setMsg(msg);
+        ent.get('Action').setStatus(-1);
+        if (ent.has('Player')) {
+            ent.add(new Component.ImpossibleCmd());
+        }
+    }
+}
