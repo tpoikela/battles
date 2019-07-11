765a3630e69a5e5015d92cf5ce32fd80f5389001
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-07-11 19:43:27 +0300

    Added functions for processing phase/day changed events

--- client/src/system/system.world-sim.ts
+++ client/src/system/system.world-sim.ts
@@ -5,11 +5,14 @@
 
 import RG from '../rg';
 import {SystemBase} from './system.base';
+import {WS_EVENT} from '../world.simulation';
 
 type WorldTop = import('../world').WorldTop;
 type OWMap = import('../overworld.map').OWMap;
 type WorldSimulation = import('../world.simulation').WorldSimulation;
 type Territory = import('../territory').Territory;
+type EventPool = import('../eventpool').EventPool;
+type Entity = import('../entity').Entity;
 
 export class SystemWorldSim extends SystemBase {
 
@@ -18,25 +21,51 @@ export class SystemWorldSim extends SystemBase {
     protected worldTop: WorldTop;
     protected territory: Territory;
 
-    constructor(compTypes, pool?) {
+    protected _dtable: {[key: string]: (ent, wsEvent) => void};
+
+    constructor(compTypes: string[], pool?: EventPool) {
         super(RG.SYS.WORLD_SIM, compTypes, pool);
         this.legalArgs = ['owMap', 'worldSim', 'worldTop', 'territory'];
+        this._dtable = {};
+        this._dtable[WS_EVENT.PHASE_CHANGED] = this._processPhaseChanged.bind(this);
+        this._dtable[WS_EVENT.DAY_CHANGED] = this._processDayChanged.bind(this);
     }
 
-    public updateEntity(ent): void {
-        const worldEvt = ent.get('WorldSimEvent');
-        this._checkEntityValid(ent);
-        console.log('WorldEvent was processed OK');
-        console.log(this.worldSim.toString());
-        ent.remove(worldEvt);
+    public updateEntity(ent: Entity): void {
+        const worldEvtList = ent.getList('WorldSimEvent');
+        worldEvtList.forEach(worldEvt => {
+            this._checkEntityValid(ent);
+            this._processEvent(ent, worldEvt);
+            console.log('WorldEvent was processed OK');
+            console.log(this.worldSim.toString());
+            ent.remove(worldEvt);
+        });
     }
 
-    protected _checkEntityValid(ent): void {
-        if (ent.getName() !== 'WorldEntity') {
-            const name = ent.getName();
+    protected _checkEntityValid(ent: Entity): void {
+        const entName = RG.getEntName(ent);
+        if (entName !== 'WorldEntity') {
+            const name = entName;
             RG.err('System.WorldSim', 'updateEntity',
                 `Only WorldEntity can be added to System.WorldSim. Got: ${name}`);
         }
     }
+
+    protected _processEvent(ent: Entity, wsEvent): void {
+        const evtType: string = wsEvent.getEventType();
+        if (this._dtable.hasOwnProperty(evtType)) {
+            this._dtable[evtType](ent, wsEvent);
+        }
+    }
+
+    // Callbacks for events
+    protected _processPhaseChanged(ent: Entity, wsEvent): void {
+        const evtData = wsEvent.getEventData();
+        RG.gameMsg(`It is ${evtData.currPhase} now.`);
+    }
+
+    protected _processDayChanged(ent: Entity, wsEvent): void {
+    }
+
 
 }
