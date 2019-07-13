eb110c4821c1a50d6cdc9da238e959d43f846507
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-07-13 15:37:35 +0300

    Fixed lint errors

--- client/src/gamesave.ts
+++ client/src/gamesave.ts
@@ -22,17 +22,17 @@ export class GameSave {
         this._playerList = '_battles_player_data_';
     }
 
-    setStorage(stor: Storage) {this._storageRef = stor;}
+    public setStorage(stor: Storage): void {this._storageRef = stor;}
 
-    getDungeonLevel(): number {return this._dungeonLevel;}
+    public getDungeonLevel(): number {return this._dungeonLevel;}
 
     /* Main function which saves the full game.*/
-    save(game, conf): void {
+    public save(game, conf): void {
         this.savePlayer(game, conf);
     }
 
     /* Restores game/player with the given name.*/
-    restore(name): any {
+    public restore(name: string): any {
         if (!RG.isNullOrUndef([name])) {
             const game = this.restorePlayer(name);
             return game;
@@ -44,7 +44,7 @@ export class GameSave {
     }
 
     /* Returns a list of saved players.*/
-    getPlayersAsList(): string[] {
+    public getPlayersAsList(): string[] {
         const dbObj = this.getPlayersAsObj();
         if (dbObj !== null) {
             return Object.keys(dbObj).map((val) => dbObj[val]);
@@ -55,14 +55,14 @@ export class GameSave {
     }
 
     /* Returns an object containing the saved players.*/
-    getPlayersAsObj() {
+    public getPlayersAsObj() {
         this._checkStorageValid();
         const dbString = this._storageRef.getItem(this._playerList);
         return JSON.parse(dbString);
     }
 
     /* Deletes given player from the list of save games.*/
-    deletePlayer(name) {
+    public deletePlayer(name: string): void {
         this._checkStorageValid();
         let dbString = this._storageRef.getItem(this._playerList);
         const dbObj = JSON.parse(dbString);
@@ -74,7 +74,7 @@ export class GameSave {
     }
 
     /* Saves a player object. */
-    savePlayer(game, conf) {
+    public savePlayer(game, conf): void {
         this._checkStorageValid();
         const player = game.getPlayer();
         if (!RG.isNullOrUndef([player])) {
@@ -88,7 +88,7 @@ export class GameSave {
     }
 
     /* Restores a player with given name. */
-    restorePlayer(name) {
+    public restorePlayer(name: string): any | null {
         this._checkStorageValid();
         const playersObj = this.getPlayersAsObj();
         if (playersObj.hasOwnProperty(name)) {
@@ -107,7 +107,7 @@ export class GameSave {
     }
 
     /* Saves name and level of the player into a list of players/save games.*/
-    _savePlayerInfo(name, obj, conf) {
+    protected _savePlayerInfo(name: string, obj, conf): void {
         let dbString = this._storageRef.getItem(this._playerList);
         let dbObj = JSON.parse(dbString);
         if (dbObj === null) {dbObj = {};}
@@ -126,7 +126,7 @@ export class GameSave {
         this._storageRef.setItem(this._playerList, dbString);
     }
 
-    _checkStorageValid() {
+    protected _checkStorageValid() {
         if (RG.isNullOrUndef([this._storageRef])) {
             throw new Error('GameSave you must setStorage() first.');
         }
