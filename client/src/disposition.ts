cd492da1db9a234c6542ee8e6a5973a9b972c59e
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-07-30 17:01:04 +0300

    Converted into class

--- client/src/disposition.ts
+++ client/src/disposition.ts
@@ -2,80 +2,103 @@
 
 import RG from './rg';
 import {Random} from './random';
+import {RandWeights} from './interfaces';
 const RNG = Random.getRNG();
 
-export const Disposition = function(rivals, conf?) {
-    this.rivals = rivals;
-    this.conf = Object.assign({
-    }, conf);
+interface WeightValues {
+    ally: number;
+    neutral: number;
+    enemy: number;
+}
 
-    // Weights used for randomisation
-    this.weights = {
-        default: {
-            ally: 20,
-            neutral: 50,
-            enemy: 30
-        }
+interface IWeights {
+    [key: string]: WeightValues;
+}
+
+export interface IDispTable {
+    [key: string]: {
+        [key: string]: string;
     };
-};
+}
 
-Disposition.prototype.setWeights = function(weights) {
-    this.weights = weights;
-};
+export class Disposition {
 
-Disposition.prototype.addWeight = function(rival, weights) {
-    this.weights[rival] = weights;
-};
+    public rivals: string[];
+    public conf: object;
+    public weights: IWeights;
+    public dispTable: IDispTable;
 
-Disposition.prototype.getTable = function() {
-    return this.dispTable;
-};
+    constructor(rivals: string[], conf: object) {
+        this.rivals = rivals;
+        this.conf = Object.assign({
+        }, conf);
 
-Disposition.prototype._initTable = function() {
-    this.dispTable = {};
-    this.rivals.forEach(rival1 => {
-        this.dispTable[rival1] = {};
-    });
-};
-
-Disposition.prototype.randomize = function() {
-    this._initTable();
-    this.rivals.forEach(rival1 => {
-        this.rivals.forEach(rival2 => {
-            if (!this.pairDone(rival1, rival2)) {
-                const weights = this.getWeights(rival1, rival2);
-                const disposition = RNG.getWeighted(weights);
-                this.dispTable[rival1][rival2] = disposition;
-                this.dispTable[rival2][rival1] = disposition;
+        // Weights used for randomisation
+        this.weights = {
+            default: {
+                ally: 20,
+                neutral: 50,
+                enemy: 30
             }
+        };
+    }
+
+    public setWeights(weights: IWeights): void {
+        this.weights = weights;
+    }
+
+    public addWeight(rival: string, weights: WeightValues): void {
+        this.weights[rival] = weights;
+    }
+
+    public getTable(): IDispTable {
+        return this.dispTable;
+    }
+
+    public _initTable() {
+        this.dispTable = {};
+        this.rivals.forEach(rival1 => {
+            this.dispTable[rival1] = {};
         });
-    });
-};
-
-/* Returns the weights for given pair. */
-Disposition.prototype.getWeights = function(r1, r2) {
-    // TODO smart merging of weights if given
-    if (this.weights.hasOwnProperty(r1)) {
-        return this.weights[r1];
     }
-    else if (this.weights.hasOwnProperty(r2)) {
-        return this.weights[r2];
-    }
-    return this.weights.default;
-};
 
-Disposition.prototype.pairDone = function(r1, r2) {
-    if (r1 === r2) {return true;} // No self-computation
-    if (this.dispTable[r1][r2]) {
-        if (!this.dispTable[r2][r1]) {
-            RG.err('Disposition', 'pairDone',
-                'Something went wrong. No [r2][r1] but [r1][r2] exists');
+    public randomize(): void {
+        this._initTable();
+        this.rivals.forEach(rival1 => {
+            this.rivals.forEach(rival2 => {
+                if (!this.pairDone(rival1, rival2)) {
+                    const weights = this.getWeights(rival1, rival2);
+                    const ww = weights as unknown;
+                    const disposition = RNG.getWeighted(ww as RandWeights);
+                    this.dispTable[rival1][rival2] = disposition;
+                    this.dispTable[rival2][rival1] = disposition;
+                }
+            });
+        });
+    }
+
+    /* Returns the weights for given pair. */
+    public getWeights(r1: string, r2: string): WeightValues {
+        // TODO smart merging of weights if given
+        if (this.weights.hasOwnProperty(r1)) {
+            return this.weights[r1];
         }
-        return true;
+        else if (this.weights.hasOwnProperty(r2)) {
+            return this.weights[r2];
+        }
+        return this.weights.default;
     }
-    return false;
-};
 
-/* Convert into human-readable format. */
-Disposition.prototype.toString = function() {
-};
+    public pairDone(r1: string, r2: string): boolean {
+        if (r1 === r2) {return true;} // No self-computation
+        if (this.dispTable[r1][r2]) {
+            if (!this.dispTable[r2][r1]) {
+                RG.err('Disposition', 'pairDone',
+                    'Something went wrong. No [r2][r1] but [r1][r2] exists');
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
