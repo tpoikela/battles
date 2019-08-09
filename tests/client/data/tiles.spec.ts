d91a0406e26ddfa8d306ee2f845bdcc3b981ff46
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-08-09 18:27:32 +0300

    Added tests for custom filter

--- tests/client/data/tiles.spec.ts
+++ tests/client/data/tiles.spec.ts
@@ -5,7 +5,7 @@ import RG from '../../../client/src/rg';
 import {TemplateLevel} from '../../../client/src/template.level';
 // import {HousesDiag} from '../../../client/data/tiles.diag';
 import {Tiles3x3} from '../../../client/data/tiles3x3';
-import {Basic5x5} from '../../../client/data/tiles.basic';
+import {Basic5x5, Basic7x7} from '../../../client/data/tiles.basic';
 
 describe('HousesDiag', () => {
     /*
@@ -42,17 +42,62 @@ describe('HousesDiag', () => {
     });
 
     it('generates dungeons with 5x5 tiles', () => {
-            const level = new TemplateLevel(24, 16);
+            const level = new TemplateLevel(23, 11);
             level.tryToMatchAllExits = true;
             level.setFiller(Basic5x5.tiles.filler);
-            level.setGenParams({x: [1, 3, 1], y: [2, 2]});
+            level.setGenParams({x: [1, 2, 1], y: [2, 1, 2]});
             // level.setGenParams({x: [3], y: [3]});
             level.setTemplates(Basic5x5.templates);
             // level.setTemplates(Tiles3x3.templates.vault);
             level.roomCount = -1;
+            // level.customMatchFilter = customMatchFilter;
             level.setExitMap(Basic5x5.remap.exits, Basic5x5.remap.nsew2Dir);
             level.create();
             console.log('\n=== Created level: ====\n');
             RG.printMap(level.getMap());
     });
+
+    it('generates dungeons with 7x7 tiles', () => {
+            const level = new TemplateLevel(23, 11);
+            level.tryToMatchAllExits = true;
+            level.setFiller(Basic7x7.tiles.filler);
+            level.setGenParams({x: [1, 2, 1], y: [1, 2, 1]});
+            level.setTemplates(Basic7x7.templates);
+            // level.setTemplates(Tiles3x3.templates.vault);
+            level.roomCount = -1;
+            level.customMatchFilter = filterCells;
+            // level.setExitMap(Basic5x5.remap.exits, Basic5x5.remap.nsew2Dir);
+            level.create();
+            console.log('\n=== Created level: ====\n');
+            RG.printMap(level.getMap());
+    });
 });
+
+function customMatchFilter(tl: TemplateLevel, x: number, y: number, list, prev) {
+    if (x === Math.ceil(tl.tilesX / 2) || y === Math.round(tl.tilesY/2)) {
+        console.log('Matching custom filter x,y', x, y);
+        const res = list.filter(t => /hall/.test(t.getProp('name')));
+        console.log('Matching custom filter, len was:', res.length);
+        if (res.length > 0) {
+            return res;
+        }
+        console.log('\t==> REJECTED custom match with 0 len');
+    }
+    else if (tl.isEdge(x, y)) {
+        console.log('Matching custom filter x,y', x, y);
+        const res = list.filter(t => /^(corridor|corner)$/.test(t.getProp('name')));
+        if (res.length > 0) {
+            return res;
+        }
+        console.log('\t==> REJECTED custom match with 0 len');
+    }
+    return list;
+}
+
+function filterCells(tl: TemplateLevel, x: number, y: number, list, prev) {
+    if (tl.isEdge(x, y)) {
+        const res = list.filter(t => /small_room/.test(t.getProp('name')));
+        if (res.length > 0) {return res;}
+    }
+    return list;
+}
