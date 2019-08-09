45b94e884d50f547340b5f1fd8f7447f08cc9cc1
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-08-09 21:17:15 +0300

    Added tests to check with different weights

--- tests/client/data/tiles.spec.ts
+++ tests/client/data/tiles.spec.ts
@@ -5,9 +5,10 @@ import RG from '../../../client/src/rg';
 import {TemplateLevel} from '../../../client/src/template.level';
 // import {HousesDiag} from '../../../client/data/tiles.diag';
 import {Tiles3x3} from '../../../client/data/tiles3x3';
-import {Basic5x5, Basic7x7} from '../../../client/data/tiles.basic';
+import {Basic5x5} from '../../../client/data/tiles.basic';
+import {Basic7x7} from '../../../client/data/tiles.7x7';
 
-describe('HousesDiag', () => {
+describe('TemplateLevel level generation', () => {
     /*
     it('generates diagonal-like structures', () => {
         const level = new TemplateLevel(12, 8);
@@ -45,13 +46,14 @@ describe('HousesDiag', () => {
             const level = new TemplateLevel(23, 11);
             level.tryToMatchAllExits = true;
             level.setFiller(Basic5x5.tiles.filler);
-            level.setGenParams({x: [1, 2, 1], y: [2, 1, 2]});
+            level.setGenParams({x: [1, 1, 1], y: [1, 1, 1]});
             // level.setGenParams({x: [3], y: [3]});
             level.setTemplates(Basic5x5.templates);
             // level.setTemplates(Tiles3x3.templates.vault);
             level.roomCount = -1;
             // level.customMatchFilter = customMatchFilter;
             level.setExitMap(Basic5x5.remap.exits, Basic5x5.remap.nsew2Dir);
+            level.weights = {room_end: 10, room_term1: 10};
             level.create();
             console.log('\n=== Created level: ====\n');
             RG.printMap(level.getMap());
@@ -64,6 +66,11 @@ describe('HousesDiag', () => {
             level.setGenParams({x: [1, 2, 1], y: [1, 2, 1]});
             level.setTemplates(Basic7x7.templates);
             // level.setTemplates(Tiles3x3.templates.vault);
+            // level.weights = {room_hall: 15, room: 7, room_small: 7};
+            level.weights = {};
+            Basic7x7.names.diag.forEach(name => {
+                level.weights[name] = 10;
+            });
             level.roomCount = -1;
             level.customMatchFilter = filterCells;
             // level.setExitMap(Basic5x5.remap.exits, Basic5x5.remap.nsew2Dir);
