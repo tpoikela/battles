59caac0b7624d23653eb922f020cf9d3d4c5e251
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-09-02 19:39:26 +0300

    Added ExploreContextProcessor.

--- tests/helpers/context-processors.ts
+++ tests/helpers/context-processors.ts
@@ -4,6 +4,8 @@ import {Random} from '../../client/src/random';
 
 type PlayerDriver = import('./player-driver').PlayerDriver;
 type DriverContext = import('./player-driver').DriverContext;
+type Level = import('../../client/src/level').Level;
+type Cell = import('../../client/src/map.cell').Cell;
 
 const RNG = Random.getRNG();
 
@@ -15,7 +17,13 @@ export class ContextProcessor {
 
     public processContext(): boolean {
         if (this.isWithinContext()) {
-            return this._process();
+            const action = this.drv.action;
+            const ok = this._process();
+            const newAction = this.drv.action;
+            if (action !== newAction) {
+                console.log(`${this.name} set action ${action} -> ${newAction}`);
+            }
+            return ok;
         }
         return false;
     }
@@ -115,8 +123,13 @@ export class EnemyContextProcessor extends ContextProcessor {
 /* Processor is used when player is on level belonging to Area. */
 export class AreaContextProcessor extends ContextProcessor {
 
+    protected visitedLevels: {[key: string]: number};
+    protected maxVisitWait: number;
+
     constructor(public name: string, public drv: PlayerDriver) {
         super(name, drv);
+        this.visitedLevels = {};
+        this.maxVisitWait = 200;
     }
 
     /* Processor applies only when player is in Area. */
@@ -129,8 +142,43 @@ export class AreaContextProcessor extends ContextProcessor {
         return false;
     }
 
+    public hasVisited(level: Level): boolean {
+        const currTurns = this.drv.nTurns;
+        const visitTurns = this.visitedLevels[level.getID()];
+        return (currTurns - visitTurns) < this.maxVisitWait;
+    }
+
     protected _process(): boolean {
+        const player = this.drv.getPlayer();
+        const pCell = player.getCell();
+
+        if (pCell.hasPassage()) {
+            const conn = pCell.getConnection();
+            const targetLevel = conn.getTargetLevel();
+            if (!this.hasVisited(targetLevel)) {
+                this.drv.action = 'stairs';
+                this.visitedLevels[targetLevel.getID()] = this.drv.nTurns;
+                return true;
+            }
+        }
+
         if (!this.drv.hasPath()) {
+            const cells: Cell[] = player.getBrain().getSeenCells();
+            const connCells = cells.filter(c => c.hasPassage());
+            if (connCells.length > 0) {
+                console.log(`Found ${connCells.length} connections`);
+                for (let i = 0; i < connCells.length; i++) {
+                    const cell = connCells[i];
+                    const conn = cell.getConnection();
+                    const targetLevel = conn.getTargetLevel();
+                    if (!this.hasVisited(targetLevel as Level)) {
+                        if (this.drv.setPathAction(conn.getXY())) {
+                            break;
+                        }
+                    }
+                }
+            }
+
             // Main functions:
             // - Move player towards north
             //   - We can find unexplored cell by doing +1 FOV
@@ -139,6 +187,34 @@ export class AreaContextProcessor extends ContextProcessor {
         return false;
     }
 
+}
+
+export class ExploreContextProcessor extends ContextProcessor {
+
+    constructor(public name: string, public drv: PlayerDriver) {
+        super(name, drv);
+    }
+
+    /* Processor applies only when player is in AreaTiles. */
+    public isWithinContext(): boolean {
+        return true;
+    }
+
+    protected _process(): boolean {
+        const player = this.drv.getPlayer();
+        const map = player.getLevel().getMap();
+        const [cells, unseen]: [Cell[], Cell[]] = map.getCellsInFOVPlus(player, 1);
+        if (unseen.length > 0) {
+            const unExplored = unseen.filter((c: Cell) => !c.isExplored());
+            if (unExplored.length > 0) {
+                const cell = RNG.arrayGetRand(unExplored);
+                if (this.drv.setPathAction(cell.getXY())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
 
 export class ZoneContextProcessor extends ContextProcessor {
