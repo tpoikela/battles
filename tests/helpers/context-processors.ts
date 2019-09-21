0e9c741617fa9d8c645d9a01cfa704a0bbce1b94
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-09-21 12:37:47 +0300

    Added RandomContextProc. Bug fixes.

--- tests/helpers/context-processors.ts
+++ tests/helpers/context-processors.ts
@@ -1,11 +1,19 @@
 
+import RG from '../../client/src/rg';
 import {Brain, BrainPlayer} from '../../client/src/brain';
 import {Random} from '../../client/src/random';
+import {TCoord} from '../../client/src/interfaces';
+import { GoalExplore } from '../../client/src/goals';
 
 type PlayerDriver = import('./player-driver').PlayerDriver;
 type DriverContext = import('./player-driver').DriverContext;
 type Level = import('../../client/src/level').Level;
 type Cell = import('../../client/src/map.cell').Cell;
+
+import dbg = require('debug');
+const debug = dbg('bitn:ContextProcessor');
+
+debug.enabled = true;
 
 const RNG = Random.getRNG();
 
@@ -18,10 +26,11 @@ export class ContextProcessor {
     public processContext(): boolean {
         if (this.isWithinContext()) {
             const action = this.drv.action;
+            console.log(`${this.name} isWithinContext() OK, start action |${action}|`);
             const ok = this._process();
             const newAction = this.drv.action;
             if (action !== newAction) {
-                console.log(`${this.name} set action ${action} -> ${newAction}`);
+                console.log(`${this.name} modified action |${action}| -> |${newAction}|`);
             }
             return ok;
         }
@@ -35,6 +44,12 @@ export class ContextProcessor {
     protected _process(): boolean {
         return false;
     }
+
+    protected dbg(...args: any[]): void {
+        if (debug.enabled) {
+            debug(`${this.name}: `, ...args);
+        }
+    }
 }
 
 export class EnemyContextProcessor extends ContextProcessor {
@@ -46,9 +61,10 @@ export class EnemyContextProcessor extends ContextProcessor {
     constructor(public name: string, public drv: PlayerDriver) {
         super(name, drv);
 
-        this.hpLow = RNG.getUniformRange(0.25, 0.40);
-        this.ppRestLimit = RNG.getUniformRange(0.75, 1.0);
-        this.hpRestLimit = RNG.getUniformRange(0.75, 1.0);
+        this.ppRestLimit = RNG.getUniformRange(0.10, 0.25);
+
+        this.hpLow = RNG.getUniformRange(0.10, 0.25);
+        this.hpRestLimit = RNG.getUniformRange(0.25, 0.5);
     }
 
     public isWithinContext(): boolean {
@@ -104,7 +120,9 @@ export class EnemyContextProcessor extends ContextProcessor {
             const spellPower = player.get('SpellPower');
             const pp = spellPower.getPP();
             const maxPP = spellPower.getMaxPP();
-            return pp < (this.ppRestLimit * maxPP);
+            if (pp < (this.ppRestLimit * maxPP)) {
+                return true;
+            }
         }
         const health = player.get('Health');
         const maxHP = health.getMaxHP();
@@ -152,6 +170,7 @@ export class AreaContextProcessor extends ContextProcessor {
         const player = this.drv.getPlayer();
         const pCell = player.getCell();
 
+        // Enter an unvisited passage, if found
         if (pCell.hasPassage()) {
             const conn = pCell.getConnection();
             const targetLevel = conn.getTargetLevel();
@@ -162,6 +181,7 @@ export class AreaContextProcessor extends ContextProcessor {
             }
         }
 
+        // No path set, continue searching
         if (!this.drv.hasPath()) {
             const cells: Cell[] = player.getBrain().getSeenCells();
             const connCells = cells.filter(c => c.hasPassage());
@@ -173,7 +193,7 @@ export class AreaContextProcessor extends ContextProcessor {
                     const targetLevel = conn.getTargetLevel();
                     if (!this.hasVisited(targetLevel as Level)) {
                         if (this.drv.setPathAction(conn.getXY())) {
-                            break;
+                            return true;
                         }
                     }
                 }
@@ -186,6 +206,7 @@ export class AreaContextProcessor extends ContextProcessor {
         }
         else {
             this.drv.action = 'path';
+            return true;
         }
         return false;
     }
@@ -198,7 +219,7 @@ export class ExploreContextProcessor extends ContextProcessor {
         super(name, drv);
     }
 
-    /* Processor applies only when player is in AreaTiles. */
+    /* Explore can be applied anywhere. */
     public isWithinContext(): boolean {
         return true;
     }
@@ -220,13 +241,72 @@ export class ExploreContextProcessor extends ContextProcessor {
     }
 }
 
+export class RandomContextProcessor extends ContextProcessor {
+
+    public prob: number;
+
+    constructor(public name: string, public drv: PlayerDriver) {
+        super(name, drv);
+        this.prob = 0.10;
+    }
+
+    /* Explore can be applied anywhere. */
+    public isWithinContext(): boolean {
+        return true;
+    }
+
+    protected _process(): boolean {
+        if (!this.drv.hasPath()) {
+            const goal = new GoalExplore(this.drv.player);
+            goal.setNewPassableDir();
+            const pathLen = RNG.getUniformInt(1, 10);
+            const [pX, pY] = this.drv.player.getXY();
+            const [newX, newY] = [pX + pathLen * goal.dX, pY + pathLen * goal.dY];
+            if (this.drv.setPathAction([newX, newY])) {
+                return true;
+            }
+            else {
+                // Take a step to random direction
+                const [nX, nY] = [pX + goal.dX, pY + goal.dY];
+                if (this.drv.setPathAction([nX, nY])) {
+                    return true;
+                }
+                else {
+                    const msg = `Dir: ${[goal.dX, goal.dY]}, ` +
+                        `pXY: ${this.drv.player.getXY()}, impassable: ${[nX, nY]}`;
+                    this.drv.player.getLevel().debugPrintInASCII();
+                    RG.err('RandomContextProcessor', '_process',
+                        `Fallback proc failed! ${msg}`);
+                }
+            }
+        }
+        else {
+            this.drv.action = 'path';
+            return true;
+        }
+        return false;
+    }
+}
+
+interface LevelData {
+    stairsUp: TCoord[];
+    stairsDown: TCoord[];
+    turnsExplored: number;
+}
+
 export class ZoneContextProcessor extends ContextProcessor {
 
     public reZones: RegExp;
+    public scannedLevels: {[key: number]: LevelData};
+    public visitedStairs: {[key: string]: boolean};
+    public state: {[key: string]: any};
 
     constructor(public name: string, public drv: PlayerDriver) {
         super(name, drv);
         this.reZones = /mountain|dungeon/;
+        this.scannedLevels = {};
+        this.visitedStairs = {};
+        this.state = {};
     }
 
     /* Processor applies only when player is in AreaTiles. */
@@ -236,13 +316,106 @@ export class ZoneContextProcessor extends ContextProcessor {
         if (parent) {
             return this.reZones.test(parent.getType());
         }
+        this.state = {}; // Clear state in non-context
         return false;
     }
 
     protected _process(): boolean {
-        if (!this.drv.hasPath()) {
+        const level = this.drv.player.getLevel();
+        const id = level.getID();
+        let ok = false;
+        // scan the seen the cells first (not every turn)
+        if (!this.scannedLevels[id]) {
+            this.scanLevel(level);
         }
-        return false;
+        this.scannedLevels[id].turnsExplored += 1;
+
+        if (!this.drv.hasPath()) {
+            // Check for stairs down, then up
+            const stairsDown = this.scannedLevels[id].stairsDown;
+            const stairsUp = this.scannedLevels[id].stairsUp;
+
+            // Last level reached, do some exploration, but set goUp
+            // variable to prevent going down on stairs again
+            if (stairsDown.length === 0) {
+                if (!this.state.goUp) {
+                    this.dbg(`No stairsDown found. goUp triggered for ${id}`);
+                    this.state.goUp = true;
+                }
+                if (this.scannedLevels[id].turnsExplored < 300) {
+                    return false;
+                }
+            }
+
+            const pCell: Cell = this.drv.player.getCell();
+            if (pCell.hasStairs()) {
+                this.dbg(`hasStairs() OK at ${pCell.getXY()}`);
+                if (this.state.goUp) {
+                    const stairs = pCell.getStairs()!;
+                    if (stairs.getName() === 'stairsUp') {
+                        this.drv.action = 'stairs';
+                        this.addVisited(id, pCell.getXY());
+                        return true;
+                    }
+                }
+                else if (!this.hasVisited(id, pCell.getXY())) {
+                    this.drv.action = 'stairs';
+                    this.addVisited(id, pCell.getXY());
+                    return true;
+                }
+            }
+
+            let stairsLists = [stairsUp];
+            if (!this.state.goUp) {
+                stairsLists = [stairsDown, stairsUp];
+            }
+
+            stairsLists.forEach(sList => {
+                sList.forEach(sXY => {
+                    if (!ok && (this.state.goUp || !this.hasVisited(id, sXY))) {
+                        if (this.drv.setPathAction(sXY)) {
+                            this.dbg(`Setting path to stairs ${pCell.getXY()} -> ${sXY}`);
+                            ok = true;
+                        }
+                    }
+                });
+            });
+        }
+        else {
+            this.drv.action = 'path';
+            return true;
+        }
+        return ok;
+    }
+
+    protected hasVisited(id: number, xy: TCoord): boolean {
+        const key = '' + id + ',' + xy[0] + ',' + xy[1];
+        return this.visitedStairs[key];
+    }
+
+    protected addVisited(id: number, xy: TCoord): void {
+        const key = '' + id + ',' + xy[0] + ',' + xy[1];
+        this.visitedStairs[key] = true;
+    }
+
+    protected scanLevel(lv: Level): void {
+        console.log(`${this.name} scanning level ${lv.getID()} for stairs..`);
+        const lvData: LevelData = {
+            stairsUp: [], stairsDown: [],
+            turnsExplored: 0
+        };
+        const elems = lv.getElements();
+        // TODO show only explored elems
+        elems.forEach(elem => {
+            if (elem.getName() === 'stairsDown') {
+                lvData.stairsDown.push(elem.getXY());
+            }
+            else if (elem.getName() === 'stairsUp') {
+                lvData.stairsUp.push(elem.getXY());
+            }
+        });
+
+        this.scannedLevels[lv.getID()] = lvData;
     }
 
 }
