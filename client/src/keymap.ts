85f5c10e65900a455887a93d0a0f24047519c93f
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-10-15 10:02:10 +0300

    Converted KeyMap into class

--- client/src/keymap.ts
+++ client/src/keymap.ts
@@ -5,7 +5,222 @@ import RG from './rg';
 import ROT from '../../lib/rot';
 import {TCoord} from './interfaces';
 
-export const Keys: any = {};
+/* Lookup table object for movement and actions keys.*/
+class KeyMap {
+    public moveKeyMap: {[key: string]: any};
+
+    constructor() {
+        this.moveKeyMap = {};
+    }
+
+    // Start from W, go clock wise on keyboard
+    public initMap(): void {
+        this.moveKeyMap[Keys.KEY.MOVE_N] = 0;
+        this.moveKeyMap[Keys.KEY.MOVE_NE] = 1;
+        this.moveKeyMap[Keys.KEY.MOVE_E] = 2;
+        this.moveKeyMap[Keys.KEY.MOVE_SE] = 3;
+        this.moveKeyMap[Keys.KEY.MOVE_S] = 4;
+        this.moveKeyMap[Keys.KEY.MOVE_SW] = 5;
+        this.moveKeyMap[Keys.KEY.MOVE_W] = 6;
+        this.moveKeyMap[Keys.KEY.MOVE_NW] = 7;
+
+        this.moveKeyMap[ROT.VK_8] = 0;
+        this.moveKeyMap[ROT.VK_9] = 1;
+        this.moveKeyMap[ROT.VK_6] = 2;
+        this.moveKeyMap[ROT.VK_3] = 3;
+        this.moveKeyMap[ROT.VK_2] = 4;
+        this.moveKeyMap[ROT.VK_1] = 5;
+        this.moveKeyMap[ROT.VK_4] = 6;
+        this.moveKeyMap[ROT.VK_7] = 7;
+    }
+
+    public inMoveCodeMap(code: number): boolean {
+        return this.moveKeyMap.hasOwnProperty(code);
+    }
+
+    public isRest(code: number): boolean {
+        return (code === Keys.VK_s || code === Keys.VK_PERIOD);
+    }
+    public isPickup(code: number): boolean {
+        return code === Keys.KEY.PICKUP;
+    }
+    public isUseStairs(code: number): boolean {
+        return (
+        code === Keys.KEY.USE_STAIRS_DOWN || code === Keys.KEY.USE_STAIRS_UP
+        );
+    }
+
+    public isChat(code: number): boolean {
+        return code === Keys.KEY.CHAT;
+    }
+    public isConfirmYes(code: number): boolean {
+        return code === Keys.KEY.YES;
+    }
+    public isConfirmNo(code: number): boolean {
+        return code === Keys.KEY.NO;
+    }
+    public isFightMode(code: number): boolean {
+        return code === Keys.KEY.FIGHT;
+    }
+    public isGive(code: number): boolean {
+        return code === Keys.KEY.GIVE;
+    }
+    public isGoto(code: number): boolean {
+        return code === Keys.KEY.GOTO;
+    }
+    public isJump(code: number): boolean {
+        return code === Keys.KEY.JUMP;
+    }
+    public isIssueOrder(code: number): boolean {
+        return code === Keys.KEY.ORDER;
+    }
+    public isLook(code: number): boolean {
+        return code === Keys.KEY.LOOK;
+    }
+    public isMark(code: number): boolean {
+        return code === Keys.KEY.MARK;
+    }
+    public isNextItem(code: number): boolean {
+        return code === Keys.KEY.NEXT_ITEM;
+    }
+    public isNextTarget(code: number): boolean {
+        return code === Keys.KEY.NEXT;
+    }
+    public isPrevTarget(code: number): boolean {
+        return code === Keys.KEY.PREV;
+    }
+    public isRead(code: number): boolean {
+        return code === Keys.KEY.READ;
+    }
+    public isRunMode(code: number): boolean {
+        return code === Keys.KEY.RUN;
+    }
+    public isSelect(code: number): boolean {
+        return code === Keys.KEY.SELECT;
+    }
+    public isSelectAll(code: number): boolean {
+        return code === Keys.KEY.SELECT_ALL;
+    }
+    public isTargetMode(code: number): boolean {
+        return code === Keys.KEY.TARGET;
+    }
+    public isToggleDoor(code: number): boolean {
+        return code === Keys.KEY.DOOR;
+    }
+    public isUsePower(code: number): boolean {
+        return code === Keys.KEY.POWER;
+    }
+    public isUseAbility(code: number): boolean {
+        return code === Keys.KEY.ABILITY;
+    }
+    public isMultiPurpose(code: number): boolean {
+        return code === Keys.KEY.MULTI;
+    }
+
+    /* Based on keycode, computes and returns a new x,y pair. If code is
+     * invalid, returns null. */
+    public getDiff(code: number, x: number, y: number): TCoord | null {
+        if (this.moveKeyMap.hasOwnProperty(code)) {
+            const diff = ROT.DIRS[8][this.moveKeyMap[code]];
+            const newX = x + diff[0];
+            const newY = y + diff[1];
+            return [newX, newY];
+        }
+        else if (code === Keys.VK_s) {
+            return [x, y];
+        }
+        else {
+            return null;
+        }
+    }
+
+    /* Returns a direction vector for given keycode. */
+    public getDir(code: number): TCoord | null {
+        if (this.moveKeyMap.hasOwnProperty(code)) {
+            const selIndex = this.moveKeyMap[code];
+            return ROT.DIRS[8][selIndex] as TCoord;
+        }
+        else if (this.isRest(code)) {
+            return [0, 0];
+        }
+        return null;
+    }
+
+    /* Converts a direction vector to keycode. */
+    public dirToKeyCode(dXArg: number | TCoord, dYArg?: number): number | null {
+        // Normalize first to unit vector (-1,0 or 1)
+        let dX = dXArg as number;
+        let dY = dYArg;
+        if (Array.isArray(dXArg)) {
+            dX = dXArg[0] as number;
+            dY = dXArg[1];
+        }
+        if (dX !== 0) {dX = dX / Math.abs(dX);}
+        if (dY !== 0) {dY = dY / Math.abs(dY);}
+
+        switch (dX) {
+            case -1:
+                switch (dY) {
+                    case -1: return Keys.KEY.MOVE_NW;
+                    case 0: return Keys.KEY.MOVE_W;
+                    case 1: return Keys.KEY.MOVE_SW;
+                    default: RG.err('Keys.KeyMap', 'dirToKeyCode',
+                        `Dir ${dX},${dY} not supported`);
+                }
+                break;
+            case 0:
+                switch (dY) {
+                    case -1: return Keys.KEY.MOVE_N;
+                    case 0: return Keys.KEY.REST;
+                    case 1: return Keys.KEY.MOVE_S;
+                    default: RG.err('Keys.KeyMap', 'dirToKeyCode',
+                        `Dir ${dX},${dY} not supported`);
+                }
+                break;
+            case 1:
+                switch (dY) {
+                    case -1: return Keys.KEY.MOVE_NE;
+                    case 0: return Keys.KEY.MOVE_E;
+                    case 1: return Keys.KEY.MOVE_SE;
+                    default: RG.err('Keys.KeyMap', 'dirToKeyCode',
+                        `Dir ${dX},${dY} not supported`);
+                }
+                break;
+            default: RG.err('Keys.KeyMap', 'dirToKeyCode',
+                `Dir ${dX},${dY} not supported`);
+        }
+        return null;
+    }
+
+    public keyCodeToCardinalDir(code: number): string {
+        switch (code) {
+            case Keys.KEY.MOVE_NW: return 'NW';
+            case Keys.KEY.MOVE_W: return 'W';
+            case Keys.KEY.MOVE_SW: return 'SW';
+            case Keys.KEY.MOVE_N: return 'N';
+            case Keys.KEY.REST: return 'REST';
+            case Keys.KEY.MOVE_S: return 'S';
+            case Keys.KEY.MOVE_NE: return 'NE';
+            case Keys.KEY.MOVE_E: return 'E';
+            case Keys.KEY.MOVE_SE: return 'SE';
+            default: return '';
+        }
+    }
+
+}
+
+interface Keys {
+    KeyMap: KeyMap;
+    KEY: {[key: string]: number};
+    GUI: {[key: string]: number};
+    [key: string]: any;
+}
+
+export const Keys: Keys = {
+    KeyMap: new KeyMap(),
+    KEY: {},
+    GUI: {}
+};
 
 /* eslint-disable */
 Keys.VK_a = ROT.VK_A + 32;
@@ -68,157 +283,6 @@ Keys.VK_PERIOD = 46;
 Keys.VK_LT = 60;
 Keys.VK_GT = 62;
 
-/* Lookup table object for movement and actions keys.*/
-Keys.KeyMap = {
-
-    moveKeyMap: { },
-
-    // Start from W, go clock wise on keyboard
-    initMap(): void {
-        this.moveKeyMap[Keys.KEY.MOVE_N] = 0;
-        this.moveKeyMap[Keys.KEY.MOVE_NE] = 1;
-        this.moveKeyMap[Keys.KEY.MOVE_E] = 2;
-        this.moveKeyMap[Keys.KEY.MOVE_SE] = 3;
-        this.moveKeyMap[Keys.KEY.MOVE_S] = 4;
-        this.moveKeyMap[Keys.KEY.MOVE_SW] = 5;
-        this.moveKeyMap[Keys.KEY.MOVE_W] = 6;
-        this.moveKeyMap[Keys.KEY.MOVE_NW] = 7;
-
-        this.moveKeyMap[ROT.VK_8] = 0;
-        this.moveKeyMap[ROT.VK_9] = 1;
-        this.moveKeyMap[ROT.VK_6] = 2;
-        this.moveKeyMap[ROT.VK_3] = 3;
-        this.moveKeyMap[ROT.VK_2] = 4;
-        this.moveKeyMap[ROT.VK_1] = 5;
-        this.moveKeyMap[ROT.VK_4] = 6;
-        this.moveKeyMap[ROT.VK_7] = 7;
-    },
-
-    inMoveCodeMap(code: number): boolean {
-        return this.moveKeyMap.hasOwnProperty(code);
-    },
-
-    isRest: (code: number): boolean => (
-        (code === Keys.VK_s || code === Keys.VK_PERIOD)
-    ),
-    isPickup: (code: number): boolean => code === Keys.KEY.PICKUP,
-    isUseStairs: (code: number): boolean => (
-        code === Keys.KEY.USE_STAIRS_DOWN || code === Keys.KEY.USE_STAIRS_UP
-    ),
-    isChat: (code: number): boolean => code === Keys.KEY.CHAT,
-    isConfirmYes: (code: number): boolean => code === Keys.KEY.YES,
-    isConfirmNo: (code: number): boolean => code === Keys.KEY.NO,
-    isFightMode: (code: number): boolean => code === Keys.KEY.FIGHT,
-    isGive: (code: number): boolean => code === Keys.KEY.GIVE,
-    isGoto: (code: number): boolean => code === Keys.KEY.GOTO,
-    isJump: (code: number): boolean => code === Keys.KEY.JUMP,
-    isIssueOrder: (code: number): boolean => code === Keys.KEY.ORDER,
-    isLook: (code: number): boolean => code === Keys.KEY.LOOK,
-    isMark: (code: number): boolean => code === Keys.KEY.MARK,
-    isNextItem: (code: number): boolean => code === Keys.KEY.NEXT_ITEM,
-    isNextTarget: (code: number): boolean => code === Keys.KEY.NEXT,
-    isPrevTarget: (code: number): boolean => code === Keys.KEY.PREV,
-    isRead: (code: number): boolean => code === Keys.KEY.READ,
-    isRunMode: (code: number): boolean => code === Keys.KEY.RUN,
-    isSelect: (code: number): boolean => code === Keys.KEY.SELECT,
-    isSelectAll: (code: number): boolean => code === Keys.KEY.SELECT_ALL,
-    isTargetMode: (code: number): boolean => code === Keys.KEY.TARGET,
-    isToggleDoor: (code: number): boolean => code === Keys.KEY.DOOR,
-    isUsePower: (code: number): boolean => code === Keys.KEY.POWER,
-    isUseAbility: (code: number): boolean => code === Keys.KEY.ABILITY,
-    isMultiPurpose: (code: number): boolean => code === Keys.KEY.MULTI,
-
-    /* Based on keycode, computes and returns a new x,y pair. If code is
-     * invalid, returns null. */
-    getDiff(code: number, x: number, y: number): TCoord | null {
-        if (this.moveKeyMap.hasOwnProperty(code)) {
-            const diff = ROT.DIRS[8][this.moveKeyMap[code]];
-            const newX = x + diff[0];
-            const newY = y + diff[1];
-            return [newX, newY];
-        }
-        else if (code === Keys.VK_s) {
-            return [x, y];
-        }
-        else {
-            return null;
-        }
-    },
-
-    /* Returns a direction vector for given keycode. */
-    getDir(code: number): TCoord | null {
-        if (this.moveKeyMap.hasOwnProperty(code)) {
-            const selIndex = this.moveKeyMap[code];
-            return ROT.DIRS[8][selIndex] as TCoord;
-        }
-        else if (this.isRest(code)) {
-            return [0, 0];
-        }
-        return null;
-    },
-
-    /* Converts a direction vector to keycode. */
-    dirToKeyCode(dXArg: number | TCoord, dYArg?: number): number {
-        // Normalize first to unit vector (-1,0 or 1)
-        let dX = dXArg as number;
-        let dY = dYArg;
-        if (Array.isArray(dXArg)) {
-            dX = dXArg[0] as number;
-            dY = dXArg[1];
-        }
-        if (dX !== 0) {dX = dX / Math.abs(dX);}
-        if (dY !== 0) {dY = dY / Math.abs(dY);}
-
-        switch (dX) {
-            case -1:
-                switch (dY) {
-                    case -1: return Keys.KEY.MOVE_NW;
-                    case 0: return Keys.KEY.MOVE_W;
-                    case 1: return Keys.KEY.MOVE_SW;
-                    default: RG.err('Keys.KeyMap', 'dirToKeyCode',
-                        `Dir ${dX},${dY} not supported`);
-                }
-                break;
-            case 0:
-                switch (dY) {
-                    case -1: return Keys.KEY.MOVE_N;
-                    case 0: return Keys.KEY.REST;
-                    case 1: return Keys.KEY.MOVE_S;
-                    default: RG.err('Keys.KeyMap', 'dirToKeyCode',
-                        `Dir ${dX},${dY} not supported`);
-                }
-                break;
-            case 1:
-                switch (dY) {
-                    case -1: return Keys.KEY.MOVE_NE;
-                    case 0: return Keys.KEY.MOVE_E;
-                    case 1: return Keys.KEY.MOVE_SE;
-                    default: RG.err('Keys.KeyMap', 'dirToKeyCode',
-                        `Dir ${dX},${dY} not supported`);
-                }
-                break;
-            default: RG.err('Keys.KeyMap', 'dirToKeyCode',
-                `Dir ${dX},${dY} not supported`);
-        }
-        return null;
-    },
-
-    keyCodeToCardinalDir(code: number): string {
-        switch (code) {
-            case Keys.KEY.MOVE_NW: return 'NW';
-            case Keys.KEY.MOVE_W: return 'W';
-            case Keys.KEY.MOVE_SW: return 'SW';
-            case Keys.KEY.MOVE_N: return 'N';
-            case Keys.KEY.REST: return 'REST';
-            case Keys.KEY.MOVE_S: return 'S';
-            case Keys.KEY.MOVE_NE: return 'NE';
-            case Keys.KEY.MOVE_E: return 'E';
-            case Keys.KEY.MOVE_SE: return 'SE';
-            default: return '';
-        }
-    }
-
-};
 
 Keys.menuIndices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f',
     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
