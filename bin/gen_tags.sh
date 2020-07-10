06656b58c1053009874f61018a0aee168a4d5020
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-07-10 12:50:33 +0300

    Updated glob

--- bin/gen_tags.sh
+++ bin/gen_tags.sh
@@ -2,7 +2,7 @@
 
 #find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./jsctags {} -f \; | sed '/^$/d' > tags_temp
 #find ./client -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec ./jsctags {} -f \; | sed '/^$/d' | sort > tags_temp
-ctags -R ./client tests/helpers/ tests/*.js lib/rot.js
+ctags -R ./client tests/helpers/ tests/*.ts lib/rot.js
 #cat tags >> tags_temp
 #cat tags_temp | sort > tags
 #rm tags_temp
