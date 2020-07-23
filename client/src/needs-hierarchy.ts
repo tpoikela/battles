ceb09a65953319402bfbdac1ec2000b24271c923
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-07-24 00:14:17 +0300

    Added support for job schedules

--- client/src/needs-hierarchy.ts
+++ client/src/needs-hierarchy.ts
@@ -4,8 +4,10 @@ const debug = dbg('bitn:Needs');
 
 import RG from './rg';
 import {Evaluator} from './evaluators';
-import {RandWeights} from './interfaces';
+import {RandWeights, IConstraint} from './interfaces';
 import {Random} from './random';
+import {Constraints} from './constraints';
+import {JobSchedule} from './job-schedule';
 
 debug.enabled = true;
 
@@ -24,89 +26,183 @@ const needToEvalMap: any = {
 const BIAS: {[key: string]: number} = {
     Equip: 1.0,
     Flee: 1.5,
+    FindAmmo: 1.0,
+    FindMissile: 1.0,
     FindFood: 1.0,
     FindWeapon: 1.0,
     Rest: 1.0
 };
 
-type EvalPair = [string, number];
+export interface INeedEntry {
+    last?: boolean;
+    only?: boolean;
+    constr: IConstraint;
+    func?: (args: any) => boolean;
+    evalName: string;
+    bias: number;
+}
+
+/* Passed as props to Evaluator object. */
+interface EvalArgs {
+    isOneShot?: boolean;
+    ammoType?: string;
+}
+
+export type EvalTuple = [string, number, EvalArgs?];
+
+const createConstr = new Constraints();
+
+/* All needs can be added here. This is data-driven
+ * approach to defining the needs without any
+ * functions. */
+export const Needs: {[key: string]: INeedEntry} = {};
+
+const needHealth: INeedEntry = {
+    constr: {
+        op: '<', value: 0.2, comp: ['Health', 'propLeft']
+    },
+    evalName: 'Flee', bias: BIAS.Flee
+};
+Needs.Health = needHealth;
+const needRest: INeedEntry = {
+    constr: {
+        op: '<', value: 0.5, comp: ['Health', 'propLeft']
+    },
+    evalName: 'Rest', bias: BIAS.Rest
+};
+Needs.Rest = needRest;
+
+const needHunger: INeedEntry = {
+    constr: {
+        op: 'eq', value: true, comp: ['Hunger', 'isStarving']
+    },
+    evalName: 'FindFood', bias: BIAS.FindFood
+};
+Needs.Hunger = needHunger;
 
 /* Needs hierarchy does not have actual state. The state of needs is based on
  * components attached to an actor (ie Hunger/Health/Needs), etc. This object
  * handles mapping needs to different evaluators. */
 export class NeedsHierarchy {
 
+    public needs: INeedEntry[];
     public weightedRand: boolean;
     protected actor: SentientActor;
     protected _debug: boolean;
+    protected _jobSchedule: JobSchedule;
 
     constructor(actor: SentientActor) {
         this.actor = actor;
         this.weightedRand = true;
         this._debug = debug.enabled;
+        this.needs = [];
+
+        // Order is important, especially for last/only flags
+        this.addNeedWithEval(needHealth);
+        this.addNeedWithEval(needRest);
+        this.addNeedWithEval(needHunger);
     }
 
     public setDebug(debugEn: boolean): void {
         this._debug = debugEn;
     }
 
-    public process(goal: GoalTop): void {
-        const evals: EvalPair[] = this.getEvaluators(goal);
+    public process(goal: GoalTop, turnArgs): void {
+        const evals: EvalTuple[] = this.getEvaluators(goal);
+        const {timeOfDay} = turnArgs;
+        this.processJobSchedule(evals, timeOfDay);
 
         // Either inject only one evaluator, or inject several based on all
         // different needs
         if (this.weightedRand && evals.length > 0) {
             const weights: RandWeights = {};
-            evals.forEach((evalPair: EvalPair) => {
-                const [name, evalBias] = evalPair;
+            const evalArgs: any = {};
+            evals.forEach((evalPair: EvalTuple) => {
+                const [name, evalBias, args] = evalPair;
                 if (!weights[name]) {
                     weights[name] = evalBias;
                 }
                 else {
                     weights[name] += evalBias;
                 }
+                // Combine flags set for different evaluators
+                if (args) {
+                    if (evalArgs[name]) {
+                        evalArgs[name] = Object.assign(evalArgs[name], args);
+                    }
+                    else {
+                        evalArgs[name] = args;
+                    }
+                }
             });
 
             const evalName = RNG.getWeighted(weights);
             const bias = weights[evalName];
             this.dbg(`Chose need [${evalName}], bias: ${bias}`);
+
+            let evalObj: any = null;
             if (Evaluator[evalName]) {
-                goal.addEvaluator(new Evaluator[evalName](bias));
+                evalObj = new Evaluator[evalName](bias);
             }
             else {
                 // Use generic evaluator as no specific one is available
-                goal.addEvaluator(Evaluator.create(evalName, bias));
+                evalObj = Evaluator.create(evalName, bias);
+            }
+            goal.removeEvaluatorsByType(evalName); // Prevent duplicates
+            goal.addEvaluator(evalObj);
+            if (evalArgs[evalName]) {
+                Object.keys(evalArgs[evalName]).forEach(key => {
+                evalObj[key] = evalArgs[evalName][key];
+                });
             }
         }
         else {
             // Simply add each evaluator
-            evals.forEach((evalPair: EvalPair) => {
+            evals.forEach((evalPair: EvalTuple) => {
                 const evalName = evalPair[0];
-                goal.addEvaluator(new Evaluator[evalName](BIAS[evalName]));
+                const evalObj = new Evaluator[evalName](BIAS[evalName]);
+                goal.addEvaluator(evalObj);
+                if (evalPair[2] && evalPair[2].isOneShot) {
+                    evalObj.isOneShot = true;
+                }
             });
         }
     }
 
+    public addNeedWithEval(need: INeedEntry): void {
+        if (!need.func) {
+            need.func = createConstr.getConstraints(need.constr);
+        }
+        this.needs.push(need);
+    }
+
+
+    public processJobSchedule(evals, timeOfDay): void {
+        this._jobSchedule.getCurrentNeeds(evals, timeOfDay);
+    }
+
     /* Returns the evaluators required to satisfy actors needs. */
-    protected getEvaluators(goal: GoalTop): EvalPair[] {
+    protected getEvaluators(goal: GoalTop): EvalTuple[] {
         const actor = this.actor;
-        const health = actor.get('Health');
-        const evals: EvalPair[] = [];
+        const evals: EvalTuple[] = [];
         const inv: Inventory = this.actor.getInvEq();
 
-        // Health checks
-        if (health.propLeft() < 0.2) {
-            evals.push(['Flee', BIAS.Flee]);
-        }
-        else if (health.propLeft() < 0.5) {
-            evals.push(['Rest', 0.5]);
-        }
+        for (let i = 0; i < this.needs.length; i++) {
+            const need = this.needs[i];
+            if (need.func(this.actor)) {
+                const evalPair: EvalTuple = [need.evalName, need.bias];
 
-        if (actor.has('Hunger')) {
-            if (actor.get('Hunger').isStarving()) {
-                evals.push(['FindFood', BIAS.FindFood]);
+                // Use only current eval if only flag is set
+                if (need.only) {
+                    return [evalPair];
+                }
+
+                evals.push(evalPair);
+                // Stop iteration if last flag is given
+                if (need.last) {return evals;}
             }
         }
+
 
         // Spellcasters may need some resting
         if (actor.has('SpellPower')) {
@@ -117,9 +213,47 @@ export class NeedsHierarchy {
             }
         }
 
-        // If out of missiles, and has weapon, find missiles
 
         // If no weapon, find one
+        this.weaponNeeds(goal, evals);
+
+        // If no equipment, find some
+
+        // If not close to ally/friend, find one
+
+        // If out of missiles, and has weapon, find missiles
+        this.findAmmo(evals);
+
+        return evals;
+    }
+
+
+    protected findAmmo(evals: EvalTuple[]): void {
+        const inv: Inventory = this.actor.getInvEq();
+        const missWeapon = inv.getMissileWeapon();
+        const miss = inv.getMissile();
+        if (missWeapon) {
+            const ammoType = missWeapon.getType();
+            evals.push(['FindAmmo' , BIAS.FindAmmo, {ammoType}]);
+        }
+        else if (!miss) {
+            const items = inv.getInventory().getItems();
+            const missile = items.find(i => i.getType() === RG.ITEM.MISSILE);
+            if (!missile) {
+                evals.push(['FindMissile' , BIAS.FindMissile]);
+            }
+            else {
+                evals.push(['Equip', BIAS.Equip, {isOneShot: true}]);
+            }
+        }
+        else {
+            evals.push(['FindMissile' , BIAS.FindMissile]);
+        }
+    }
+
+    protected weaponNeeds(goal, evals: EvalTuple[]): void {
+        const inv: Inventory = this.actor.getInvEq();
+
         const weapon = inv.getWeapon();
         if (!weapon) {
             this.dbg('No weapon found. Checking inventory');
@@ -134,20 +268,13 @@ export class NeedsHierarchy {
             else {
                 this.dbg('Found Weapon in inv. Trying to equip one');
                 goal.removeEvaluatorsByType('FindWeapon');
-                evals.push(['Equip', BIAS.Equip]);
+                evals.push(['Equip', BIAS.Equip, {isOneShot: true}]);
             }
-            return evals;
+            return;
         }
         else {
             // Find better weapon..
         }
-
-        // If no equipment, find some
-
-
-        // If not close to ally/friend, find one
-
-        return evals;
     }
 
     protected dbg(msg: string): void {
