ad3ee9e409f60b37b99e4a6da3b82eb98dffd66b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-08-08 12:18:49 +0300

    Added comment

--- client/data/lore.ts
+++ client/data/lore.ts
@@ -210,6 +210,7 @@ export function createLoreObj(
         }
     };
     if (metaData) {
+        // On compObj, call compObj.addEntry({..., metaData})
         compObj.func.addEntry.metaData = metaData;
     }
     return compObj;
