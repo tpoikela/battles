b5bbf4cec950a738d3caf035eea090fe3b5b7c49
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-08-18 08:10:24 +0300

    Added more iterations to the test

--- tests/client/src/dungeon-generator.spec.ts
+++ tests/client/src/dungeon-generator.spec.ts
@@ -3,7 +3,8 @@ import { expect } from 'chai';
 
 import {Random} from '../../../client/src/random';
 import {DungeonGenerator} from '../../../client/src/generator';
-import {CellMap} from '../../../client/src/map';
+// import {CellMap} from '../../../client/src/map';
+import {Dice} from '../../../client/src/dice';
 
 const RNG = Random.getRNG();
 
@@ -12,18 +13,24 @@ describe('DungeonGenerator', function() {
 
     it('generates dungeon levels', () => {
         RNG.setSeed(new Date().getTime());
+        // RNG.setSeed(6);
+        // Dice.RNG.setSeed(6);
         const dungGen = new DungeonGenerator();
+        // dungGen._debug = true;
         const conf = {
             nBigRooms: 0,
             // rerunOnFailure: true
             errorOnFailure: true
         };
-        for (let i = 0; i < 1; i++) {
+        for (let i = 0; i < 5; i++) {
             const cols = RNG.getUniformInt(80, 120);
             const rows = RNG.getUniformInt(28, 56);
+            // const rows = RNG.getUniformInt(28, 120);
             const level = dungGen.create(cols, rows, conf);
             // level.debugPrintInASCII();
-            expect(level).to.exist;
+            expect(level.getActors()).to.have.length.above(5);
+            expect(level.getItems()).to.have.length.above(5);
+            expect(level.getElements()).to.have.length.above(0);
         }
     });
 
