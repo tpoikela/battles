c7fd7a13320861da65501664667100d9cd183723
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-09-05 21:32:02 +0300

    Modified default actor char

--- client/src/rg.ts
+++ client/src/rg.ts
@@ -284,7 +284,7 @@ class RGClass {
                 }
             },
             actors: {
-                default: 'X'
+                default: '@'
             },
             items: {
                 default: '?',
