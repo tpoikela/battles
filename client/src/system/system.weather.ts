1970e5bf6d59789d6a9935fd9dfe98338a982a02
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-09-14 08:43:32 +0300

    Added types and messages

--- client/src/system/system.weather.ts
+++ client/src/system/system.weather.ts
@@ -3,23 +3,33 @@ import RG from '../rg';
 import {SystemBase} from './system.base';
 import {EventPool} from '../eventpool';
 import {MapGenerator} from '../generator';
+import {snowMeltMap} from '../../data/elem-constants';
+
+type CellMap = import('../map').CellMap;
+type Cell = import('../map.cell').Cell;
+type Entity = import('../entity').Entity;
 
 /* Handles WeatherEffect components and has handler functions for
  * different types of weather effects. */
 export class SystemWeather extends SystemBase {
 
-    private _effTable: {[key: string]: (ent, comp) => void};
+    private _effTable: {[key: string]: (ent: Entity, comp) => void};
 
-    constructor(compTypes, pool?: EventPool) {
+    constructor(compTypes: string[], pool?: EventPool) {
         super(RG.SYS.WEATHER, compTypes, pool);
 
+        // TODO: Proper weather. Just a simplified model here.
         this._effTable = {
+            // Winter
             snowStorm: this.handleSnowStorm = this.handleSnowStorm.bind(this),
-            warm: this.handleMeltSnow = this.handleMeltSnow.bind(this)
+            // Summer/Spring
+            warm: this.handleMeltSnow = this.handleMeltSnow.bind(this),
+            // Autumn
+            rain: this.handleRain = this.handleRain.bind(this)
         };
     }
 
-    public updateEntity(ent): void {
+    public updateEntity(ent: Entity): void {
         if (ent.has('WeatherEffect')) {
             const eff = ent.get('WeatherEffect');
             const effName = eff.getEffectType();
@@ -30,25 +40,42 @@ export class SystemWeather extends SystemBase {
         }
     }
 
-    protected handleSnowStorm(ent, comp): void {
-        const level = ent.getLevel();
+    protected handleSnowStorm(ent: Entity, comp): void {
+        const level = RG.getLevel(ent);
+        if (level) {
+            const map = level.getMap();
+            const nonSnowCells = map.getFree().filter(
+                c => !c.getBaseElem().has('Snowy'));
+            MapGenerator.addRandomSnow(map, 0.1, nonSnowCells);
+            RG.gameMsg('It is snowing heavily!');
+        }
+        else {
+            RG.err('SystemWeather', 'handleSnowStorm',
+                `Null level for ent: ${JSON.stringify(ent)}`);
+        }
+    }
+
+    protected handleRain(ent: Entity, comp): void {
+        const level = RG.getLevel(ent);
         const map = level.getMap();
-        MapGenerator.addRandomSnow(map, 0.1);
+        // MapGenerator.addRandomSnow(map, 0.1);
     }
 
     /* Melts down the snow located in the level with the entity. */
     protected handleMeltSnow(ent, comp): void {
         const ratio = 0.10;
-        const map = ent.getLevel().getMap();
+        const map: CellMap = ent.getLevel().getMap();
         const snowCells = map.getFree().filter(c => c.getBaseElem().has('Snowy'));
-        snowCells.forEach(cell => {
+
+        snowCells.forEach((cell: Cell) => {
             const meltSnow = this.rng.getUniform();
             if (meltSnow < ratio) {
                 const baseType = cell.getBaseElem().getType();
-                const newElem = MapGenerator.snowMeltMap[baseType];
+                const newElem = snowMeltMap[baseType];
                 cell.setBaseElem(newElem);
             }
         });
+        RG.gameMsg('It is getting warmer. Snow and ice are melting');
     }
 
 }
