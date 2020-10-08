41392a75d9ed9007e2d0b0388f3537da0247d44b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-10-08 10:46:24 +0300

    Added combine() and side manipulation methods

--- client/src/bbox.ts
+++ client/src/bbox.ts
@@ -5,6 +5,13 @@ import {Random} from './random';
 
 const RNG: Random = Random.getRNG();
 
+type TLine = [TCoord, TCoord];
+
+const SIDE_TOP = 0;
+const SIDE_LEFT = 1;
+const SIDE_RIGHT = 2;
+const SIDE_BOTTOM = 3;
+
 /* Bounding box (BBox) is defined 4 points:
  * upper-left X: ulx (smaller X)
  * upper-left Y: uly (smaller Y)
@@ -142,6 +149,54 @@ export class BBox {
         return res;
     }
 
+    public getSides(): TLine[] {
+        const res: TLine[] = [
+            [[this.ulx, this.uly], [this.lrx, this.uly]], // Top
+            [[this.ulx, this.uly], [this.ulx, this.lry]], // Left
+            [[this.lrx, this.uly], [this.lrx, this.lry]], // Right
+            [[this.ulx, this.lry], [this.lrx, this.lry]], // Bottom
+        ];
+        return res;
+    }
+
+    public getMatchingSide(rhs: BBox): [number, TLine] | null {
+        const sides: TLine[] = this.getSides();
+        const rhsSides: TLine[] = rhs.getSides();
+        for (let i = 0; i < sides.length; i++) {
+            const side = sides[i];
+            for (let j = 0;  j < rhsSides.length; j++) {
+                const rhsSide = rhsSides[j];
+                if (sidesMatch(side, rhsSide)) {
+                    return [i, side];
+                }
+            }
+        }
+        return null;
+    }
+
+    public combine(rhs: BBox): BBox | null {
+        const side = this.getMatchingSide(rhs);
+        if (side) {
+            const [n, line] = side;
+            if (n === SIDE_TOP) {
+                // rhs above this bbox
+                return new BBox(rhs.ulx, rhs.uly, this.lrx, this.lry);
+            }
+            else if (n === SIDE_LEFT) {
+                // rhs left of this bbox
+                return new BBox(rhs.ulx, rhs.uly, this.lrx, this.lry);
+            }
+            else if (n === SIDE_RIGHT) {
+                // rhs right of this bbox
+                return new BBox(this.ulx, this.uly, rhs.lrx, rhs.lry);
+            }
+            else {
+                return new BBox(this.ulx, this.uly, rhs.lrx, rhs.lry);
+            }
+        }
+        return null;
+    }
+
     /* Returns a box of coordinates given starting point and end points
      * (inclusive). */
     public getCoord(): TCoord[] {
@@ -172,4 +227,16 @@ export class BBox {
         ];
     }
 
+}
+
+/* Returns true if the 2 given sides match (have same start/end). */
+function sidesMatch(lhs: TLine, rhs: TLine): boolean {
+    const [cl0, cl1] = lhs;
+    const [cr0, cr1] = rhs;
+    if (cl0[0] === cr0[0] && cl0[1] === cr0[1]) {
+        if (cl1[0] === cr1[0] && cl1[1] === cr1[1]) {
+            return true;
+        }
+    }
+    return false;
 }
