49ed5b635d3c0f323d9d49a30600244c2103bcbb
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2021-08-10 10:29:14 +0300

    Cleanup of console.log

--- tests/client/src/fov3d.spec.ts
+++ tests/client/src/fov3d.spec.ts
@@ -9,7 +9,6 @@ import {FactoryLevel} from '../../../client/src/factory.level';
 // import {ELEM} from '../../../client/data/elem-constants';
 
 describe('Fov3D', function() {
-    // this.timeout(5000);
 
     it('it computes 3D FOV', () => {
         const factLevel = new FactoryLevel();
@@ -20,18 +19,13 @@ describe('Fov3D', function() {
         actor.setFOVRange(1);
 
         let cells = map.getCellsInFOV(actor);
-        console.log('Seen cells are:', cells.map(c => c.getXY()));
         expect(cells).to.have.length(9);
 
-        // return;
-
         const cells2 = map.getCellsInFOV(actor);
-        // console.log('Seen cells2 are:', cells2.map(c => c.getXY()));
         expect(cells2).to.have.length(cells.length);
 
         level.moveActorTo(actor, 1, 1);
         cells = map.getCellsInFOV(actor);
-        // console.log('Seen cells are:', cells.map(c => c.getXY()));
         expect(cells).to.have.length(9);
 
         cells.forEach((c) => {
@@ -42,14 +36,12 @@ describe('Fov3D', function() {
         actor.setFOVRange(2);
         level.moveActorTo(actor, 2, 2);
         cells = map.getCellsInFOV(actor);
-        // console.log('Seen cells are:', cells.map(c => c.getXY()));
         expect(cells).to.have.length(25);
 
         actor.setFOVRange(3);
         level.moveActorTo(actor, 3, 3);
         cells = map.getCellsInFOV(actor);
         cells = cells.sort((a, b) => a.getX() < b.getX() ? -1 : 1);
-        console.log('Seen cells are:', cells.map(c => c.getXY()));
         expect(cells).to.have.length(45);
 
         actor.setFOVRange(4);
